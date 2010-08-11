ef693f5f47cd3daf7b9c24c1e7049b63b2e9d82e
Sebastian Klose <kloses@in.tum.de>
2010-08-11 15:34:22 +0000

    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@203 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tools/io/AbstractCamera.hpp
+++ tools/io/AbstractCamera.hpp
@@ -1,11 +1,80 @@
 #ifndef TOOLS_ABSTRACT_CAMERA
 #define TOOLS_ABSTRACT_CAMERA
 
-class AbstractCamera 
-{		
-	virtual ~AbstractCamera()=0;
-	
-	virtual IplImage* image() = 0;
-};
+#include <opencv/cv.h>
+
+namespace tools {
+    struct CameraParameters {
+        CameraParameters():
+            width(752),
+            height(480),
+            horizonticalMirror(false),
+            verticalMirror(false),
+            autoWhiteBalance(false),
+            autoGain(false),
+            autoShutter(false),
+            autoSensorShutter(false),
+            fps(60.0),
+            pixelClock(20.0),
+            maxAutoShutter(33.0),
+            exposureTime(33.0)
+        {
+        }
+
+        unsigned int width;
+        unsigned int height;
+
+        bool horizonticalMirror;
+        bool verticalMirror;
+        bool autoWhiteBalance;
+        bool autoGain;
+        bool autoShutter;
+        bool autoSensorShutter;
+
+        double fps;
+        unsigned int pixelClock;
+        double maxAutoShutter;
+        double exposureTime;
+    };
+
+    class AbstractCamera
+    {
+        public:
+            AbstractCamera(CameraParameters const& params = CameraParameters()):
+                _width(params.width),
+                _height(params.height)
+        {
+        }
+
+            virtual ~AbstractCamera(){};
+
+            virtual void init() = 0;
+
+            virtual void captureNext() = 0;
+            virtual void captureStart() = 0;
+            virtual void captureStop() = 0;
+
+            virtual IplImage const* image() = 0;
+
+            virtual void setAutoGain(bool value) = 0;
+            virtual void setAutoShutter(bool value) = 0;
+            virtual void setAutoSensorShutter(bool value) = 0;
+            virtual void setAutoWhiteBalance(bool value) = 0;
+            virtual void setMaxAutoShutter(double value) = 0;
+            virtual void setExposureTime(double value) = 0;
+            virtual void setPixelClock(unsigned int value) = 0;
+            virtual void setHorizontalMirror(bool value) = 0;
+            virtual void setVerticalMirror(bool value) = 0;
+            virtual void setParameters(CameraParameters const& params) = 0;
+
+            unsigned int width(){ return _width; }
+            unsigned int height(){ return _height; }
+
+        protected:
+            unsigned int _width;
+            unsigned int _height;
+    };
+
+}
 
 #endif
--- tools/math/LevenbergMarquard.cpp
+++ tools/math/LevenbergMarquard.cpp
@@ -7,112 +7,128 @@
  *
  */
 
-#include "LevenbergMarquard.h"
+namespace tools {
 
-LevenbergMarquard::LevenbergMarquard(TerminationCriteria termCrit):
-	maxIterations(20),
-	maxEpsilon(0.0001),
-	lamda(0.001),
-	lamdaUpdateFactor(10.0),
-	terminationCriteria(termCrit)
-{
-}
-
-LevenbergMarquard::~LevenbergMarquard()
-{
-}
-
-void LevenbergMarquard::setMaxIterations(unsigned int maxIters)
-{
-	maxIterations = maxIters;
-}
-
-void LevenbergMarquard::setMaxEpsilon(double maxEps)
-{
-	maxEpsilon = maxEps;
-}
-
-template <class CostFunctionType, class JacobianFunctionType>
-void LevenbergMarquard::optimize(CostFunctionType& costs,
-								 JacobianFunctionType& jacFunc,
-								 Eigen::VectorXd & startParameters, 
-								 Eigen::VectorXd & measurement,
-								 Eigen::VectorXd & deltaParameters)
-{
-	Eigen::VectorXd residual(measurement.rows());
-	
-	Eigen::VectorXd currentParameters(startParameters.rows());
-	Eigen::VectorXd currentDeltaP(startParameters.rows());
-	Eigen::VectorXd jTres(startParameters.rows());
-	
-	Eigen::MatrixXd jacobian(measurement.rows(), startParameters.rows());
-	Eigen::MatrixXd jT(startParameters.rows(), measurement.rows());
-	Eigen::MatrixXd jTj(startParameters.rows());
-	
-	deltaParameters.setZero();	
-	currentParameters = startParameters;
-	
-	// start the iteration:
-	unsigned int currentIter = 1;
-	double currentCosts;
-	double lastCosts;
-	
-	while (true) {
-		// get the residual:
-		costs.residual(currentParameters, measurement, residual);
-	
-		// get the jacobians
-		jacFunc.jacobian(currentParameters, jacobian);
-		jT = jacobian.transpose();
-		jTj = jT*jacobian;
-		jTres = jT*residual;
-		jTres.cwise()*=-1.0;
-		
-		// add lambda to the diagonal
-		jTj.diagonal().cwise()+=lamda;
-		
-		// get the costs before the update:
-		lastCosts = costs.costFunction(currentParameters, measurement);
-				
-		// do the update:
-		jTj.svd().solve(jTres, &currentDeltaP);
-		
-		// update parameters:
-		currentParameters+=currentDeltaP;
-		
-		// evaluate the current costs
-		currentCosts = costs.costFunction(currentParameters, measurement);
-		
-		if(currentCosts > lastCosts){
-			currentParameters-=currentDeltaP;
-			lamda*=lamdaUpdateFactor;
-			continue;
-		}
-		
-		lamda/=lamdaUpdateFactor;
-		
-		currentIter++;
-		if(checkTermination(currentIter, currentCosts)){
-			break;
-		}
+	LevenbergMarquard::LevenbergMarquard(TerminationCriteria termCrit):
+		maxIterations(20),
+		maxEpsilon(0.0001),
+		lamda(0.001),
+		lamdaUpdateFactor(10.0),
+		terminationCriteria(termCrit)
+	{
 	}
-	
-	lamda = 0.001;
-}
 
-bool LevenbergMarquard::checkTermination(unsigned int currIter, double currEpsilon)
-{
-	if(terminationCriteria == Iterations || terminationCriteria == IterationsOrEpsilon){
-		if (currIter > maxIterations) {
-			return true;
-		}
+	LevenbergMarquard::~LevenbergMarquard()
+	{
 	}
-	
-	if(terminationCriteria == Epsilon || terminationCriteria == IterationsOrEpsilon){
-		if (currEpsilon < maxEpsilon) {
-			return true;
-		}
+
+	void LevenbergMarquard::setMaxIterations(unsigned int maxIters)
+	{
+		maxIterations = maxIters;
 	}
-	
-	return false;
+
+	void LevenbergMarquard::setMaxEpsilon(double maxEps)
+	{
+		maxEpsilon = maxEps;
+	}
+
+	template <class CostFunctionType, class JacobianFunctionType>
+		double LevenbergMarquard::optimize(CostFunctionType& costs,
+										   JacobianFunctionType& jacFunc,
+										   Eigen::VectorXd & startParameters,
+										   std::vector<Eigen::VectorXd> & measurements,
+										   std::vector<Eigen::VectorXd> & model,
+										   Eigen::VectorXd & resultParameters)
+		{
+			int measurementDimension = measurements[0].rows();
+			int jacRows = measurementDimension * (int)measurements.size();
+			int parameterDimension = startParameters.rows();
+
+			Eigen::VectorXd currentDeltaP(parameterDimension);
+			Eigen::VectorXd jTres(parameterDimension);
+
+
+			Eigen::MatrixXd jacobian(jacRows, parameterDimension);
+			Eigen::VectorXd residual(jacobian.rows());
+			Eigen::MatrixXd jT(parameterDimension, jacobian.rows());
+			Eigen::MatrixXd jTj(parameterDimension, parameterDimension);
+
+			resultParameters = startParameters;
+
+			// start the iteration:
+			unsigned int currentIter = 1;
+			double currentCosts = costs.costs(resultParameters, model, measurements);
+			double lastCosts;
+
+			while (true) {
+				// get the residual:
+				costs.residual(resultParameters,
+							   model,
+							   measurements,
+							   residual);
+
+				jacFunc.jacobians(resultParameters,
+								  model,
+								  jacobian);
+
+				// get the jacobians
+				jT = jacobian.transpose();
+				jTj = jT*jacobian;
+				jTres = jT*residual;
+				jTres*=-1.0;
+
+				// add lambda to the diagonal
+				jTj.diagonal().cwise()+=lamda;
+
+				// save the costs before the update:
+				lastCosts = currentCosts;
+
+				// do the update:
+				jTj.svd().solve(jTres, &currentDeltaP);
+
+				// update parameters:
+				resultParameters+=currentDeltaP;
+
+				// evaluate the current costs
+				currentCosts = costs.costs(resultParameters, model, measurements);
+
+				if(currentCosts > lastCosts){
+					resultParameters-=currentDeltaP;
+					lamda*=lamdaUpdateFactor;
+					continue;
+				}
+
+				lamda/=lamdaUpdateFactor;
+
+				currentIter++;
+				if(checkTermination(currentIter, currentCosts))
+				{
+					break;
+				}
+
+				std::cout << "CurrentIteration: " << currentIter << " Epsilon: " << currentCosts << std::endl;
+			}
+
+			lamda = 0.001;
+
+			return currentCosts;
+		}
+
+	bool LevenbergMarquard::checkTermination(unsigned int currIter, double currEpsilon)
+	{
+		if(terminationCriteria == Iterations || terminationCriteria == IterationsOrEpsilon){
+			if (currIter > maxIterations) {
+				return true;
+			}
+		}
+
+		if(terminationCriteria == Epsilon || terminationCriteria == IterationsOrEpsilon){
+			if (currEpsilon < maxEpsilon) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 }
