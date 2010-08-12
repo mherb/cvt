75f15c0f7e0a988dfdc6e705e111da025adecd85
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-08-12 15:31:51 +0000

    Added Rect class and modified CMakeLists.txt
    	modified:   cvt/CMakeLists.txt
    	new file:   cvt/util/Rect.cpp
    	new file:   cvt/util/Rect.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@222 f7bcf5df-4c73-0410-a0d8-b61c104701fe

---
+++ cvt/util/Rect.cpp
@@ -0,0 +1,12 @@
+#include "Rect.h"
+#include "CVTTest.h"
+
+BEGIN_CVTTEST(rect)
+    cvt::Rectf r( 0.0f, 0.0f, 100.0f, 100.0f );
+    std::cout << r << std::endl;
+
+    std::cout << r.contains( r ) << std::endl;
+    std::cout << r.intersects( r ) << std::endl;
+    return true;
+END_CVTTEST
+
