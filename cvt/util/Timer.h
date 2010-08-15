375685f41b30af7fa005adcb3b5ed9d367c021f0
Philipp Heise <heise@in.tum.de>
2010-08-15 16:03:34 +0000

    Small improvements to V4L2Camera and Timer
    	modified:   io/V4L2Camera.cpp
    	modified:   util/Timer.h
    	modified:   ../tests/v4l2/v4l2.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@228 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/util/Timer.h
+++ cvt/util/Timer.h
@@ -32,7 +32,7 @@ namespace cvt {
 	    {
 		boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
 		boost::posix_time::time_duration t = now-time;
-		return t.total_microseconds();
+		return ( double ) t.total_microseconds();
 	    }
 
 	private:
--- tests/v4l2/v4l2.cpp
+++ tests/v4l2/v4l2.cpp
@@ -4,6 +4,7 @@
 
 #include <cvt/gfx/Image.h>
 #include <cvt/io/V4L2Camera.h>
+#include <cvt/util/Timer.h>
 
 using namespace cvt;
 
@@ -12,12 +13,15 @@ int main(int argc, char* argv[])
 	const Image* frame;
 	V4L2Camera cam( 0, 640, 480, 30.0 );
 	int key;
+	size_t frames = 0;
+	Timer timer;
 
 
 	cam.open();
 	cam.init();
 	cam.captureStart();
 
+	timer.reset();
 	while( 1 ) {
 	    cam.captureNext();
 	    frame = cam.image();
@@ -27,6 +31,12 @@ int main(int argc, char* argv[])
 	    key = cvWaitKey( 10 );
 	    if( ( key & 0xff ) == 27 )
 		break;
+	    frames++;
+	    if( timer.elapsedSeconds() > 5.0f ) {
+		std::cout << "FPS: " << ( double ) frames / timer.elapsedSeconds() << std::endl;
+		frames = 0;
+		timer.reset();
+	    }
 	}
 
 	return 0;
