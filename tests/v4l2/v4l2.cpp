ef90eb62f6587fb4ea9512ed40773d1387490dc3
Philipp Heise <heise@in.tum.de>
2010-08-16 14:33:17 +0000

    Lots of image stuff
    	modified:   ../cvt/gfx/Image.cpp
    	modified:   ../cvt/gfx/Image.h
    	modified:   ../cvt/util/SIMD.cpp
    	modified:   ../cvt/util/SIMD.h
    	modified:   ../tests/v4l2/v4l2.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@237 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/v4l2/v4l2.cpp
+++ tests/v4l2/v4l2.cpp
@@ -15,6 +15,7 @@ int main(int argc, char* argv[])
 	int key;
 	size_t frames = 0;
 	Timer timer;
+	Image f, dx, dy;
 
 
 	cam.open();
@@ -26,7 +27,11 @@ int main(int argc, char* argv[])
 	    cam.captureNext();
 	    frame = cam.image();
 
-	    cvShowImage( "V4L2", frame->iplimage() );
+	    frame->convert( f, CVT_GRAY, CVT_FLOAT );
+	    f.dx( dx );
+	    f.dy( dy );
+	    cvShowImage( "V4L2", dx.iplimage() );
+	    cvShowImage( "V4L2-2", dy.iplimage() );
 
 	    key = cvWaitKey( 10 );
 	    if( ( key & 0xff ) == 27 )
