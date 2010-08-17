5e5a2aeea01a064e4450c0e295dc6843c21a1f69
kloses <kloses@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-08-17 07:04:04 +0000

    - added visible points class for onboard tracking
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@240 f7bcf5df-4c73-0410-a0d8-b61c104701fe

---
+++ tests/onboardTracking/VisiblePoints.h
@@ -0,0 +1,45 @@
+/*
+ *  VisiblePoints.h
+ *  CVTools
+ *
+ *  Created by Sebastian Klose on 12.08.10.
+ *  Copyright 2010. All rights reserved.
+ */
+
+#ifndef VISIBLE_POINTS_H
+#define VISIBLE_POINTS_H
+
+#include "Range.h"
+
+#include <Eigen/Core>
+
+#include <vector>
+
+typedef std::vector<Eigen::Vector4d> PointsHom;
+
+class VisiblePoints
+{
+public:
+	
+	VisiblePoints(std::string file);
+	
+	void visiblityInfoForViewAngles(double alpha, 
+									double beta,
+									PointsHom & point,
+									PointsHom & edges);
+	
+	
+private:
+	Range<double> xAngleRange;
+	Range<double> yAngleRange;
+	
+	double xAngleStep;
+	double yAngleStep;	
+	
+	std::vector<std::vector<PointsHom> > visiblePointsForAngles;
+	std::vector<std::vector<PointsHom> > linesForPoints;
+	
+	void parse(std::string fileName);
+};
+
+#endif
