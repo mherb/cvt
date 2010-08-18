15d4554b1fcfd213408a1ddac517a343bbfef3b4
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-08-18 12:01:58 +0000

    Whitespace stuff in gfx
    	modified:   gfx/Color.h
    	modified:   gfx/IFilter.h
    	modified:   gfx/IFilterAttributes.h
    	modified:   gfx/Image.cpp
    	modified:   gfx/Image.h
    	modified:   gfx/ImageOperations.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@256 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gfx/Color.h
+++ cvt/gfx/Color.h
@@ -4,93 +4,93 @@
 #include "math/Math.h"
 
 namespace cvt {
-    class Image;
+	class Image;
 
-    class Color
-    {
-	friend class Image;
+	class Color
+	{
+		friend class Image;
 
-	public:
-	    Color( float r, float g, float b, float a );
-	    Color( float gray );
-	    Color( int r, int g, int b, int a );
+		public:
+		Color( float r, float g, float b, float a );
+		Color( float gray );
+		Color( int r, int g, int b, int a );
 
-	    void set( float r, float g, float b, float a );
-	    void set( int r, int g, int b, int a );
+		void set( float r, float g, float b, float a );
+		void set( int r, int g, int b, int a );
 
-	    void get( float& r, float& g, float& b, float& a ) const;
-	    void get( int& r, int& g, int& b, int& a ) const;
+		void get( float& r, float& g, float& b, float& a ) const;
+		void get( int& r, int& g, int& b, int& a ) const;
 
-	    float red() const { return _r; };
-	    float green() const { return _g; };
-	    float blue() const { return _b; };
-	    float alpha() const { return _a; };
-	    float gray() const;
+		float red() const { return _r; };
+		float green() const { return _g; };
+		float blue() const { return _b; };
+		float alpha() const { return _a; };
+		float gray() const;
 
-	    void setRed( float r ) { _r = r; };
-	    void setGreen( float g ) { _g = g; };
-	    void setBlue( float b ) { _b = b; };
-	    void setAlpha( float a ) { _a = a; };
+		void setRed( float r ) { _r = r; };
+		void setGreen( float g ) { _g = g; };
+		void setBlue( float b ) { _b = b; };
+		void setAlpha( float a ) { _a = a; };
 
-	private:
-	    float _r, _g, _b, _a;
-    };
+		private:
+		float _r, _g, _b, _a;
+	};
 
 
-    inline Color::Color( float r, float g, float b, float a )
-    {
-	set( r, g, b, a );
-    }
+	inline Color::Color( float r, float g, float b, float a )
+	{
+		set( r, g, b, a );
+	}
 
-    inline Color::Color( float g )
-    {
-	set( g, g, g, 1.0f );
-    }
+	inline Color::Color( float g )
+	{
+		set( g, g, g, 1.0f );
+	}
 
-    inline Color::Color( int r, int g, int b, int a )
-    {
-	_r = Math::clamp( ( float ) r / 255.0f, 0.0f, 1.0f );
-	_g = Math::clamp( ( float ) g / 255.0f, 0.0f, 1.0f );
-	_b = Math::clamp( ( float ) b / 255.0f, 0.0f, 1.0f );
-	_a = Math::clamp( ( float ) a / 255.0f, 0.0f, 1.0f );
-    }
+	inline Color::Color( int r, int g, int b, int a )
+	{
+		_r = Math::clamp( ( float ) r / 255.0f, 0.0f, 1.0f );
+		_g = Math::clamp( ( float ) g / 255.0f, 0.0f, 1.0f );
+		_b = Math::clamp( ( float ) b / 255.0f, 0.0f, 1.0f );
+		_a = Math::clamp( ( float ) a / 255.0f, 0.0f, 1.0f );
+	}
 
-    inline void Color::set( float r, float g, float b, float a )
-    {
-	_r = Math::clamp( r, 0.0f, 1.0f );
-	_g = Math::clamp( g, 0.0f, 1.0f );
-	_b = Math::clamp( b, 0.0f, 1.0f );
-	_a = Math::clamp( a, 0.0f, 1.0f );
-    }
+	inline void Color::set( float r, float g, float b, float a )
+	{
+		_r = Math::clamp( r, 0.0f, 1.0f );
+		_g = Math::clamp( g, 0.0f, 1.0f );
+		_b = Math::clamp( b, 0.0f, 1.0f );
+		_a = Math::clamp( a, 0.0f, 1.0f );
+	}
 
-    inline void Color::set( int r, int g, int b, int a )
-    {
-	_r = Math::clamp( ( float ) r / 255.0f, 0.0f, 1.0f );
-	_g = Math::clamp( ( float ) g / 255.0f, 0.0f, 1.0f );
-	_b = Math::clamp( ( float ) b / 255.0f, 0.0f, 1.0f );
-	_a = Math::clamp( ( float ) a / 255.0f, 0.0f, 1.0f );
-    }
+	inline void Color::set( int r, int g, int b, int a )
+	{
+		_r = Math::clamp( ( float ) r / 255.0f, 0.0f, 1.0f );
+		_g = Math::clamp( ( float ) g / 255.0f, 0.0f, 1.0f );
+		_b = Math::clamp( ( float ) b / 255.0f, 0.0f, 1.0f );
+		_a = Math::clamp( ( float ) a / 255.0f, 0.0f, 1.0f );
+	}
 
-    inline void Color::get( float& r, float& g, float& b, float& a ) const
-    {
-	r = _r;
-	g = _g;
-	b = _b;
-	a = _a;
-    }
+	inline void Color::get( float& r, float& g, float& b, float& a ) const
+	{
+		r = _r;
+		g = _g;
+		b = _b;
+		a = _a;
+	}
 
-    inline void Color::get( int& r, int& g, int& b, int& a ) const
-    {
-	r = ( int ) ( _r * 255.0f );
-	g = ( int ) ( _g * 255.0f );
-	b = ( int ) ( _b * 255.0f );
-	a = ( int ) ( _a * 255.0f );
-    }
+	inline void Color::get( int& r, int& g, int& b, int& a ) const
+	{
+		r = ( int ) ( _r * 255.0f );
+		g = ( int ) ( _g * 255.0f );
+		b = ( int ) ( _b * 255.0f );
+		a = ( int ) ( _a * 255.0f );
+	}
 
-    inline float Color::gray() const
-    {
-	return 0.2126f * _r + 0.7152f * _g + 0.0722f * _b;
-    }
+	inline float Color::gray() const
+	{
+		return 0.2126f * _r + 0.7152f * _g + 0.0722f * _b;
+	}
 
 }
 
--- cvt/gfx/Image.h
+++ cvt/gfx/Image.h
@@ -10,178 +10,178 @@
 
 namespace cvt {
 
-    enum ImageChannelOrder {
-	CVT_GRAY = 0,
-	CVT_GRAYALPHA,
-	CVT_RGBA,
-	CVT_BGRA
-    };
+	enum ImageChannelOrder {
+		CVT_GRAY = 0,
+		CVT_GRAYALPHA,
+		CVT_RGBA,
+		CVT_BGRA
+	};
 
-    enum ImageChannelType {
-	CVT_UBYTE = 0,
-	CVT_FLOAT,
-    };
+	enum ImageChannelType {
+		CVT_UBYTE = 0,
+		CVT_FLOAT,
+	};
 
-    class Image
-    {
-	friend std::ostream& operator<<(std::ostream &os, const Image &f);
+	class Image
+	{
+		friend std::ostream& operator<<(std::ostream &os, const Image &f);
 
-	public:
-	    Image( size_t w = 1, size_t h = 1, ImageChannelOrder order = CVT_RGBA, ImageChannelType type = CVT_UBYTE );
-	    Image( const Image& img );
-	    ~Image();
+		public:
+		Image( size_t w = 1, size_t h = 1, ImageChannelOrder order = CVT_RGBA, ImageChannelType type = CVT_UBYTE );
+		Image( const Image& img );
+		~Image();
 
-	    size_t width() const;
-	    size_t height() const;
-	    size_t stride() const;
-	    size_t channels() const;
-	    ImageChannelOrder order() const;
-	    ImageChannelType type() const;
-	    uint8_t* data();
-	    uint8_t* scanline( size_t i );
-	    void reallocate( size_t w, size_t h, ImageChannelOrder order = CVT_RGBA, ImageChannelType type = CVT_UBYTE );
-	    void reallocate( const Image& i );
-	    void copy( const Image& i );
-	    IplImage* iplimage() const;
+		size_t width() const;
+		size_t height() const;
+		size_t stride() const;
+		size_t channels() const;
+		ImageChannelOrder order() const;
+		ImageChannelType type() const;
+		uint8_t* data();
+		uint8_t* scanline( size_t i );
+		void reallocate( size_t w, size_t h, ImageChannelOrder order = CVT_RGBA, ImageChannelType type = CVT_UBYTE );
+		void reallocate( const Image& i );
+		void copy( const Image& i );
+		IplImage* iplimage() const;
 
-	    void convert( Image& dst, ImageChannelOrder order, ImageChannelType type ) const;
+		void convert( Image& dst, ImageChannelOrder order, ImageChannelType type ) const;
 
-	    void fill( const Color& c );
+		void fill( const Color& c );
 
-	    void add( float alpha = 0.0f );
-	    void sub( float alpha = 0.0f );
-	    void mul( float alpha = 1.0f );
-	    void mad( const Image& i, float alpha = 1.0f );
+		void add( float alpha = 0.0f );
+		void sub( float alpha = 0.0f );
+		void mul( float alpha = 1.0f );
+		void mad( const Image& i, float alpha = 1.0f );
 
-	    void add( const Color& c );
-	    void sub( const Color& c );
-	    void mul( const Color& c );
+		void add( const Color& c );
+		void sub( const Color& c );
+		void mul( const Color& c );
 
-	    void convolve( Image& dst, const Image& kernel, bool normalize = true ) const;
+		void convolve( Image& dst, const Image& kernel, bool normalize = true ) const;
 
-	    Image& operator=( const Color& c );
-	    Image& operator=( const Image& c );
+		Image& operator=( const Color& c );
+		Image& operator=( const Image& c );
 
-	    Image& operator*( float alpha );
-	    Image& operator+( float alpha );
-	    Image& operator-( float alpha );
+		Image& operator*( float alpha );
+		Image& operator+( float alpha );
+		Image& operator-( float alpha );
 
-	    Image& operator*( const Color& c );
-	    Image& operator+( const Color& c );
-	    Image& operator-( const Color& c );
-
-
-
-	    /* FIXME: remove - use convolve instead */
-	    void ddx( Image& dx, bool forward = true ) const;
-	    void ddy( Image& dy, bool forward = true ) const;
-
-	private:
-	    void upateIpl();
-	    float* imageToKernel( const Image& k, bool normalize ) const;
-	    void convolveFloat( Image& dst, const Image& kernel, bool normalize ) const;
-
-	    ImageChannelOrder _order;
-	    ImageChannelType _type;
-	    size_t _width;
-	    size_t _height;
-	    size_t _stride;
-	    uint8_t* _data;
-	    IplImage* _iplimage;
+		Image& operator*( const Color& c );
+		Image& operator+( const Color& c );
+		Image& operator-( const Color& c );
 
 
-	    static size_t _type_size[];
-	    static size_t _order_channels[];
-    };
 
-    std::ostream& operator<<(std::ostream &out, const Image &f);
+		/* FIXME: remove - use convolve instead */
+		void ddx( Image& dx, bool forward = true ) const;
+		void ddy( Image& dy, bool forward = true ) const;
 
-    inline ImageChannelOrder Image::order() const
-    {
-	return _order;
-    }
+		private:
+		void upateIpl();
+		float* imageToKernel( const Image& k, bool normalize ) const;
+		void convolveFloat( Image& dst, const Image& kernel, bool normalize ) const;
 
-    inline ImageChannelType Image::type() const
-    {
-	return _type;
-    }
+		ImageChannelOrder _order;
+		ImageChannelType _type;
+		size_t _width;
+		size_t _height;
+		size_t _stride;
+		uint8_t* _data;
+		IplImage* _iplimage;
 
-    inline size_t Image::width() const
-    {
-	return _width;
-    }
 
-    inline size_t Image::height() const
-    {
-	return _height;
-    }
+		static size_t _type_size[];
+		static size_t _order_channels[];
+	};
 
-    inline size_t Image::stride() const
-    {
-	return _stride;
-    }
+	std::ostream& operator<<(std::ostream &out, const Image &f);
 
-    inline uint8_t* Image::data()
-    {
-	return _data;
-    }
+	inline ImageChannelOrder Image::order() const
+	{
+		return _order;
+	}
 
-    inline uint8_t* Image::scanline( size_t y )
-    {
-	y = Math::min( y, _height - 1 );
-	return _data + _stride * y;
-    }
+	inline ImageChannelType Image::type() const
+	{
+		return _type;
+	}
 
-    inline void Image::reallocate( const Image& i )
-    {
-	reallocate( i._width, i._height, i._order, i._type );
-    }
+	inline size_t Image::width() const
+	{
+		return _width;
+	}
 
-    inline IplImage* Image::iplimage() const
-    {
-	return _iplimage;
-    }
+	inline size_t Image::height() const
+	{
+		return _height;
+	}
 
-    inline Image& Image::operator=( const Image& c )
-    {
-	copy( c );
-	return *this;
-    }
+	inline size_t Image::stride() const
+	{
+		return _stride;
+	}
 
-    inline Image& Image::operator*( float alpha )
-    {
-	mul( alpha );
-	return *this;
-    }
+	inline uint8_t* Image::data()
+	{
+		return _data;
+	}
 
-    inline Image& Image::operator+( float alpha )
-    {
-	add( alpha );
-	return *this;
-    }
+	inline uint8_t* Image::scanline( size_t y )
+	{
+		y = Math::min( y, _height - 1 );
+		return _data + _stride * y;
+	}
 
-    inline Image& Image::operator-( float alpha )
-    {
-	sub( alpha );
-	return *this;
-    }
+	inline void Image::reallocate( const Image& i )
+	{
+		reallocate( i._width, i._height, i._order, i._type );
+	}
 
-    inline Image& Image::operator*( const Color& c )
-    {
-	mul( c );
-	return *this;
-    }
+	inline IplImage* Image::iplimage() const
+	{
+		return _iplimage;
+	}
 
-    inline Image& Image::operator+( const Color& c )
-    {
-	add( c );
-	return *this;
-    }
-    inline Image& Image::operator-( const Color& c )
-    {
-	sub( c );
-	return *this;
-    }
+	inline Image& Image::operator=( const Image& c )
+	{
+		copy( c );
+		return *this;
+	}
+
+	inline Image& Image::operator*( float alpha )
+	{
+		mul( alpha );
+		return *this;
+	}
+
+	inline Image& Image::operator+( float alpha )
+	{
+		add( alpha );
+		return *this;
+	}
+
+	inline Image& Image::operator-( float alpha )
+	{
+		sub( alpha );
+		return *this;
+	}
+
+	inline Image& Image::operator*( const Color& c )
+	{
+		mul( c );
+		return *this;
+	}
+
+	inline Image& Image::operator+( const Color& c )
+	{
+		add( c );
+		return *this;
+	}
+	inline Image& Image::operator-( const Color& c )
+	{
+		sub( c );
+		return *this;
+	}
 }
 
 #endif
