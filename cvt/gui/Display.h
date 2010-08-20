a9c2d3855e548d57889faecff4d780523a6cf3ab
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-08-20 07:14:56 +0000

    Whitespace stuff
    	modified:   Display.cpp
    	modified:   Display.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@275 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gui/Display.h
+++ cvt/gui/Display.h
@@ -8,19 +8,19 @@
 #include <cvt/gfx/Image.h>
 
 namespace cvt {
-    class Display : public QWidget
-    {
-	Q_OBJECT
-    public:
-	Display(QWidget * parent = 0);
-	
-	void addImage(cvt::Image & img);
-	
-    private:
-	QGraphicsScene * scene;
-	QGraphicsView * view;	
-    };
-    
+	class Display : public QWidget
+	{
+		Q_OBJECT
+		public:
+			Display(QWidget * parent = 0);
+
+			void addImage(cvt::Image & img);
+
+		private:
+			QGraphicsScene * scene;
+			QGraphicsView * view;	
+	};
+
 }
 
 #endif
