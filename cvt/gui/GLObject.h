e61af0a475fff2db81b401793252f2716dde7c4a
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-08-24 07:47:09 +0000

    GUI stuff
    	modified:   CMakeLists.txt
    	modified:   gui/GLImage.cpp
    	modified:   gui/GLObject.h
    	modified:   gui/GLView.cpp
    	new file:   gui/signalslib.hpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@291 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gui/GLObject.h
+++ cvt/gui/GLObject.h
@@ -1,13 +1,17 @@
 #ifndef GLOBJECT_H
 #define GLOBJECT_H
 
-#include <cvt/util/Rect.h>
-#include <cvt/gfx/Color.h>
+#include "util/Rect.h"
+#include "gfx/Color.h"
+
 #include <GL/gl.h>
+
+#include "signalslib.hpp"
 
 namespace cvt {
 
 	class GLObject {
+
 		public:
 			GLObject() : _rect( 0, 0, 1, 1 ) , _color( 1.0f, 1.0f ) {};
 			virtual ~GLObject() {};
@@ -17,8 +21,10 @@ namespace cvt {
 			const Recti& rect() const { return _rect; };
 			void setRect( const Recti& r ) { _rect = r; };
 
-			void setSize( int w, int h ) { _rect.setSize( w, h ); };
-			void setPosition( int x, int y ) { _rect.setPosition( x, y ); };
+			void setSize( int w, int h ) { _rect.setSize( w, h ); updated(); };
+			void setPosition( int x, int y ) { _rect.setPosition( x, y ); updated(); };
+
+			boost::signal<void ()>  updated;
 
 		protected:
 			Recti _rect;
