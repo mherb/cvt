3825c0163cdec2d3e781b4a2070c6a92d08fe43f
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-09-30 09:32:38 +0000

    ...
    	new file:   ../tests/openclimg/CMakeLists.txt
    	new file:   ../tests/openclimg/main.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@372 f7bcf5df-4c73-0410-a0d8-b61c104701fe

---
+++ tests/openclimg/main.cpp
@@ -0,0 +1,12 @@
+#include <cvt/cl/CLContext.h>
+#include <cvt/gfx/Image.h>
+#include <iostream>
+
+using namespace cvt;
+
+int main( int argc, char** argv )
+{
+	CLContext cl;
+
+	cl.makeCurrent();
+}
