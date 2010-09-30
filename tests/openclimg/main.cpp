6a5281d13ed4743ada4660d85e893ad6d2a7c116
Philipp Heise <heise@in.tum.de>
2010-09-30 16:45:36 +0000

    Don't use CLImage anymore
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/cl/CLKernel.cpp
    	modified:   ../tests/openclimg/main.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@377 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/openclimg/main.cpp
+++ tests/openclimg/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #include "fill.h"
+#include "gaussiir.h"
 
 using namespace cvt;
 
@@ -18,8 +19,9 @@ int main( int argc, char** argv )
 
 	cl.makeCurrent();
 
-	CLKernel kernel;
+	CLKernel kernel, kerneliir;
 	kernel.build("fill", _fill_source, strlen( _fill_source ), log );
+	kerneliir.build("gaussiir", _gaussiir_source, strlen( _gaussiir_source ), log );
 
 	Image x( 400, 300, IOrder::RGBA, IType::UBYTE, IALLOCATOR_CL );
 
