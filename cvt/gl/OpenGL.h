45e42f975bfdb195dceddc736a59e6bed6cef0c1
Philipp Heise <heise@in.tum.de>
2010-10-20 11:31:10 +0000

    Fix cmake stuff and add basic GL helpers
    ( also removed QT stuff )
    
    	modified:   ../CMakeLists.txt
    	deleted:    ../cmake/CLTOHEADER.cmake
    	new file:   ../cmake/GLCLTOHEADER.cmake
    	modified:   ../cmake/LibraryConfig.cmake
    	modified:   ../cmake/MacroTestApplication.cmake
    	modified:   ../cvt/CMakeLists.txt
    	new file:   ../cvt/gl/GLException.h
    	new file:   ../cvt/gl/GLProgram.cpp
    	new file:   ../cvt/gl/GLProgram.h
    	new file:   ../cvt/gl/OpenGL.h
    	new file:   ../cvt/gl/shader/basic.frag
    	new file:   ../cvt/gl/shader/basic.vert
    	modified:   ../cvt/gui/GFXGL.cpp
    	modified:   ../cvt/gui/GFXGL.h
    	modified:   ../cvt/gui/internal/ApplicationX11.h
    	modified:   ../cvt/gui/internal/X.h
    	modified:   ../tests/gui/CMakeLists.txt
    	renamed:    ../util/cltoheader.cpp -> ../util/glcltoheader.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@400 f7bcf5df-4c73-0410-a0d8-b61c104701fe

---
+++ cvt/gl/OpenGL.h
@@ -0,0 +1,8 @@
+#ifndef CVTGL_H
+#define CVTGL_H
+
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+#endif
---
+++ cvt/gl/shader/basic.vert
@@ -0,0 +1,6 @@
+in vec3 in_Position;   
+
+void main(void)   
+{   
+	gl_Position = vec4( in_Position, 1.0 );   
+}  
