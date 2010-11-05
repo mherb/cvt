375934a1b764d5798569c28116c13b25688b6cf7
Philipp Heise <heise@in.tum.de>
2010-11-05 13:35:35 +0000

    Added GL class
    	modified:   ../cvt/CMakeLists.txt
    	new file:   ../cvt/gl/OpenGL.cpp
    	modified:   ../cvt/gl/OpenGL.h
    	modified:   ../cvt/gui/internal/ApplicationX11.cpp
    	modified:   ../cvt/gui/internal/WidgetImplWinGLX11.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@456 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gl/OpenGL.h
+++ cvt/gl/OpenGL.h
@@ -5,4 +5,29 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace cvt {
+	class GL {
+
+		public:
+			static bool init();
+			static void GLVersion( unsigned int* major, unsigned int* minor );
+			static void GLSLVersion( unsigned int* major, unsigned int* minor );
+			static void info( std::ostream& out );
+
+		private:
+			GL();
+			~GL();
+			static void parseVersion( const char* str, unsigned int* major, unsigned int* minor );
+			static void parseExtensions( const char* str );
+
+		static unsigned int _glmajor, _glminor;
+		static unsigned int _glslmajor, _glslminor;
+		static std::vector<std::string*> _extensions;
+	};
+}
+
 #endif
