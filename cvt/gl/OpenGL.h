f85c08c777a69b295aef820e614ac4fc3a334b92
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-11-05 15:04:16 +0000

    Added GL class GLProcAddress function pointers
    -Vertex Array stuff should now work on APPLE
    
    	modified:   ../cvt/gl/GLVertexArray.cpp
    	modified:   ../cvt/gl/OpenGL.cpp
    	modified:   ../cvt/gl/OpenGL.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@458 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gl/OpenGL.h
+++ cvt/gl/OpenGL.h
@@ -5,9 +5,15 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 
+#ifdef APPLE
+#elif LINUX
+#include <GL/glx.h>
+#endif
+
 #include <iostream>
 #include <string>
 #include <vector>
+
 
 namespace cvt {
 	class GL {
@@ -16,18 +22,25 @@ namespace cvt {
 		public:
 			static void GLVersion( unsigned int* major, unsigned int* minor );
 			static void GLSLVersion( unsigned int* major, unsigned int* minor );
+			static bool existsExtension( const std::string& extname );
+			static void ( *getProcAddress( const char* str ) ) ();
 			static void info( std::ostream& out );
 
+			static void ( *glBindVertexArray )( GLuint array );
+			static void ( *glDeleteVertexArrays )( GLsizei n, const GLuint *arrays );
+			static void ( *glGenVertexArrays )( GLsizei n, GLuint *arrays);
+			static GLboolean ( *glIsVertexArray )( GLuint array);
+
 		private:
-			static bool init();
+			static void init();
 			GL();
 			~GL();
 			static void parseVersion( const char* str, unsigned int* major, unsigned int* minor );
 			static void parseExtensions( const char* str );
 
-		static unsigned int _glmajor, _glminor;
-		static unsigned int _glslmajor, _glslminor;
-		static std::vector<std::string*> _extensions;
+			static unsigned int _glmajor, _glminor;
+			static unsigned int _glslmajor, _glslminor;
+			static std::vector<std::string*> _extensions;
 	};
 }
 
