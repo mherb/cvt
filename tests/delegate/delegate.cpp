48ab41051e3e9b02d61f27870b6c4e3337574469
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-11-09 10:44:14 +0000

    Add delegate test and remove virtual from delegate
    	modified:   ../cvt/util/Delegate.inl
    	modified:   ../tests/delegate/delegate.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@494 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/delegate/delegate.cpp
+++ tests/delegate/delegate.cpp
@@ -1,4 +1,6 @@
 #include <cvt/util/Delegate.h>
+#include <cvt/util/Timer.h>
+
 #include <iostream>
 
 
@@ -6,7 +8,7 @@ using namespace cvt;
 
 class A {
 	public:
-		int bla(  ) { std::cout << "A::bla() " << std::endl; };
+		int bla(  ) { /*std::cout << "A::bla() " << std::endl;*/ };
 		int bla( int x ) { std::cout << "A::bla( int ) " << x << std::endl; };
 		int blo( int x ) const { std::cout << "A::blo " << x << std::endl; };
 		void blim( int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10 ) const { std::cout << "A::blim " << x1 << " " << x10 << std::endl; };
@@ -24,7 +26,7 @@ static int bli( int x )
 
 void bla( )
 {
-	std::cout << "bla " << std::endl;
+	//std::cout << "bla " << std::endl;
 }
 
 
@@ -37,6 +39,7 @@ int main()
 	Delegate<int (int)> d4( &a, &A::blo );
 	Delegate<void ()> d5( &bla );
 	Delegate<void (int, int, int, int, int, int, int, int, int, int )> d6( &a, &A::blim );
+	Delegate<int ()> d7( &a, &A::bla );
 
 	d1( 10 );
 	d2( 20 );
@@ -44,4 +47,18 @@ int main()
 	d4( 40 );
 	d5( );
 	d6( 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10 );
+
+#define CALLMAX 90000000
+
+	Timer t;
+	t.reset();
+	for( size_t i = 0; i < CALLMAX; i++ )
+		a.bla();
+	std::cout << "Time: " << t.elapsedMiliSeconds() << std::endl;
+
+	t.reset();
+	for( size_t i = 0; i < CALLMAX; i++ )
+		d7();
+	std::cout << "Time: " << t.elapsedMiliSeconds() << std::endl;
+
 }
