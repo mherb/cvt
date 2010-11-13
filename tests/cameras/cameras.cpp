4979fdd9e02b75af5c283bf7bdae6e0defe0e562
kloses <kloses@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-11-13 16:59:17 +0000

    - ask user to select a camera - show image with opencv ... - now: showering, shopping, sportschau, coden ...
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@519 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/cameras/cameras.cpp
+++ tests/cameras/cameras.cpp
@@ -1,4 +1,9 @@
 #include <cvt/io/Camera.h>
+#include <cvt/util/Timer.h>
+#include <cvt/util/Exception.h>
+
+#include <cv.h>
+#include <highgui.h>
 
 using namespace cvt;
 
@@ -13,9 +18,54 @@ int main( int argc, char* argv[] )
 	for( size_t i = 0; i < numCams; i++ ){
 		const CameraInfo & info = Camera::info( i );
 		
-		std::cout << "Camera " << i << std::endl;
-		std::cout << "Name: " << info.name() << std::endl;
+		std::cout << "Camera " << i << ": " << info.name() << std::endl;
 	}
+	
+	
+	size_t selection = numCams;
+	std::cout << "Select camera: ";	
+	std::cin >> selection;
+	while ( selection >= numCams ){
+		std::cout << "Index out of bounds -> select camera in Range:";
+		std::cin >> selection;	
+	}
+
+
+	int key;
+	size_t frames = 0;
+	Timer timer;
+	Camera * cam = 0;
+	
+	try {
+		cam = Camera::get( selection, 800, 600 );			
+		
+		timer.reset();
+		while( 1 ) {
+			cam->nextFrame();
+			const Image & frame = cam->frame();
+			
+			size_t stride;
+			const uint8_t * data = frame.map( &stride );
+			cv::Mat ocvImage( frame.height(), frame.width(), CV_8UC4, (void *)data, stride );
+			cv::imshow( "frame", ocvImage );
+			frame.unmap( data );
+			
+			key = cvWaitKey( 3 ) & 0xff;
+			if( key == 27 )
+				break;
+			
+			frames++;
+			if( timer.elapsedSeconds() > 5.0f ) {
+				std::cout << "FPS: " << ( double ) frames / timer.elapsedSeconds() << std::endl;
+				frames = 0;
+				timer.reset();
+			}			
+		}		
+	} catch( cvt::Exception e ) {
+		std::cout << e.what() << std::endl;
+	} 
+	if(cam)
+		delete cam;
 	
 	return 0;
 }
