83e0bfd33eade5007f3617795614359398281635
Sebastian Klose <kloses@in.tum.de>
2010-11-29 14:06:21 +0000

    - UEye now works with better buffer handling - We only support retrieving the Bayer Image
    M    io/UEyeUsbCamera.h
    M    io/Camera.cpp
    M    io/UEyeUsbCamera.cpp
    
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@617 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/io/UEyeUsbCamera.h
+++ cvt/io/UEyeUsbCamera.h
@@ -2,7 +2,6 @@
 #define TOOLS_MUEYEUSBCAMERA_H
 
 #include <cvt/io/Camera.h>
-
 #include <ueye.h>
 
 namespace cvt
@@ -14,36 +13,45 @@ class UEyeUsbCamera : public Camera
             UEyeUsbCamera( size_t camIndex, const CameraMode& mode );
             virtual ~UEyeUsbCamera();
 
-            virtual void startCapture();
-            virtual void stopCapture();
-			virtual void nextFrame();
-            virtual const Image & frame() const;
+            void startCapture();
+            void stopCapture();
+			void nextFrame();
+            const Image & frame() const;
 
             static size_t count();
 			static void cameraInfo( size_t index, CameraInfo & info );
+			size_t width() const;
+			size_t height() const;
+			const IFormat & format() const;
         private:
             bool initCam();
             void open( const CameraMode & mode );
-			virtual void setAutoGain( bool value );
-            virtual void setAutoShutter( bool value );
-            virtual void setAutoSensorShutter( bool value );
-            virtual void setAutoWhiteBalance( bool value );
-            virtual void setMaxAutoShutter( double value );
-            virtual void setExposureTime( double value );
-            virtual void setPixelClock( unsigned int value );
-            virtual void setHorizontalMirror( bool value );
-            virtual void setVerticalMirror( bool value );
+			void initMemories( const CameraMode & mode );
+			void freeMemories();
+			void setAutoGain( bool value );
+            void setAutoShutter( bool value );
+            void setAutoSensorShutter( bool value );
+            void setAutoWhiteBalance( bool value );
+			void setWhiteBalanceOnce();
+            void setMaxAutoShutter( double value );
+            void setExposureTime( double value );
+            void setPixelClock( unsigned int value );
+            void setHorizontalMirror( bool value );
+            void setVerticalMirror( bool value );
+            void setFramerate( double fps );
 
             int _camIndex;
 
             // uEye variables
-            HIDS	_camHandle;		// handle to camera
-            INT		_width;			// width of video
-            INT		_height;		// height of video
-            INT		_bufferID;		// grabber memory - buffer ID
-            char*	_bufferPtr;		// grabber memory - pointer to buffer
+            HIDS		_camHandle;		// handle to camera
+            INT			_width;			// width of video
+            INT			_height;		// height of video
 
-            Image _frame;
+			static const size_t	_numImageBuffers = 4;
+			int8_t*				_buffers[ _numImageBuffers ];
+			INT					_bufferIds[ _numImageBuffers ];
+
+			Image _frame;
     };
 
 	inline const Image & UEyeUsbCamera::frame() const
@@ -51,5 +59,19 @@ class UEyeUsbCamera : public Camera
 		return _frame;
 	}
 
+	inline size_t UEyeUsbCamera::width() const
+	{
+		return _frame.width();
+	}
+
+	inline size_t UEyeUsbCamera::height() const
+	{
+		return _frame.height();
+	}
+
+	inline const IFormat & UEyeUsbCamera::format() const
+	{
+		return _frame.format();
+	}
 }
 #endif
