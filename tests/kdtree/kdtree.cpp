6b352afb032e0a0f56d0483d3571b86dabc27a4d
kloses <kloses@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-12-11 13:06:12 +0000

    - implementation more like in intros to algos (more recursion also for select) - seems to work now, maybe now make it iteratively again instead of recursion
    M    kdtree/kdtree.cpp
    
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@656 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/kdtree/kdtree.cpp
+++ tests/kdtree/kdtree.cpp
@@ -17,8 +17,14 @@ class KDTree {
 		KDTree( Point2f* pts, size_t npts );
 		~KDTree();
 		void locate( Point2f* pt, float dist );
+
 	private:
 		void medsort( uint32_t l, uint32_t h, int idx );
+		uint32_t partition( uint32_t l, uint32_t h, int idx );
+
+		/* select the x-th element according to dimension idx */
+		void select(  uint32_t l, uint32_t h, uint32_t x, int idx );
+		void print();
 
 		Point2f* _pts;
 		uint32_t* _ptidx;
@@ -33,14 +39,10 @@ KDTree::KDTree( Point2f* pts, size_t npts )
 
 	for( size_t i = 0; i < _npts; i++ )
 		_ptidx[ i ] = i;
-	medsort( 0, _npts - 1, 0 );
 
-	{
-		int idx = 0;
-		for( int i = 0; i < _npts; i++ )
-			std::cout << PT( i ) << " ";
-		std::cout << std::endl;
-	}
+	medsort( 0, _npts - 1, 0 );
+	//select( 0, _npts - 1, _npts << 1, 0 );
+	print();
 }
 
 KDTree::~KDTree()
@@ -48,59 +50,82 @@ KDTree::~KDTree()
 	delete[] _ptidx;
 }
 
+void KDTree::print()
+{
+	int idx = 0;
+	for( uint32_t i = 0; i < _npts; i++ )
+		std::cout << PT( i ) << " ";
+	idx ^= 0x01;
+
+	std::cout << std::endl;
+
+	for( uint32_t i = 0; i < _npts; i++ )
+		std::cout << PT( i ) << " ";
+	std::cout << std::endl;
+}
+
 void KDTree::medsort( uint32_t l, uint32_t h, int idx )
 {
-	uint32_t ll = l;
-	uint32_t hh = h;
 	uint32_t med = ( h + l ) >> 1;
 
-	std::cout << "medsort" << l << " < - > " << h << std::endl;
-	if( h == l || l == h - 1 )
+	std::cout << "medsort(" << med <<") " << l << " < - > " << h << " index: " << idx << std::endl;
+
+	// partition
+	select( l, h, med, idx );
+
+	if( med > l + 2 )
+		medsort( l, med - 1, idx ^ 0x01 );
+	if( med < h - 2 )
+		medsort( med + 1, h, idx ^ 0x01 );
+}
+
+
+void KDTree::select(  uint32_t l, uint32_t h, uint32_t x, int idx )
+{
+	if( l == h ) return;
+
+	uint32_t i = partition( l, h, idx );
+	uint32_t k = i - l + 1;
+
+	if( x == k )
 		return;
+	else if( x < k )
+		select( l, i - 1, x, idx );
+	else
+		select( i + 1, h, x - k, idx );
+}
 
-	while( 1 ) {
-		if( hh  <= ll + 1 ) {
-			medsort( l, med - 1, idx ^ 0x01 );
-			medsort( med + 1, h, idx ^ 0x01 );
-			return;
-		} else {
-			uint32_t mid = ( hh + ll ) >> 1;
-			if( PT( ll ) > PT( mid ) )
-				SWAP( ll, mid  );
-			if( PT( mid ) > PT( hh ) )
-				SWAP( mid, hh );
-			if( PT( ll ) > PT( mid ) )
-				SWAP( ll, mid  );
-			SWAP( mid, hh );
+uint32_t KDTree::partition( uint32_t l, uint32_t h, int idx )
+{
+	// median of 3
+	uint32_t mid = ( l + h ) >> 1;
+	if( PT( l ) > PT( mid ) )
+		SWAP( l, mid  );
+	if( PT( mid ) > PT( h ) )
+		SWAP( mid, h );
+	if( PT( l ) > PT( mid ) )
+		SWAP( l, mid  );
+	SWAP( mid, h );
 
-			uint32_t i, k;
-			uint32_t p = hh;
-			hh--;
-
-			i = hh;
-			k = ll;
-			std::cout << PT( ll ) << " " << PT( mid ) << " " << PT( hh + 1 ) << std::endl;
-
-			while( 1 ) {
-				while( PT( i ) > PT( p ) ) i--;
-				while( PT( k ) < PT( p ) ) k++;
-				if( i <= k )
-					break;
-				SWAP( i, k );
-			}
-
-			SWAP( k, p );
-			if( i >= med  ) { hh = i; }
-			else { ll = k; h++; }
+	// i defines is the partitioning index
+	int32_t i = l -1;
+	for( uint32_t j = l ; j < h; j++ ){
+		if( PT( j ) <= PT( h ) ){
+			i++;
+			SWAP( i, j );
 		}
 	}
+	// MOVE our partitioning element back to the partitioning position
+	i++;
+	SWAP( i, h );
+	return i;
 }
 
 
 
 int main()
 {
-	Point2f pts[ 11 ];
+	Point2f pts[ 10 ];
 
 	for( int i = 0; i < 10; i++ )
 		pts[ i ].set( Math::rand( 0.0f, 10.0f ), Math::rand( 0.0f, 10.0f ) );
