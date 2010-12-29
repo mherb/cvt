c64a6f1a6522fafb2736a79e7555920b3e4e78ea
Philipp Heise <heise@in.tum.de>
2010-12-29 16:24:19 +0000

    ...
    	modified:   ../cvt/geom/PointSet.h
    	modified:   ../tests/ptset/ptset.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@719 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/ptset/ptset.cpp
+++ tests/ptset/ptset.cpp
@@ -6,9 +6,17 @@ using namespace cvt;
 int main()
 {
 	PointSet3f ptset[ 2 ];
+
 	Vector3f x( 1.0f, 0.0f, 0.0f );
 	ptset[ 0 ].add( x );
 	ptset[ 1 ].add( x );
+	ptset[ 0 ].add( Vector3f( 0.0f, 1.0f, 0.0f ) );
+
+
+	ptset[ 0 ].scale( 2.0f );
+	std::cout << ptset[ 0 ] << std::endl;
+	std::cout << ptset[ 0 ].mean() << std::endl;
+
 	Matrix4f mat;
 	mat = ptset[ 0 ].alignSimilarity( ptset[ 1 ] );
 	std::cout << mat << std::endl;
