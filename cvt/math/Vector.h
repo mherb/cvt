df8e4d4c494f27d5b6d59878d30b5fb7d4f3d6ea
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-12-29 15:51:28 +0000

    Add template to select matrix by type and dimension
    	new file:   ../cvt/geom/PointSet.h
    	modified:   ../cvt/math/Matrix.h
    	modified:   ../cvt/math/Matrix2.h
    	modified:   ../cvt/math/Matrix3.h
    	modified:   ../cvt/math/Matrix4.h
    	modified:   ../cvt/math/Vector.h
    	modified:   ../tests/CMakeLists.txt
    	new file:   ../tests/ptset/CMakeLists.txt
    	new file:   ../tests/ptset/ptset.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@718 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/math/Vector.h
+++ cvt/math/Vector.h
@@ -11,6 +11,27 @@
 namespace cvt {
 	typedef Vector2f Point2f;
 	typedef Vector3f Point3f;
+
+	template<int dim, typename T>
+		struct Vector {
+			typedef T TYPE;
+		};
+
+	template<typename T>
+		struct Vector<2, T> {
+			typedef Vector2<T> TYPE;
+		};
+
+	template<typename T>
+		struct Vector<3, T> {
+			typedef Vector3<T> TYPE;
+		};
+
+	template<typename T>
+		struct Vector<4, T> {
+			typedef Vector4<T> TYPE;
+		};
+
 }
 
 #endif
