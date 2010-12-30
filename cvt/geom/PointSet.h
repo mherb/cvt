a3219239dbc81a21cf8d0766206dd87ed0b6d475
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2010-12-30 13:34:20 +0000

    Add generic transform to PointSet
    	modified:   ../cvt/geom/PointSet.h
    	modified:   ../cvt/math/Matrix2.h
    	modified:   ../tests/ptset/ptset.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@721 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/geom/PointSet.h
+++ cvt/geom/PointSet.h
@@ -18,6 +18,7 @@ namespace cvt
 		class PointSet
 		{
 			typedef typename Vector<dim,_T>::TYPE PTTYPE;
+			typedef typename Matrix<dim,_T>::TYPE MATTYPENT;
 			typedef typename Matrix<dim + 1,_T>::TYPE MATTYPE;
 
 			public:
@@ -32,6 +33,8 @@ namespace cvt
 				size_t size() const;
 				void translate( const PTTYPE& t );
 				void scale( _T t );
+				void transform( const MATTYPENT& mat );
+				void transform( const MATTYPE& mat );
 				MATTYPE alignSimilarity( const PointSet<dim,_T>& ptset );
 
 			private:
@@ -95,7 +98,7 @@ namespace cvt
 
 
 	template<int dim, typename _T>
-		void PointSet<dim,_T>::translate( const PTTYPE& t )
+		inline void PointSet<dim,_T>::translate( const PTTYPE& t )
 		{
 			PTTYPE* pt = &_pts[ 0 ];
 			size_t n = _pts.size();
@@ -104,12 +107,115 @@ namespace cvt
 		}
 
 	template<int dim, typename _T>
-		void PointSet<dim,_T>::scale( _T s )
+		inline void PointSet<dim,_T>::scale( _T s )
 		{
 			PTTYPE* pt = &_pts[ 0 ];
 			size_t n = _pts.size();
 			while( n-- )
 				*pt++ *= s;
+		}
+
+	template<int dim, typename _T>
+		inline void PointSet<dim,_T>::transform( const MATTYPENT& mat )
+		{
+			PTTYPE* pt = &_pts[ 0 ];
+			size_t n = _pts.size();
+			while( n-- ) {
+				*pt = mat * *pt;
+				pt++;
+			}
+		}
+
+	template<>
+		inline void PointSet<2,float>::transform( const MATTYPE& mat )
+		{
+			PTTYPE* pt = &_pts[ 0 ];
+			size_t n = _pts.size();
+
+			/* if last row is [ 0 0 1 ], split mat into 2 x 2 matrix and translation */
+			if( mat[ 2 ] == Vector3f( 0, 0, 1.0f ) ) {
+				Matrix2f _mat( mat );
+				Vector2f trans( mat[ 0 ][ 2 ], mat[ 1 ][ 2 ] );
+				while( n-- ) {
+					*pt = _mat * *pt;
+					*pt += trans;
+					pt++;
+				}
+			} else {
+				while( n-- ) {
+					*pt = mat * *pt;
+					pt++;
+				}
+			}
+		}
+
+	template<>
+		inline void PointSet<2,double>::transform( const MATTYPE& mat )
+		{
+			PTTYPE* pt = &_pts[ 0 ];
+			size_t n = _pts.size();
+
+			/* if last row is [ 0 0 1 ], split mat into 2 x 2 matrix and translation */
+			if( mat[ 2 ] == Vector3d( 0, 0, 1.0 ) ) {
+				Matrix2d _mat( mat );
+				Vector2d trans( mat[ 0 ][ 2 ], mat[ 1 ][ 2 ] );
+				while( n-- ) {
+					*pt = _mat * *pt;
+					*pt += trans;
+					pt++;
+				}
+			} else {
+				while( n-- ) {
+					*pt = mat * *pt;
+					pt++;
+				}
+			}
+		}
+
+	template<>
+		inline void PointSet<3,float>::transform( const MATTYPE& mat )
+		{
+			PTTYPE* pt = &_pts[ 0 ];
+			size_t n = _pts.size();
+
+			/* if last row is [ 0 0 0 1 ], split mat into 3 x 3 matrix and translation */
+			if( mat[ 3 ] == Vector4f( 0, 0, 0, 1 ) ) {
+				Matrix3f _mat( mat );
+				Vector3f trans( mat[ 0 ][ 3 ], mat[ 1 ][ 3 ], mat[ 2 ][ 3 ] );
+				while( n-- ) {
+					*pt = _mat * *pt;
+					*pt += trans;
+					pt++;
+				}
+			} else {
+				while( n-- ) {
+					*pt = mat * *pt;
+					pt++;
+				}
+			}
+		}
+
+	template<>
+		inline void PointSet<3,double>::transform( const MATTYPE& mat )
+		{
+			PTTYPE* pt = &_pts[ 0 ];
+			size_t n = _pts.size();
+
+			/* if last row is [ 0 0 0 1 ], split mat into 3 x 3 matrix and translation */
+			if( mat[ 3 ] == Vector4d( 0, 0, 0, 1 ) ) {
+				Matrix3d _mat( mat );
+				Vector3d trans( mat[ 0 ][ 3 ], mat[ 1 ][ 3 ], mat[ 2 ][ 3 ] );
+				while( n-- ) {
+					*pt = _mat * *pt;
+					*pt += trans;
+					pt++;
+				}
+			} else {
+				while( n-- ) {
+					*pt = mat * *pt;
+					pt++;
+				}
+			}
 		}
 
 	template<int dim, typename _T>
--- tests/ptset/ptset.cpp
+++ tests/ptset/ptset.cpp
@@ -5,19 +5,20 @@ using namespace cvt;
 
 int main()
 {
-	PointSet3f ptset[ 2 ];
+	PointSet3f ptset;
 
-	Vector3f x( 1.0f, 0.0f, 0.0f );
-	ptset[ 0 ].add( x );
-	ptset[ 1 ].add( x );
-	ptset[ 0 ].add( Vector3f( 0.0f, 1.0f, 0.0f ) );
+	ptset.add( Vector3f( 0.0f, 1.0f, 0.0f ) );
+	ptset.add( Vector3f( 1.0f, 0.0f, 0.0f ) );
+	ptset.add( Vector3f( 0.0f, 0.0f, 1.0f ) );
 
+	std::cout << "Initial " << ptset << std::endl;
+	ptset.scale( 3.0f );
+	std::cout << "After scale: " << ptset << std::endl;
 
-	ptset[ 0 ].scale( 2.0f );
-	std::cout << ptset[ 0 ] << std::endl;
-	std::cout << ptset[ 0 ].mean() << std::endl;
-
-	Matrix4f mat;
-	mat = ptset[ 0 ].alignSimilarity( ptset[ 1 ] );
-	std::cout << mat << std::endl;
+	Matrix4f m;
+	m.identity();
+	m[ 1 ][ 3 ] = 1.0f;
+	ptset.transform( m );
+	std::cout << "After transform: " << ptset << std::endl;
+	std::cout << "Mean: " << ptset.mean() << std::endl;
 }
