ecda648f5d0a0829e22d0ddb72dc58666d0fdc03
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2011-01-03 20:49:53 +0000

    Hrm - works but not really well
    	modified:   ../cvt/geom/PointSet.h
    	modified:   ../cvt/math/Matrix.cpp
    	modified:   ../tests/ptset/ptset.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@726 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/ptset/ptset.cpp
+++ tests/ptset/ptset.cpp
@@ -15,18 +15,21 @@ int main()
 
 	Matrix4f m, ms;
 	m.identity();
-//	m[ 0 ][ 3 ] = 3.0f;
-//	m[ 1 ][ 3 ] = 5.0f;
-//	m[ 2 ][ 3 ] = 2.0f;
+//	m *= 2.5f;
+//	m[ 3 ][ 3 ] = 1.0f;
+	m[ 0 ][ 3 ] = 3.0f;
+	m[ 1 ][ 3 ] = 5.0f;
+	m[ 2 ][ 3 ] = -2.0f;
 
 	Quaternionf qrot( 1.0f, 0.0f, 0.0f, 0.5f );
-//	m *= qrot.toMatrix4();
+	m = qrot.toMatrix4();
 
 	PointSet3f ptset2( ptset );
 	ptset2.transform( m );
 
+	std::cout << "Transformed " << ptset2 << std::endl;
+
 	std::cout << "Transform\n" << m << std::endl;
 	ms = ptset.alignSimilarity( ptset2 );
 	std::cout << "Transform estimated\n" << ( ms) << std::endl;
-	std::cout << "Transformed both\n" << ( ms * m ) << std::endl;
 }
