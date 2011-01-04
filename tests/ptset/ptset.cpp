3f37ed4e629903a4ea9971034b505ae3d3876cae
Philipp Heise <heise@in.tum.de>
2011-01-04 16:57:06 +0000

    ...
    	modified:   ../cvt/geom/PointSet.h
    	modified:   ../tests/ptset/ptset.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@730 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/ptset/ptset.cpp
+++ tests/ptset/ptset.cpp
@@ -10,21 +10,20 @@ int main()
 
 	srand( time( NULL ) );
 
-	for( int i = 0; i < 50; i++ )
-		ptset.add( Vector3f( Math::rand( -1.0f, 1.0f), Math::rand( -1.0f, 1.0f), Math::rand( -1.0f, 1.0f) ) );
+	for( int i = 0; i < 10; i++ )
+		ptset.add( Vector3f( Math::rand( -100.0f, 100.0f), Math::rand( -100.0f, 100.0f), Math::rand( -100.0f, 100.0f) ) );
 
 	std::cout << "Initial " << ptset << std::endl;
 
 	Matrix4f m, ms;
 	m.identity();
-//	m *= 2.5f;
 //	m[ 3 ][ 3 ] = 1.0f;
 	m[ 0 ][ 3 ] = 3.0f;
 	m[ 1 ][ 3 ] = 5.0f;
 	m[ 2 ][ 3 ] = -2.0f;
 
 	Quaternionf qrot( 1.0f, 0.0f, 0.0f, Math::rand( -1.0f, 1.0f ) );
-	m = qrot.toMatrix4();
+	m *= qrot.toMatrix4();
 
 	PointSet3f ptset2( ptset );
 	ptset2.transform( m );
@@ -36,4 +35,7 @@ int main()
 	std::cout << "Transform estimated\n" << ( ms) << std::endl;
 
 	std::cout << "Diff\n" << (m - ms) << std::endl;
+
+	ptset.transform( ms );
+	std::cout << "Transformed " << ptset << std::endl;
 }
