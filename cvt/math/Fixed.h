21220a5e5580240df05ff4f3705d57dcd03221ab
kloses <kloses@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2011-01-10 16:58:10 +0000

    - round() in Math - Fixed point class
    M    math/Math.h
    A    math/Fixed.cpp
    A    math/Fixed.h
    
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@747 f7bcf5df-4c73-0410-a0d8-b61c104701fe

---
+++ cvt/math/Fixed.h
@@ -0,0 +1,233 @@
+#ifndef CVT_FIXED_H
+#define CVT_FIXED_H
+
+#include <cvt/math/Math.h>
+
+namespace cvt
+{
+	/* fixed point format class (16x16) */	
+	class Fixed {		
+		public:
+			Fixed();
+			Fixed( int16_t i );			
+			Fixed( float f );
+			Fixed( const Fixed & other );
+						
+			float 	toFloat() const;
+			int		round()	const;
+			int		frac() const;
+			int		floor() const;			
+			int		ceil() const;
+			Fixed	abs() const;
+			Fixed	sqrt() const;
+			Fixed	recip() const;
+			
+			const Fixed & operator=( const Fixed & other );
+			
+			bool operator==( const Fixed & other ) const;
+			bool operator!=( const Fixed & other ) const;
+			
+			/* to allow cast float f = (float)fixed; */
+			operator float() const;
+			
+			Fixed operator+( Fixed other ) const;
+			Fixed operator-( Fixed other ) const;
+			Fixed operator*( Fixed other ) const;
+			Fixed operator/( Fixed other ) const;
+			Fixed operator+=( Fixed other );
+			Fixed operator-=( Fixed other );
+			Fixed operator*=( Fixed other );
+			Fixed operator/=( Fixed other );
+
+			void  sin_cos( Fixed* sinOutput, Fixed* cosOutput ) const;
+			Fixed sin() const;
+			Fixed cos() const;		
+		
+		private:
+			static const uint8_t 	_fixFracShift = 16;
+			static const uint32_t 	_fixFracMask = ( 1 << ( _fixFracShift + 1 ) ) - 1;
+			static const uint32_t 	_fixedMax = ( 1 << 31 );
+			int32_t		_val;			
+	};
+	
+	inline Fixed::Fixed() : _val( 0 )
+	{		
+	}
+	
+	inline Fixed::Fixed( int16_t i )
+	{
+		_val = (int32_t) ( i << _fixFracShift );
+	}
+	
+	inline Fixed::Fixed( float f )
+	{		
+		_val = (int32_t) Math::round( ( f * (float) ( 1 << _fixFracShift ) ) );		
+	}
+	
+	inline Fixed::Fixed( const Fixed & other ) : _val( other._val )
+	{		
+	}
+	
+	inline float Fixed::toFloat() const
+	{
+		return ( (float) _val ) / (float) ( 1 << _fixFracShift );
+	}
+	
+	inline int Fixed::round() const
+	{
+		return ( ( _val + ( 1 << ( _fixFracShift - 1 ) ) ) >> _fixFracShift );
+	}
+	
+	inline int Fixed::frac() const
+	{
+		return _val & _fixFracMask;
+	}
+	
+	inline int Fixed::floor() const
+	{
+		return _val >= 0 ? ( ( _val ) >> _fixFracShift ) : ( -( ( -_val - 1 ) >> _fixFracShift ) - 1 );
+	}
+	
+	inline int Fixed::ceil() const
+	{
+		return _val >= 0 ? ( ( _val - 1 ) >> _fixFracShift ) + 1 : ( -( ( -_val ) >> _fixFracShift ) );
+	}
+	
+	inline Fixed Fixed::abs() const
+	{
+		Fixed ret = *this;
+		
+		if( _val < 0 )
+			ret._val = -ret._val;
+		
+		return ret;
+	}
+	
+	inline Fixed::operator float() const
+	{
+		return toFloat();
+	}
+	
+	inline const Fixed & Fixed::operator=( const Fixed & other )
+	{
+		_val = other._val;
+		return *this;		
+	}
+
+	inline Fixed Fixed::operator+( Fixed other ) const
+	{		
+		other._val += _val;
+		return other;				
+	}
+	
+	inline Fixed Fixed::operator-( Fixed other ) const
+	{
+		Fixed r = *this;
+		r._val -= other._val;
+		return r;
+	}
+	
+	inline Fixed Fixed::operator*( Fixed other ) const
+	{
+		int s = 1;
+	    
+	
+		int32_t a = _val;
+		
+	    if( a < 0 ) { a = -a; s = -1; }
+	    if( other._val < 0 ) { other._val = -other._val; s = -s; }
+		
+		int32_t c;
+	    c = ( ( (int64_t) a * other._val + 0x8000 ) >> 16 );
+	
+		Fixed ret;
+		ret._val = ( s > 0 ) ? c : -c;
+	
+	    return ret;
+	}
+	
+	inline Fixed Fixed::operator/( Fixed other ) const
+	{
+		int s = 1;
+
+		int32_t a = _val;
+
+	    if( a < 0 ) { a = -a; s = -1; }
+	    if( other._val < 0 ) { other._val = -other._val; s = -s; }
+
+	    uint32_t q = ( ( ( (int64_t) a << 16 ) + ( other._val >> 1 ) ) / other._val );
+		Fixed ret;
+		ret._val = ( s < 0 ? -q : q );
+	    return ret;
+	}
+	
+	inline Fixed Fixed::operator+=( Fixed other )
+	{
+		_val += other._val;
+		return *this;
+	}
+	
+	inline Fixed Fixed::operator-=( Fixed other )
+	{
+		_val -= other._val;
+		return *this;
+	}
+	
+	inline Fixed Fixed::operator*=( Fixed other )
+	{
+		return *this * other;
+	}
+	
+	inline Fixed Fixed::operator/=( Fixed other )
+	{
+		return *this / other;
+	}
+	
+	inline Fixed Fixed::recip() const
+	{	
+		Fixed ret( (int16_t) 1 );
+		return 	ret / *this;
+	}
+
+	inline Fixed Fixed::sqrt() const
+	{
+		uint32_t m, y, b;
+	    int32_t a, t;
+		
+		a = _val;
+	    if( a < 0 ) a = -a;
+
+	    m = 0x40000000;
+	    y = 0;
+	    while( m != 0 ) {
+			b = y | m;
+			y = y >> 1;
+			t = (int32_t) ( a | ~( a - b ) ) >> 31;
+			a = a - ( b & t );
+			y = y | ( m & t );
+			m = m >> 2;
+	    }
+		Fixed ret;
+		ret._val = ( y << 8 );
+	    return ret;
+	}
+
+	inline bool Fixed::operator==( const Fixed & other ) const
+	{
+		return _val == other._val;
+	}
+	
+	inline bool Fixed::operator!=( const Fixed & other ) const
+	{
+		return _val != other._val;
+	}
+	
+	inline std::ostream & operator<<( std::ostream & out, const Fixed& f )
+	{
+		out << (float)f << "_fxd";
+		return out;
+	}
+
+}
+
+#endif
