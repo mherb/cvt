3d6f13c6974354a067f8a6a75b8878c789cc53fc
heise <heise@f7bcf5df-4c73-0410-a0d8-b61c104701fe>
2011-01-18 16:01:03 +0000

    Added Flags class template
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/gfx/IFilter.h
    	new file:   ../cvt/util/Flags.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@794 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gfx/IFilter.h
+++ cvt/gfx/IFilter.h
@@ -3,14 +3,17 @@
 
 #include <stdint.h>
 #include <string>
+#include <cvt/util/Flags.h>
 #include <cvt/util/ParamSet.h>
 
 namespace cvt {
-	enum IFilterType {
+	enum IFilterTypeFlags {
 					   IFILTER_CPU = ( 1 << 0 ),
 					   IFILTER_OPENGL = ( 1 << 1 ),
 					   IFILTER_OPENCL = ( 1 << 2 )
 					 };
+
+	typedef Flags<IFilterTypeFlags> IFilterType;
 
 	class IFilter {
 		public:
@@ -20,14 +23,14 @@ namespace cvt {
 			const std::string & name() const { return _name; };
 
 		protected:
-			IFilter( std::string name, ParamInfo** pinfo, size_t pinfosize, uint32_t ifiltertype ) : _iftype( ifiltertype ), _name( name ), _pinfo( pinfo ), _pinfosize( pinfosize ) {};
+			IFilter( std::string name, ParamInfo** pinfo, size_t pinfosize, IFilterType ifiltertype ) : _iftype( ifiltertype ), _name( name ), _pinfo( pinfo ), _pinfosize( pinfosize ) {};
 			virtual ~IFilter() {};
 
 		private:
 			IFilter( const IFilter& ifilter );
 			IFilter& operator=( const IFilter& ifilter );
 
-			uint32_t _iftype;
+			IFilterType _iftype;
 			std::string _name;
 			ParamInfo** _pinfo;
 			size_t _pinfosize;
