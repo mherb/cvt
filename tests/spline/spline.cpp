51b52039e781dcd8cceaca4204720e5c6a9e4c8d
Philipp Heise <heise@in.tum.de>
2011-01-30 20:27:19 +0000

    Spline
    	modified:   math/Spline2.h
    	modified:   ../tests/spline/spline.cpp
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@859 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/spline/spline.cpp
+++ tests/spline/spline.cpp
@@ -29,9 +29,21 @@ int main()
  std::cout << n << " : " << t[ 0 ] << " " << t[ 1 ] << std::endl;
  */
 
-#define DIST 5.0f
+ for( float t = 0; t <= 1.0f; t += 0.01f ) {
+	 spl.samplePoint( norm,  t  );
+	 spl.samplePoint( pt, t + 0.0001f );
+	 pt -= norm;
+	 pt.normalize();
+	 spl.sampleDerivative( norm, t );
+	 norm.normalize();
+	 float dot =   -pt.x * norm.y + pt.y * norm.x;
+	 std::cerr << ( pt - norm ) << std::endl;
+	 std::cerr << dot << std::endl;
+ }
 
-    spl.samplePoint( pt, 0.0f );
+
+#define DIST 1.0f
+	spl.samplePoint( pt, 0.0f );
 	spl.sampleDerivative( norm, 0.0f );
 	norm.normalize();
     pt.x += norm.y * DIST;
@@ -39,20 +51,23 @@ int main()
 	std::cout << pt.x << " " << pt.y << std::endl;
 
     do {
-		t = spl.flattenOffset( DIST, 0.01f );
 		spl.samplePoint( pt, 0.0f );
 		spl.sampleDerivative( norm, 0.0f );
 		norm.normalize();
 	    pt.x += norm.y * DIST;
 		pt.y += -norm.x * DIST;
 		std::cout << pt.x << " " << pt.y << std::endl;
+		t = spl.flattenOffset( DIST, 0.025f );
 		if( t < 1e-2f)
 			t = 1e-2f;
-        spl.split( spl1, spl2, t );
-		spl = spl2;
+		spl.remove( t );
+  //      spl.split( spl1, spl2, t );
+//		spl = spl2;
     } while(t < 1.0f);
 
 	spl.set( 0.0f, 0.0f, 50.0f, 20.0f, -10.0f, 20.0f, 40.0f, 0.0f );
+
+
 	spl.samplePoint( pt, 1.0f );
 	spl.sampleDerivative( norm, 1.0f );
 	norm.normalize();
