afcbc44332252fa0f7bc793176339aae8c1f91d5
Philipp Heise <heise@in.tum.de>
2011-02-14 14:49:41 +0000

    Convolution gray
    	modified:   ../cvt/gfx/ImageOperations.cpp
    	modified:   ../cvt/util/SIMDSSE2.cpp
    	modified:   ../cvt/util/SIMDSSE2.h
    	modified:   ../tests/convolve/convolve.cpp
    	modified:   ../tests/speedTests/ImageProcessing.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@934 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- tests/convolve/convolve.cpp
+++ tests/convolve/convolve.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
 {
 	std::string dataFolder(DATA_FOLDER);
 	std::string inputFile(dataFolder + "/lena.png");
+	std::string inputFileg(dataFolder + "/lena_g.png");
 	
 	try {
 		// RGBA UBYTE IMAGE
@@ -21,6 +22,7 @@ int main(int argc, char* argv[])
 		cvt::ImageIO::loadPNG(img, inputFile);
 		
 		out.reallocate( img );
+		out.fill( cvt::Color( 0.0f,0.0f,0.0f,1.0f) );
 
 		cvt::IKernel kern( 3, 3 );
 		for( int x = 0; x < 3; x++ )
@@ -32,6 +34,22 @@ int main(int argc, char* argv[])
 		out.fill( cvt::Color( 0.0f,0.0f,0.0f,1.0f) );
 		img.convolve( out, cvt::IKernel::MEAN_HORIZONTAL_3, cvt::IKernel::MEAN_VERTICAL_3);
 		cvt::ImageIO::savePNG(out, "convolve2.png");
+
+		cvt::ImageIO::loadPNG(img, inputFileg);
+		
+		out.reallocate( img );
+		out.fill( cvt::Color( 0.0f,0.0f,0.0f,1.0f) );
+
+		for( int x = 0; x < 3; x++ )
+			for( int y = 0; y < 3; y++ )
+				kern( x, y ) = 1.0f / 9.0f;
+		img.convolve( out, kern );
+		cvt::ImageIO::savePNG(out, "convolveg1.png");
+
+		out.fill( cvt::Color( 0.0f,0.0f,0.0f,1.0f) );
+		img.convolve( out, cvt::IKernel::MEAN_HORIZONTAL_3, cvt::IKernel::MEAN_VERTICAL_3);
+		cvt::ImageIO::savePNG(out, "convolveg2.png");
+
 	
 		
 	} catch( cvt::Exception e ) {
