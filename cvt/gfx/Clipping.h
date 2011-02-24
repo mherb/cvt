f55c856b5f774faa362cba18a543a1900d89cace
Philipp Heise <heise@in.tum.de>
2011-02-24 10:26:14 +0000

    Clipping stuff
    	modified:   ../cvt/gfx/Clipping.h
    
    git-svn-id: https://svnknoll.informatik.tu-muenchen.de/kloses/visionTools@988 f7bcf5df-4c73-0410-a0d8-b61c104701fe

--- cvt/gfx/Clipping.h
+++ cvt/gfx/Clipping.h
@@ -15,70 +15,57 @@ namespace cvt {
 
 	inline bool Clipping::clip( const Recti& rect, Vector2i& pt1, Vector2i& pt2 )
 	{
-		bool p1[ 4 ], p2[ 4 ];
-		bool in1, in2;
 		int xend, yend;
+		int dx, dy, ox, oy;
 
 		/* zero line */
 		if( pt1.x == pt2.x && pt1.y == pt2.y )
 			return false;
 
+		/* both points inside */
+		if( rect.contains( pt1 ) && rect.contains( pt2 ) )
+			return true;
+
 		xend = rect.x + rect.width;
 		yend = rect.y + rect.height;
 
-		p1[ 0 ] = ( pt1.x >= rect.x );
-		p1[ 1 ] = ( pt1.x <= xend );
-		p1[ 2 ] = ( pt1.y >= rect.y );
-		p1[ 3 ] = ( pt1.y <= yend );
-
-		p2[ 0 ] = ( pt2.x >= rect.x );
-		p2[ 1 ] = ( pt2.x <= xend );
-		p2[ 2 ] = ( pt2.y >= rect.y );
-		p2[ 3 ] = ( pt2.y <= yend );
-
-		/* both points inside */
-		if( p1[ 0 ] && p1[ 1 ] && p1[ 2 ] && p1[ 3 ] &&
-		    p2[ 0 ] && p2[ 1 ] && p2[ 2 ] && p2[ 3 ] )
-			return true;
-
 		/* both points outside and on the same side of one rect line */
-		if( !( p1[ 0 ] || p2[ 0 ] ) ||
-			!( p1[ 1 ] || p2[ 1 ] ) ||
-			!( p1[ 2 ] || p2[ 2 ] ) ||
-			!( p1[ 3 ] || p2[ 3 ] ) )
+		if( ( pt1.x < rect.x && pt2.x < rect.x ) ||
+			( pt1.x > xend && pt2.x > xend ) ||
+			( pt1.y < rect.y && pt2.y < rect.y ) ||
+			( pt1.y > yend && pt2.y > yend ) )
 			return false;
 
 		/* horizontal or vertical lines */
 		if( pt1.y == pt2.y ) {
-			std::cout << "HORIZONTAL" << std::endl;
-			if( !p1[ 0 ] ) pt1.x = rect.x;
-			else if( !p1[ 1 ] ) pt1.x = xend;
-			if( !p2[ 0 ] ) pt2.x = rect.x;
-			else if( !p2[ 1 ] ) pt2.x = xend;
+			pt1.x = Math::clamp( pt1.x, rect.x, xend );
+			pt2.x = Math::clamp( pt2.x, rect.x, xend );
 			return true;
 		} else if( pt1.x == pt2.x ) {
-			std::cout << "VERTICAL" << std::endl;
-			if( !p1[ 2 ] ) pt1.y = rect.y;
-			else if( !p1[ 3 ] ) pt1.y = yend;
-			if( !p2[ 2 ] ) pt2.y = rect.y;
-			else if( !p2[ 3 ] ) pt2.y = yend;
+			pt1.y = Math::clamp( pt1.y, rect.y, yend );
+			pt2.y = Math::clamp( pt2.y, rect.y, yend );
 			return true;
 		}
 
+		dx = pt2.x - pt1.x;
+		dy = pt2.y - pt1.y;
+		ox = pt1.x;
+		oy = pt1.y;
+
 		/* point 1*/
-		if( !p1[ 0 ] ) { /* left of rect, clip to left*/
-			pt1.y = pt2.y + ( ( pt1.y - pt2.y ) * ( rect.x - pt2.x ) ) / ( pt1.x - pt2.x );
+		if( pt1.x < rect.x ) { /* left of rect, clip to left*/
+			pt1.y = pt2.y + ( dy * ( rect.x - pt2.x ) ) / dx;
 			pt1.x = rect.x;
-		} else if( !p1[ 1 ] ) { /* right of rect, clip to right*/
-			pt1.y = pt2.y + ( ( pt1.y - pt2.y ) * ( xend - pt2.x ) ) / ( pt1.x - pt2.x );
+		} else if( pt1.x > xend ) { /* right of rect, clip to right*/
+			pt1.y = pt2.y + ( dy * ( xend - pt2.x ) ) / dx;
 			pt1.x = xend;
 		}
 
 		if( pt1.y < rect.y ) { /* above rect, clip to top */
-			pt1.x = pt2.x + ( ( pt1.x - pt2.x ) * ( rect.y - pt2.y ) ) / ( pt1.y - pt2.y );
+			pt1.x = Math::clamp( pt2.x + ( dx * ( rect.y - pt2.y ) ) / dy, rect.x, xend );
 			pt1.y = rect.y;
 		} else if( pt1.y > yend ) { /* below rect, clip to bottom */
-			pt1.x = pt2.x + ( ( pt1.x - pt2.x ) * ( yend - pt2.y ) ) / ( pt1.y - pt2.y );
+			pt1.x = Math::clamp( pt2.x + ( dx  * ( yend - pt2.y ) ) / dy, rect.x, xend );
 			pt1.y = yend;
 		}
 
@@ -87,21 +74,27 @@ namespace cvt {
 			return false;
 
 		/* point 2*/
-		if( !p2[ 0 ] ) { /* left of rect, clip to left*/
-			pt2.y = pt1.y + ( ( pt2.y - pt1.y ) * ( rect.x - pt1.x ) ) / ( pt2.x - pt1.x );
+		if( pt2.x < rect.x ) { /* left of rect, clip to left*/
+			pt2.y = oy + ( dy * ( rect.x - ox ) ) / dx;
 			pt2.x = rect.x;
-		} else if( !p2[ 1 ] ) { /* right of rect, clip to right*/
-			pt2.y = pt1.y + ( ( pt2.y - pt1.y ) * ( xend - pt1.x ) ) / ( pt2.x - pt1.x );
+		} else if( pt2.x > xend ) { /* right of rect, clip to right*/
+			pt2.y = oy + ( dy * ( xend - ox ) ) / dx;
 			pt2.x = xend;
 		}
 
 		if( pt2.y < rect.y ) { /* above rect, clip to top */
-			pt2.x = pt1.x + ( ( pt2.x - pt1.x ) * ( rect.y - pt1.y ) ) / ( pt2.y - pt1.y );
+			pt2.x = Math::clamp( ox + ( dx * ( rect.y - oy ) ) / dy, rect.x, xend );
 			pt2.y = rect.y;
 		} else if( pt2.y > yend ) { /* below rect, clip to bottom */
-			pt2.x = pt1.x + ( ( pt2.x - pt1.x ) * ( yend - pt1.y ) ) / ( pt2.y - pt1.y );
+			pt2.x = Math::clamp( ox + ( dx * ( yend - oy ) ) / dy, rect.x, xend );
 			pt2.y = yend;
 		}
+
+		/* zero line */
+		if( pt1.x == pt2.x && pt1.y == pt2.y )
+			return false;
+
+		std::cout << pt1 << " " << pt2 << std::endl;
 
 		return true;
 	}
