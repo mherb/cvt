c6ea0750227c92c9d6d3a18573bf1d6552170d7d
Philipp Heise <heise@in.tum.de>
2011-03-09 15:51:47 +0100

    Modified stack test
    	modified:   tests/stack/stack.cpp

--- tests/stack/stack.cpp
+++ tests/stack/stack.cpp
@@ -9,7 +9,7 @@ int main()
 	for( int i = 0; i < 20; i++ )
 		s.push( i );
 
-	std::cout << "OK size: " << s.size() << std::endl;
+	std::cout << "Size: " << s.size() << std::endl;
 
 	while( !s.isEmpty() )
 		std::cout << s.pop() << std::endl;
