32926c4c55bfcb8504b97c1a8460a94afe0b4d81
Philipp Heise <heise@in.tum.de>
2011-04-07 16:21:07 +0200

    More XML fun + FaceShape
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/util/String.h
    	modified:   ../cvt/util/Util.h
    	deleted:    ../cvt/util/XMLAttribute.h
    	deleted:    ../cvt/util/XMLCData.h
    	deleted:    ../cvt/util/XMLComment.h
    	deleted:    ../cvt/util/XMLDecoder.cpp
    	deleted:    ../cvt/util/XMLDecoder.h
    	deleted:    ../cvt/util/XMLDecoderUTF8.cpp
    	deleted:    ../cvt/util/XMLDecoderUTF8.h
    	deleted:    ../cvt/util/XMLDocument.h
    	deleted:    ../cvt/util/XMLElement.h
    	deleted:    ../cvt/util/XMLLeaf.h
    	deleted:    ../cvt/util/XMLNode.h
    	deleted:    ../cvt/util/XMLText.h
    	modified:   ../cvt/vision/FaceShape.h
    	modified:   ../tests/CMakeLists.txt
    	modified:   ../tests/faceshapeopt/main.cpp
    	modified:   ../tests/util/util.cpp
    	new file:   ../tests/xml/CMakeLists.txt
    	new file:   ../tests/xml/xml.cpp

--- tests/util/util.cpp
+++ tests/util/util.cpp
@@ -5,7 +5,17 @@ using namespace cvt;
 
 int main()
 {
+	char test[] = {0xE2,0x82,0xAC,0x0};
+	char test2[] ={0xF0,0x9D,0x84,0x9E,0x0};
+	char* end;
 	printf("0x%X\n", Util::bswap16( 0x1122 ) );
 	printf("0x%X\n", Util::bswap32( 0x11223344 ) );
 	printf("0x%llX\n", Util::bswap64( 0x1122334455667788 ) );
+	printf("0x%X\n", Util::UTF8toUTF32( test, &end ) );
+	printf("%d\n", end - test );
+	printf("%p %p 0x%X 0x%X\n", end, test + 3, *end, test[ 3 ] );
+	printf("0x%X\n", Util::UTF8toUTF32( test2, &end ) );
+	printf("%d\n", end - test2 );
+	printf("%p %p 0x%X 0x%X\n", end, test2 + 4, *end, test[ 4 ] );
+
 }
