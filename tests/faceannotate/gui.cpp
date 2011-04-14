31ecaa3eacfa734ce9428299d59a6116dffae386
Philipp Heise <heise@in.tum.de>
2011-04-14 14:56:02 +0200

    Face annotator stuff
    	modified:   ../data/facedefault.xml
    	modified:   ../tests/faceannotate/gui.cpp

--- tests/faceannotate/gui.cpp
+++ tests/faceannotate/gui.cpp
@@ -15,7 +15,7 @@ using namespace cvt;
 class FaceUI : public Window
 {
 	public:
-	FaceUI( const String& str ) : Window("Face Annotator")
+	FaceUI( const String& str ) : Window("Face Annotator"), _selection( - 1 )
 	{
 		Image img;
 		ImageIO::loadPNG( img, str.c_str() );
@@ -58,26 +58,58 @@ class FaceUI : public Window
 	void mousePressEvent( MousePressEvent* event )
 	{
 		Rectf r( 0, 0, _dw, _dh );
+
+		float x = ( ( event->x / _dw ) - 0.5f );
+		float y = ( ( event->y / _dh ) - 0.5f );
+
+
+		for( int i =0, end = _pts.size(); i < end; i++ ) {
+			float dx = ( _pts[ i ].x + 0.5f ) * _dw;
+			float dy = ( _pts[ i ].y + 0.5f ) * _dh;
+			Rectf r( dx - 4, dy - 4, 8, 8 );
+			if( r.contains( event->x, event->y ) ) {
+				_selection = i;
+				_lx = event->x;
+				_ly = event->y;
+				return;
+			}
+		}
+		_selection = -1;
 		if( r.contains( event->x, event->y ) ) {
-			std::cout << "x=\"" << ( ( event->x / _dw ) - 0.5f ) << "\" "
-					  << "y=\"" << ( ( event->y / _dh ) - 0.5f ) << "\" " << std::endl;
-			_pts.push_back( Point2f(( ( event->x / _dw ) - 0.5f ), ( ( event->y / _dh ) - 0.5f ) ) );
+			std::cout << "x=\"" << x << "\" "
+					  << "y=\"" << y << "\" " << std::endl;
+			_pts.push_back( Point2f( x, y ) );
 			update();
 		}
 	}
 
 	void mouseMoveEvent( MouseMoveEvent* event )
 	{
+		if( _selection >= 0 ) {
+			float dx = ( event->x - _lx ) / _dw;
+			float dy = ( event->y - _ly ) / _dh;
+			_pts[ _selection ] += Point2f( dx, dy );
+			_lx = event->x;
+			_ly = event->y;
+			update();
+		}
 	}
 
 	void mouseReleaseEvent( MouseReleaseEvent* e )
 	{
+		if( _selection >= 0 ) {
+			std::cout << "x=\"" << _pts[ _selection ].x << "\" "
+				<< "y=\"" << _pts[ _selection ].y << "\" " << std::endl;
+			_selection = -1;
+		}
 	}
 
 	private:
 		Image _glimage;
 		float _dw, _dh;
 		std::vector<Point2f> _pts;
+		int _selection;
+		float _lx, _ly;
 };
 
 int main( int argc, char** argv )
