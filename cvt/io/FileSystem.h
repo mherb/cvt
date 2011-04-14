6c46de03a68df5b1a949248155ce9664586ba5e3
Philipp Heise <heise@in.tum.de>
2011-04-14 10:37:59 +0200

    Added Data and basic FileSystem ops ( untested )
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/io/FileSystem.cpp
    	modified:   ../cvt/io/FileSystem.h
    	modified:   ../cvt/util/Data.h

--- cvt/io/FileSystem.h
+++ cvt/io/FileSystem.h
@@ -3,19 +3,23 @@
 
 #include <string>
 #include <vector>
+#include <cvt/util/Data.h>
 
 namespace cvt {
 
-class FileSystem
-{
-    public:
-		static bool exists( const std::string & path );
-		static void rename( const std::string & from, const std::string & to );
-		static bool isFile( std::string const & file );
-		static bool isDirectory( std::string const & dir );
-		static void ls( const std::string & path, std::vector<std::string> & entries );
-		static void filesWithExtension( const std::string & path, std::vector<std::string>& result, const std::string & ext = "" );
-};
+	class FileSystem
+	{
+		public:
+			static bool   exists( const std::string & path );
+			static void   rename( const std::string & from, const std::string & to );
+			static bool   isFile( std::string const & file );
+			static bool   isDirectory( std::string const & dir );
+			static void   ls( const std::string & path, std::vector<std::string> & entries );
+			static void   filesWithExtension( const std::string & path, std::vector<std::string>& result, const std::string & ext = "" );
+			static size_t size( const std::string& path );
+			static bool	  load( Data& d, const std::string& path );
+			static bool	  save( const std::string& path, const Data& d );
+	};
 
 }
 
