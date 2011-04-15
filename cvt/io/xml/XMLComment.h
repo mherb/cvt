e4ced6ba472fc9d40fceecf702fc0792e6c42527
Philipp Heise <heise@in.tum.de>
2011-04-15 10:46:10 +0200

    XML bugfix and comment parsing
    	modified:   ../cvt/io/xml/XMLComment.h
    	modified:   ../cvt/io/xml/XMLDecoderUTF8.cpp
    	modified:   ../cvt/io/xml/XMLDecoderUTF8.h
    	modified:   ../data/facedefault.xml

--- cvt/io/xml/XMLComment.h
+++ cvt/io/xml/XMLComment.h
@@ -1,23 +1,23 @@
 #ifndef CVT_XMLCOMMENT_H
 #define CVT_XMLCOMMENT_H
 
-#include <cvt/io/xml/XMLNode.h>
+#include <cvt/io/xml/XMLLeaf.h>
 
 namespace cvt {
-	class XMLComment : public XMLNode {
+	class XMLComment : public XMLLeaf {
 		public:
-			XMLComment( const String& name, const String& value );
+			XMLComment( const String& value );
 			XMLComment( const XMLComment& other );
 			XMLComment& operator=( const XMLComment& other );
 			~XMLComment();
 
 	};
 
-	inline XMLComment::XMLComment( const String& name, const String& value ) : XMLNode( XML_NODE_COMMENT, name, value )
+	inline XMLComment::XMLComment( const String& value ) : XMLLeaf( XML_NODE_COMMENT, "", value )
 	{
 	}
 
-	inline XMLComment::XMLComment( const XMLComment& other ) : XMLNode( XML_NODE_COMMENT, other._name, other._value )
+	inline XMLComment::XMLComment( const XMLComment& other ) : XMLLeaf( XML_NODE_COMMENT, "", other._value )
 	{
 	}
 
