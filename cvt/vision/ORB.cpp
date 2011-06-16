5b62dbf298d7053e80a37f1b2167c0d08c1fa807
Sebastian Klose <kloses@in.tum.de>
2011-06-16 11:53:19 +0200

    - added new files to cmake - make it compile

--- cvt/vision/ORB.cpp
+++ cvt/vision/ORB.cpp
@@ -1,19 +1,21 @@
 #include <cvt/vision/ORB.h>
 
 namespace cvt {
-	ORB::ORB( const Image& img, size_t octaves )
+    
+	ORB::ORB( const Image& img, size_t octaves, float scalefactor )
 	{
-		float scale = 1.0f;
-		detect( img, scale );
+		float scale = 1.0f;  
+        IScaleFilterBilinear scaleFilter;
+        detect( img, scale );
 		for( size_t i = 1; i < octaves; i++ ) {
 			Image pyrimg;
 			scale *=  scalefactor;
-			img.resize( pyrimg, img.width() * scalefactor, img.height() * scalefactor );
-			detect( pyrimg, scalefactor );
+			img.scale( pyrimg, ( size_t )( img.width() * scalefactor ), ( size_t )( img.height() * scalefactor ), scaleFilter );
+			detect( pyrimg, scale );
 		}
 	}
 
-	void ORB::detect( const Image& img, float scalefactor )
+	void ORB::detect( const Image& img, float scale )
 	{
 	}
 
