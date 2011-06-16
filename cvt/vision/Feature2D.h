9d982d82aca048e55526ffc4de8024e70ab70cf0
Philipp Heise <heise@in.tum.de>
2011-06-16 11:31:57 +0200

    Added ORB class
    	new file:   Feature2D.h
    	new file:   ORB.cpp
    	new file:   ORB.h

---
+++ cvt/vision/Feature2D.h
@@ -0,0 +1,24 @@
+#ifndef CVT_FEATURE2D_H
+#define CVT_FEATURE2D_H
+
+#include <cvt/math/Vector.h>
+
+namespace cvt {
+	template<typename T>
+	struct Feature2D {
+		Feature2D( T x, T y, float angle = 0.0f, float scale = 1.0f );
+
+		Vector2<T> pt;
+		float angle;
+		float scale;
+	};
+
+	inline Feature2D::Feature2D( T x, T y, float a, float s ) : pt( x, y ), angle( a ), scale( s )
+	{
+	}
+
+	typedef Feature2D<int> Feature2Di;
+	typedef Feature2D<float> Feature2Df;
+}
+
+#endif
---
+++ cvt/vision/ORB.h
@@ -0,0 +1,42 @@
+#ifndef CVT_ORB_H
+#define CVT_ORB_H
+
+#include <cvt/vision/Feature2D.h>
+#include <cvt/gfx/Image.h>
+#include <cvt/vision/IntegralImage.h>
+#include <vector>
+
+namespace cvt {
+
+	struct ORBFeature : Feature2Df {
+		uint8_t desc[ 32 ]; // 256 bit vector
+	};
+
+	class ORB {
+		public:
+			ORB( const Image& img, size_t octaves = 3, float scalefactor = 0.5f );
+
+			size_t size() const;
+			const ORBFeature& operator[]( size_t index ) const;
+
+		private:
+			void detect( Image& img, float scale );
+			void centroidAngle( ORBFeature& feature, IntegralImage& iimg  );
+			void descriptor( ORBFeature& feature, IntegralImage& iimg );
+
+			std::vector<ORBFeature> _features;
+			static Vector2i _patterns[ 30 ][ 256 ][ 2 ];
+	};
+
+	inline size_t ORB::size() const
+	{
+		return _features.size();
+	}
+
+	inline const ORBFeature& ORB::operator[]( size_t index ) const
+	{
+		return _features[ index ];
+	}
+}
+
+#endif
