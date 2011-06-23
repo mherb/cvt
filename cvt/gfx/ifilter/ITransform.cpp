0b781fc81508124ae4c0908cf49b6f3800d9b4a4
heise <heise@in.tum.de>
2011-06-23 13:08:46 +0200

    ITransform now bilinear
    - got really fucked by casting:
    	( int ) -0.5f gives 0 and not -1, while the correct floor gives -1
    	since in the worst case we are at -1 the fix is to add 1 to the float value
    	and to subtract one after the cast ...
    
    	modified:   ../cvt/gfx/ifilter/ITransform.cpp
    	modified:   ../cvt/gfx/ifilter/ITransform.h
    	modified:   ../tests/homography/homography.cpp

--- cvt/gfx/ifilter/ITransform.cpp
+++ cvt/gfx/ifilter/ITransform.cpp
@@ -5,8 +5,6 @@ namespace cvt {
 
 	static ParamInfoTyped<Image*>   _pinput( "Input" );
 	static ParamInfoTyped<Matrix3f> _ptransform( "Transformation" );
-	static ParamInfoTyped<bool>		_pfillcolor( "FillOutside" );
-	static ParamInfoTyped<Color>	_pcolor( "FillColor" );
 	static ParamInfoTyped<uint32_t>	_pwidth( "Width", 0, true, 1, 0 );
 	static ParamInfoTyped<uint32_t>	_pheight( "Height", 0, true, 1, 0 );
 	static ParamInfoTyped<Image*>	_poutput( "Output", NULL, false );
@@ -15,14 +13,12 @@ namespace cvt {
 	{
 		&_pinput,
 		&_ptransform,
-		&_pfillcolor,
-		&_pcolor,
 		&_pwidth,
 		&_pheight,
 		&_poutput
 	};
 
-	ITransform::ITransform() : IFilter( "ImageTransform", _itransform_params, 7, IFILTER_CPU )
+	ITransform::ITransform() : IFilter( "ImageTransform", _itransform_params, 5, IFILTER_CPU )
 	{
 	}
 
@@ -30,18 +26,18 @@ namespace cvt {
 	{
 	}
 
-	void ITransform::apply( Image& dst, const Image& src, const Matrix3f& transform, bool fill, const Color& color )
+	void ITransform::apply( Image& dst, const Image& src, const Matrix3f& transform )
 	{
 		if( src.format() != dst.format() )
 			throw CVTException( "Image formats do not match!" );
 
 		switch( src.format().formatID ) {
-			case IFORMAT_GRAY_FLOAT: return applyFC1( dst, src, transform, fill, color );
-			case IFORMAT_GRAY_UINT8: return applyU8C1( dst, src, transform, fill, color );
+			case IFORMAT_GRAY_FLOAT: return applyFC1( dst, src, transform );
+			case IFORMAT_GRAY_UINT8: return applyU8C1( dst, src, transform );
 			case IFORMAT_RGBA_FLOAT:
-			case IFORMAT_BGRA_FLOAT: return applyFC4( dst, src, transform, fill, color );
+			case IFORMAT_BGRA_FLOAT: return applyFC4( dst, src, transform );
 			case IFORMAT_RGBA_UINT8:
-			case IFORMAT_BGRA_UINT8: return applyU8C4( dst, src, transform, fill, color );
+			case IFORMAT_BGRA_UINT8: return applyU8C4( dst, src, transform );
 			default: throw CVTException( "Unsupported image format!" );
 		}
 	}
@@ -63,7 +59,7 @@ namespace cvt {
 	}
 
 
-	void ITransform::applyFC1( Image& idst, const Image& isrc, const Matrix3f& transform, bool fill, const Color& color )
+	void ITransform::applyFC1( Image& idst, const Image& isrc, const Matrix3f& transform )
 	{
 		const uint8_t* src;
 		uint8_t* dst;
@@ -87,7 +83,7 @@ namespace cvt {
 		invtrans = transform.inverse();
 		invtt = invtrans.transpose();
 
-		Rectf r( 0, 0, sw, sh );
+		Rectf r( 0 - 1.0f, 0 - 1.0f, sw + 2, sh + 2);
 		Vector3f nx = transform * Vector3f( 1.0f, 0.0f, 0.0f );
 
 		for( size_t y = 0; y < h; y++  ) {
@@ -106,18 +102,24 @@ namespace cvt {
 					px2 = tmp;
 				}
 
-				Vector3f p = transform * Vector3f( ( float ) Math::clamp<size_t>( ( px1.x + 0.5f ), 0, w ), y, 1.0f );
-				for( size_t x = Math::clamp<size_t>(  px1.x + 0.5f, 0, w ), xend = Math::clamp<size_t>( ( px2.x + 0.5f ), 0, w ); x < xend; x++ )
+				Vector3f p = transform * Vector3f( ( float ) Math::clamp<size_t>( ( px1.x ), 0, w ), y, 1.0f );
+				for( size_t x = Math::clamp<size_t>(  px1.x, 0, w ), xend = Math::clamp<size_t>( ( px2.x + 1.0f ), 0, w ); x < xend; x++ )
 				{
 					float fx, fy;
 					fx = p.x / p.z;
 					fy = p.y / p.z;
-					pdst[ x ] = *( ( float* ) ( src + sstride * ( size_t ) fy + sizeof( float ) * ( size_t ) fx ) );
+					float alpha1 = fx + 1 - ( float ) ( int )( fx + 1 );
+					float alpha2 = fy + 1 - ( float ) ( int )( fy + 1 );
+#define VAL( fx, fy ) ( ( fx ) >= 0 && ( fx ) < ( int )sw && ( fy ) >= 0 && ( fy ) < ( int ) sh ) ? *( ( float* ) ( src + sstride * ( fy ) + sizeof( float ) * ( fx ) ) ) : pdst[ x ]
+					float v1, v2;
+					int lx = -1 + ( int )( fx + 1 );
+					int ly = -1 + ( int )( fy + 1 );
+					v1 = Math::mix( VAL( lx, ly ), VAL( 1 + lx, ly  ), alpha1 );
+					v2 = Math::mix( VAL( lx, 1 + ly ), VAL( 1 + lx, 1 + ly  ), alpha1 );
+					pdst[ x ] = Math::mix( v1, v2, alpha2 );
 					p += nx;
 				}
 
-			} else {
-				// fill if needed
 			}
 			dst2 += dstride;
 		}
@@ -126,15 +128,15 @@ namespace cvt {
 	}
 
 
-	void ITransform::applyFC4( Image& dst, const Image& src, const Matrix3f& transform, bool fill, const Color& color )
+	void ITransform::applyFC4( Image& dst, const Image& src, const Matrix3f& transform )
 	{
 	}
 
-	void ITransform::applyU8C1( Image& dst, const Image& src, const Matrix3f& transform, bool fill, const Color& color )
+	void ITransform::applyU8C1( Image& dst, const Image& src, const Matrix3f& transform )
 	{
 	}
 
-	void ITransform::applyU8C4( Image& dst, const Image& src, const Matrix3f& transform, bool fill, const Color& color )
+	void ITransform::applyU8C4( Image& dst, const Image& src, const Matrix3f& transform )
 	{
 	}
 
