93ab77a9de8aa88e40c3c61a55123e269f01cc45
Sebastian Klose <kloses@in.tum.de>
2011-07-14 22:42:35 +0200

    - minor tests

--- cvt/vision/ORB.cpp
+++ cvt/vision/ORB.cpp
@@ -252,7 +252,6 @@ namespace cvt {
 			_features.erase( _features.begin() + num, _features.end() );
 	}
 
-
 	const int ORB::_circularoffset[ 31 ] = {  3,  6,  8,  9, 10, 11, 12, 13, 13, 14, 14, 14, 15, 15, 15, 15,
 											 15, 15, 15, 14, 14, 14, 13, 13, 12, 11, 10,  9,  8,  6,  3 };
 #include "ORBPatterns.h"
