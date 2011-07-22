a337d9b8b6a2315b8a402bf311ffe2eaa100e7c3
Sebastian Klose <kloses@in.tum.de>
2011-07-22 13:17:25 +0200

    fixed PPMLoader (using DataIterator now)
    	modified:   ../cvt/util/DataIterator.h
    	modified:   ../cvt/util/SIMD.cpp
    	modified:   ../plugins/iloader/PPM/PPMLoader.cpp
    	modified:   ../tests/image/image.cpp

--- cvt/util/DataIterator.h
+++ cvt/util/DataIterator.h
@@ -20,7 +20,18 @@ namespace cvt {
 
         bool hasNext( );
 
-        bool nextToken( String & token, const String & deliminaters );
+        bool nextToken( String & token, const String & deliminators );
+
+        bool nextLine( String & line );
+
+        void tokenizeNextLine( std::vector<String> & tokens, const String & deliminators );
+
+        const uint8_t * pos() const { return _pos; }
+        const uint8_t * end() const { return _end; }
+
+        size_t remainingBytes() const { return _end - _pos; }
+
+        long nextLong( int base = 0 );
 
       private:
         Data &		_data;
@@ -60,6 +71,70 @@ namespace cvt {
         }
 
         return true;
+    }
+
+    inline bool DataIterator::nextLine( String & line )
+    {
+        if( !hasNext() )
+            return false;
+        uint8_t * start = _pos;
+
+        while( *_pos != '\n' ){
+            if( !hasNext() )
+                return false;
+            _pos++;
+        }
+
+        if( _pos == start ){
+            // empty line
+            _pos++;
+            return false;
+        }
+
+        // create the line
+        line.assign( ( char* )start, _pos - start );
+        _pos++;
+        return true;
+    }
+
+    inline void DataIterator::tokenizeNextLine( std::vector<String> & tokens, const String & deliminators )
+    {
+        String line;
+        if( !nextLine( line ) )
+            return;
+
+        size_t start = 0;
+        size_t end = line.length();
+        size_t curr = 0;
+
+        while( curr != end){
+            if( isDeliminator( line[ curr ], deliminators ) ){
+                curr++;
+            } else {
+                // curr is a non token!
+                start = curr;
+                while( curr != end && !isDeliminator( line[ curr ], deliminators ) )
+                    curr++;
+                tokens.push_back( String( &line.c_str()[ start ], curr - start ) );
+            }
+        }
+    }
+
+    inline long DataIterator::nextLong( int base )
+    {
+        if( !hasNext() ){
+            throw CVTException( "No more data available" );
+        }
+
+        uint8_t * end;
+        long v = strtol( ( const char* )_pos, ( char** )&end, base );
+
+        if( *end != '\0' )
+            _pos = end;
+        else
+            _pos = _end;
+
+        return v;
     }
 
     inline bool DataIterator::isDeliminator( uint8_t c, const String & delims )
