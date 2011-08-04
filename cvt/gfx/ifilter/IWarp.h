84523561f38bb6bcbdaa5d5affd529a47affb2cc
Philipp Heise <heise@in.tum.de>
2011-08-04 12:56:01 +0200

    Added IWarp files
    	new file:   ../cvt/gfx/ifilter/IWarp.cpp
    	new file:   ../cvt/gfx/ifilter/IWarp.h

---
+++ cvt/gfx/ifilter/IWarp.h
@@ -0,0 +1,29 @@
+#ifndef CVT_IWARP_H
+#define CVT_IWARP_H
+
+#include <cvt/gfx/IFilter.h>
+#include <cvt/util/Plugin.h>
+#include <cvt/util/PluginManager.h>
+
+namespace cvt {
+	class IWarp : public IFilter {
+		public:
+			IWarp();
+			~IWarp();
+			void apply( const ParamSet* attribs, IFilterType iftype ) const;
+
+			static void apply( Image& dst, const Image& src, const Image& warp );
+
+			static void warpTunnel( Image& dst, float radius, float cx, float cy );
+
+		private:
+			static void applyFC1( Image& dst, const Image& src, const Image& warp );
+			static void applyFC4( Image& dst, const Image& src, const Image& warp );
+			static void applyU8C1( Image& dst, const Image& src, const Image& warp );
+			static void applyU8C4( Image& dst, const Image& src, const Image& warp );
+
+			IWarp( const IWarp& t );
+	};
+}
+
+#endif
