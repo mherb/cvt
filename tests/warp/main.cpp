8b2ec6122c29c3d8ccf046b38dd9d41fb0472302
Sebastian Klose <kloses@in.tum.de>
2011-08-04 16:32:14 +0200

    distorted image
    	new file:   ../data/distort.png
    	modified:   ../tests/featureTracking/FeatureTrackApp.h
    	modified:   ../tests/warp/main.cpp

--- tests/warp/main.cpp
+++ tests/warp/main.cpp
@@ -9,13 +9,13 @@ int main()
 	Resources r;
 	Image in, out, warp, tmp;
 
-	tmp.load( r.find("lena.png") );
+	tmp.load( r.find("distort.png") );
 	in.reallocate( tmp.width(), tmp.height(), IFormat::RGBA_UINT8 );
 	tmp.convert( in );
 
 	warp.reallocate( in.width(), in.height(), IFormat::GRAYALPHA_FLOAT );
 
-	IWarp::warpUndistort( warp, 0.05f, -0.13f, in.width() * 0.5f, in.height() * 0.5f, 700.0f, 700.0f );
+	IWarp::warpUndistort( warp, -0.434369f, 0.194401f, 376.734f, 253.739f, 650.228f, 651.185f, 0.00115974f, 0.00120469f );
 	IWarp::apply( out, in, warp );
 
 	out.save( "warped.png" );
