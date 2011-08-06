4a26b85851d5004ca0a753a77f57ccfb4e535fb1
Philipp Heise <heise@in.tum.de>
2011-08-06 14:08:55 +0200

    Measure time for IWarp
    	modified:   ../tests/warp/main.cpp

--- tests/warp/main.cpp
+++ tests/warp/main.cpp
@@ -1,6 +1,7 @@
 #include <cvt/io/Resources.h>
 #include <cvt/gfx/Image.h>
 #include <cvt/gfx/ifilter/IWarp.h>
+#include <cvt/util/Time.h>
 
 using namespace cvt;
 
@@ -10,13 +11,16 @@ int main()
 	Image in, out, warp, tmp;
 
 	tmp.load( r.find("distort.png") );
-	in.reallocate( tmp.width(), tmp.height(), IFormat::BGRA_FLOAT );
+	in.reallocate( tmp.width(), tmp.height(), IFormat::RGBA_UINT8 );
 	tmp.convert( in );
 
 	warp.reallocate( in.width() + 250, in.height() + 250, IFormat::GRAYALPHA_FLOAT );
 
 	IWarp::warpUndistort( warp, -0.434369f, 0.194401f, 376.734f, 253.739f, 650.228f, 651.185f, in.width(), in.height(), 0, 0.00115974f, 0.00120469f );
+
+	Time t;
 	IWarp::apply( out, in, warp );
+	std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
 
 	out.save( "warped.png" );
 }
