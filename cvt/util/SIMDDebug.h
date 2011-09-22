a37d02c4410b0b161a1decb8513a0a5a8a276b47
Philipp Heise <heise@in.tum.de>
2011-09-22 16:16:02 +0200

    Harris 13x13 SSE2
    	modified:   ../cvt/util/SIMDDebug.h
    	modified:   ../cvt/util/SIMDSSE2.cpp
    	modified:   ../tests/harriscircular/main.cpp

--- cvt/util/SIMDDebug.h
+++ cvt/util/SIMDDebug.h
@@ -81,6 +81,15 @@ namespace cvt {
                 x.i16[ 4 ], x.i16[ 5 ], x.i16[ 6 ], x.i16[ 7 ] );
     }
 
+    static inline void SIMD_print_i16( const char* str, __m128i _x )
+    {
+		SIMD128 x;
+		x.m128i = _x;
+
+        printf( "%s %d %d %d %d %d %d %d %d\n", str, x.i16[ 0 ], x.i16[ 1 ], x.i16[ 2 ], x.i16[ 3 ],
+                x.i16[ 4 ], x.i16[ 5 ], x.i16[ 6 ], x.i16[ 7 ] );
+    }
+
     static inline void SIMD_print_u8hex( __m128i _x )
     {
 		SIMD128 x;
