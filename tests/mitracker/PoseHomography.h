4e43eccbfdad95997f0ba42474c8cdc19b16753a
Sebastian Klose <kloses@in.tum.de>
2011-09-28 11:05:29 +0200

    added homography representation as in paper
    	modified:   ../CMakeLists.txt
    	modified:   MITracker.h
    	new file:   PoseHomography.h
    	new file:   mitracker.cpp

---
+++ tests/mitracker/PoseHomography.h
@@ -0,0 +1,133 @@
+#ifndef CVT_POSE_HOMOGRAPHY
+#define CVT_POSE_HOMOGRAPHY
+
+#include <Eigen/Core>
+
+namespace cvt
+{
+	/**
+	 *	current is the actual homography
+	 *	Composition update is done by: Hcurrent * HDelta
+	 *	
+	 */
+	template <typename T>
+	class PoseHomography
+	{
+		public:
+			PoseHomography();
+			~PoseHomography();
+
+			const Matrix3<T> & pose() const { return _current; }
+				  Matrix3<T> & pose()		{ return _current; }
+			
+
+			void screenJacobian( Eigen::Matrix<T, 2, 8>& jac, const Vector2<T>& point ) const;
+
+			void screenHessian( Eigen::Matrix<T, 8, 8>& hx, 
+							    Eigen::Matrix<T, 8, 8>& hy,
+							    const Vector2<T>& point ) const;
+
+			/* construct the delta homography from the parameters and apply it to the current one */
+			void addDelta( const Eigen::Matrix<T, 8, 1>& delta );
+			void removeDelta( const Eigen::Matrix<T, 8, 1>& delta );
+
+		private:
+			Matrix3<T>	_current;
+
+			void fillDeltaMatrix( Matrix3<T> & deltaH, const Eigen::Matrix<T, 8, 1>& p ) const;
+
+	};
+
+	template <typename T>
+	inline PoseHomography<T>::PoseHomography()
+	{
+		_current.setIdentity();
+	}
+
+	template <typename T>
+	inline PoseHomography<T>::~PoseHomography()
+	{
+	}
+
+	template <typename T>
+	inline void PoseHomography<T>::screenJacobian( Eigen::Matrix<T, 2, 8>& j, 
+												   const Vector2<T>& p ) const
+	{
+		T xx = p.x * p.x;
+		T xy = p.x * p.y;
+		T yy = p.y * p.y;
+		j << p.x,   0, p.y,   0, 1, 0, -xx, -xy, 
+			   0, p.x,   0, p.y, 0, 1, -xy, -yy;
+	}
+			
+	template <typename T>
+	inline void PoseHomography<T>::screenHessian( Eigen::Matrix<T, 8, 8>& hx, 
+												  Eigen::Matrix<T, 8, 8>& hy,
+												  const Vector2<T>& p ) const
+	{
+		T xx = p.x * p.x;
+		T xxx = xx * p.x;
+		T xxy = xx * p.y;
+		T xy = p.x * p.y;
+		T yy = p.y * p.y;
+		T yyy = yy * p.y;
+		T yyx = yy * p.x;
+
+		hx <<	0,	0,	0,	0,	0,	0,	-xx,	-xy,
+				0,	0,	0,	0,	0,	0,	  0,	  0, 
+				0,	0,	0,	0,	0,	0,	-xy,	-yy, 
+				0,	0,	0,	0,	0,	0,	  0,	  0, 
+				0,	0,	0,	0,	0,	0,	p.x,   -p.y, 
+				0,	0,	0,	0,	0,	0,	  0,	  0, 
+			  -xx,	0,-xy,	0,-p.x,	0,2*xxx,  2*xxy,
+			  -xy,	0,-yy,	0,-p.y,	0,2*xxy,  2*yyx;
+		
+		hy <<	0,	0,	0,	0,	0,	 0,	   0,	  0,
+				0,	0,	0,	0,	0,	 0,  -xx,	-xy, 
+				0,	0,	0,	0,	0,	 0,	   0,	  0, 
+				0,	0,	0,	0,	0,	 0,  -xy,	-yy, 
+				0,	0,	0,	0,	0,	 0,    0,	  0, 
+				0,	0,	0,	0,	0,	 0, -p.x,   -p.y, 
+			    0,-xx,  0,-xy,  0,-p.x,2*xxy,  2*yyx,
+			    0,-xy,  0,-yy,  0,-p.y,2*yyx,  2*yyy;
+	}
+
+	template <typename T>
+	inline void PoseHomography<T>::addDelta( const Eigen::Matrix<T, 8, 1>& delta )
+	{
+		Matrix3<T> dH;
+		fillDeltaMatrix( dH, delta );
+		_current *= dH;
+	}
+
+	template <typename T>
+	inline void PoseHomography<T>::removeDelta( const Eigen::Matrix<T, 8, 1>& delta )
+	{
+		Matrix3<T> dH;
+		fillDeltaMatrix( dH, delta );
+
+		if( !dH.inverseSelf() ){
+			throw CVTException( "Could not invert delta Matrix" );
+		}
+
+		_current *= dH;
+	}
+
+
+	template <typename T>
+	inline void PoseHomography<T>::fillDeltaMatrix( Matrix3<T> & deltaH, const Eigen::Matrix<T, 8, 1>& p ) const
+	{
+		deltaH[ 0 ][ 0 ] = 1 + p[ 0 ];
+		deltaH[ 0 ][ 1 ] = p[ 2 ];
+		deltaH[ 0 ][ 2 ] = p[ 4 ];
+		deltaH[ 1 ][ 0 ] = p[ 1 ];
+		deltaH[ 1 ][ 1 ] = 1 + p[ 3 ];
+		deltaH[ 1 ][ 2 ] = p[ 5 ];
+		deltaH[ 2 ][ 0 ] = p[ 6 ];
+		deltaH[ 2 ][ 1 ] = p[ 7 ];
+		deltaH[ 2 ][ 2 ] = 1;
+	}
+
+}
+
+#endif
---
+++ tests/mitracker/mitracker.cpp
@@ -0,0 +1,9 @@
+#include <cvt/gfx/Image.h>
+
+#include "MITracker.h"
+
+int main( int argc, char* argv )
+{
+	// TODO: make base application
+	return 0;
+}
