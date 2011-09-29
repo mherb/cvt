277db6d8960c3cd8aada72cff75561d85d07cce7
Philipp Heise <heise@in.tum.de>
2011-09-29 20:05:05 +0200

    Added bsplinehist stuff
    	new file:   ../tests/bsplinehist/BSplineHist.h
    	modified:   ../tests/bsplinehist/main.cpp

--- tests/bsplinehist/main.cpp
+++ tests/bsplinehist/main.cpp
@@ -1,1 +1,23 @@
 #include <cvt/gfx/Image.h>
+#include <iostream>
+
+#include "BSplineHist.h"
+
+using namespace cvt;
+
+int main( int argc, char** argv )
+{
+	if( argc != 2 ) {
+		std::cout << "usage: " << argv[ 0 ] << " image" << std::endl;
+		return 1;
+	}
+
+	Image input, infloat;
+	input.load( argv[ 1 ] );
+	infloat.reallocate( input.width(), input.height(), IFormat::GRAY_FLOAT );
+	input.convert( infloat );
+
+	BSplineHist bhist( 64 );
+	bhist.update( infloat );
+	bhist.dump();
+}
