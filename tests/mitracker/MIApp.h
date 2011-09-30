83a10c9c30a5dfe6712a3e4342134d2a5fd292a5
Sebastian Klose <kloses@in.tum.de>
2011-09-30 13:03:36 +0200

    Template selection view
    	modified:   MIApp.h
    	modified:   MIGui.h
    	new file:   RectSelectImageView.h

--- tests/mitracker/MIApp.h
+++ tests/mitracker/MIApp.h
@@ -1,6 +1,7 @@
 #include <cvt/gui/Window.h>
 #include <cvt/gui/Application.h>
 #include <cvt/gui/TimeoutHandler.h>
+#include <cvt/util/Time.h>
 
 #include "MITracker.h"
 #include "MIGui.h"
@@ -15,17 +16,32 @@ namespace cvt
 
 			void onTimeout();
 
+			void selectionDidStart();
+			void selectionFinished( const Rectf& r ); 
+
 		private:
 			VideoInput*	_input;
 			MIGui		_gui;
 			MITracker	_tracker;
 			uint32_t	_timerId;
+			Time		_fpsTime;
+			size_t		_iter;
+			bool		_selectingTemplate;
+			Image		_currGray;
 	};
 
 	inline MIApp::MIApp( VideoInput* input ) :
-		_input( input )
+		_input( input ),
+		_iter( 0 ),
+		_selectingTemplate( true )
 	{
-		_timerId = Application::registerTimer( 10, this );
+		_timerId = Application::registerTimer( 5, this );
+
+		// add the delegates to the gui
+		Delegate<void (void)> selStart( this, &MIApp::selectionDidStart );
+		Delegate<void (const Rectf&)> selDone( this, &MIApp::selectionFinished );
+
+		_gui.addSelectionDelegates( selStart, selDone );
 	}
 
 	inline MIApp::~MIApp()
@@ -35,20 +51,48 @@ namespace cvt
 
 	inline void MIApp::onTimeout()
 	{
-		_input->nextFrame();
-		
-		// make it gray
-		Image gray;
-		_input->frame().convert( gray, IFormat::GRAY_UINT8 );
-	
-		//_tracker.updateInput( gray );
+		_iter++;
 
-		_tracker.updateTemplate( gray );
-		_gui.setTemplateImage( gray, 
-							   _tracker.templateGradX(), 
-							   _tracker.templateGradY() );
+		_input->nextFrame();
+		_input->frame().convert( _currGray, IFormat::GRAY_UINT8 );
+
+		if( !_selectingTemplate ){
+			_tracker.updateInput( _currGray );
+		}
+
+		if( _iter == 50 ){
+			_gui.setFPS( _iter * 1000.0f / _fpsTime.elapsedMilliSeconds() );
+			_fpsTime.reset();
+			_iter = 0;
+		}
+
+		_gui.setInputImage( _input->frame() );
+
 
 	}
 
+	inline void MIApp::selectionDidStart()
+	{
+		_selectingTemplate = true;
+	}
 
+
+	inline void MIApp::selectionFinished( const Rectf& r )
+	{
+		Recti roi( r.x * _currGray.width(),
+				   r.y * _currGray.height(),
+				   r.width * _currGray.width(),
+				   r.height * _currGray.height() );
+
+		if( roi.width < 10 || roi.height < 10 )
+			return;
+
+		Image patch( _currGray, &roi );
+
+		_tracker.updateTemplate( patch );
+		_gui.setTemplateImage( patch, 
+							   _tracker.templateGradX(), 
+							   _tracker.templateGradY() );
+		_selectingTemplate = false;
+	}
 }
---
+++ tests/mitracker/RectSelectImageView.h
@@ -0,0 +1,130 @@
+#include <cvt/gui/ImageView.h>
+#include <cvt/geom/Rect.h>
+
+namespace cvt {
+
+	class RectSelectImageView : public ImageView
+	{
+		public:
+			RectSelectImageView(); 
+			~RectSelectImageView(); 
+
+			void mouseMoveEvent( MouseMoveEvent* e );
+			void mousePressEvent( MousePressEvent* e );
+			void mouseReleaseEvent( MouseReleaseEvent* e );
+			void paintEvent( PaintEvent* e , GFX* gfx );
+			
+			// update the points that shall be drawn!
+			void updatePoints( std::vector<Vector2f> & pts, float width, float height );
+
+			Signal<const Rectf&>	selectionComplete;
+			Signal<void>			selectionDidStart;
+
+		private:
+			Vector2f _firstClick;
+			
+			// stored in normalized dimensions, s.t. we can always scale with our current dimensions!
+			Vector2f _pts[ 4 ];
+			void updateSelection( float x, float y );
+	};
+
+	inline RectSelectImageView::RectSelectImageView() : ImageView()
+	{
+		_pts[ 0 ][ 0 ] = 0;
+		_pts[ 0 ][ 1 ] = 0;
+		_pts[ 1 ][ 0 ] = 0;
+		_pts[ 1 ][ 1 ] = 0;
+		_pts[ 2 ][ 0 ] = 0;
+		_pts[ 2 ][ 1 ] = 0;
+		_pts[ 3 ][ 0 ] = 0;
+		_pts[ 3 ][ 1 ] = 0;
+	}
+
+	inline RectSelectImageView::~RectSelectImageView()
+	{
+	}
+
+	inline void RectSelectImageView::paintEvent( PaintEvent* e , GFX* gfx )
+	{
+		ImageView::paintEvent( e, gfx );
+
+		int w, h;
+
+		this->size( w, h );
+		gfx->color().set( 0.0f, 1.0f, 0.0f, 1.0f );		
+		gfx->setLineWidth( 5 );
+		gfx->drawLine( w* _pts[ 0 ][ 0 ], h* _pts[ 0 ][ 1 ], w* _pts[ 1 ][ 0 ], h* _pts[ 1 ][ 1 ] );
+		gfx->drawLine( w* _pts[ 1 ][ 0 ], h* _pts[ 1 ][ 1 ], w* _pts[ 2 ][ 0 ], h* _pts[ 2 ][ 1 ] );
+		gfx->drawLine( w* _pts[ 2 ][ 0 ], h* _pts[ 2 ][ 1 ], w* _pts[ 3 ][ 0 ], h* _pts[ 3 ][ 1 ] );
+		gfx->drawLine( w* _pts[ 3 ][ 0 ], h* _pts[ 3 ][ 1 ], w* _pts[ 0 ][ 0 ], h* _pts[ 0 ][ 1 ] );
+	}
+
+
+	inline void RectSelectImageView::updatePoints( std::vector<Vector2f> & pts, float width, float height )
+	{
+		for( size_t i = 0; i < 4; i++ ){
+			_pts[ i ].x = pts[ i ].x / width;
+			_pts[ i ].y = pts[ i ].y / height;
+		}
+		update();
+	}
+
+	inline void RectSelectImageView::updateSelection( float x, float y )
+	{
+		_pts[ 0 ][ 0 ] = _firstClick[ 0 ];
+		_pts[ 0 ][ 1 ] = _firstClick[ 1 ];
+		_pts[ 1 ][ 0 ] = x;
+		_pts[ 1 ][ 1 ] = _firstClick[ 1 ];
+		_pts[ 2 ][ 0 ] = x;
+		_pts[ 2 ][ 1 ] = y;
+		_pts[ 3 ][ 0 ] = _firstClick[ 0 ];
+		_pts[ 3 ][ 1 ] = y;
+	}
+
+	inline void RectSelectImageView::mouseMoveEvent( MouseMoveEvent* e )
+	{
+		if( ! ( e->buttonMask() & 1 ) ) return;
+
+		int w, h;
+		this->size( w, h );
+		float x = ( float )e->x / ( float )w;
+		float y = ( float )e->y / ( float )h;
+		updateSelection( x, y );			
+	}
+
+	inline void RectSelectImageView::mousePressEvent( MousePressEvent* e )
+	{
+		if( e->button() != 1 ) return;		
+
+		int w, h;
+		this->size( w, h );
+
+		_firstClick[ 0 ] = ( float )( e->x ) / ( float )w;
+		_firstClick[ 1 ] = ( float )( e->y ) / ( float )h;
+
+		updateSelection( _firstClick[ 0 ] , _firstClick[ 1 ] );
+		selectionDidStart.notify();
+	}
+
+	inline void RectSelectImageView::mouseReleaseEvent( MouseReleaseEvent* e )
+	{
+		if( e->button() != 1 ) return;
+
+		int w, h;
+		this->size( w, h );
+		float x = ( float )e->x / ( float )w;
+		float y = ( float )e->y / ( float )h;
+		updateSelection( x, y );
+
+
+		Rectf r;
+		r.x = Math::min( _pts[ 0 ][ 0 ], _pts[ 2 ][ 0 ] );
+		r.y = Math::min( _pts[ 0 ][ 1 ], _pts[ 2 ][ 1 ] );
+		r.width = Math::abs( _pts[ 1 ][ 0 ] - _pts[ 0 ][ 0 ] );
+		r.height = Math::abs( _pts[ 1 ][ 1 ] - _pts[ 2 ][ 1 ] );
+
+		selectionComplete.notify( r );
+	}
+
+
+}
