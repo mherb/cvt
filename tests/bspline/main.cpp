e77940a7b9da98dc424632a57f760af783392c9e
Philipp Heise <heise@in.tum.de>
2011-10-21 18:22:37 +0200

    Added bspline test
    	new file:   ../tests/bspline/CMakeLists.txt
    	new file:   ../tests/bspline/main.cpp

---
+++ tests/bspline/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cvt/math/BSpline.h>
+
+using namespace cvt;
+
+int main()
+{
+	for( float t = -2.5f; t <= 2.5f; t += 0.01f )
+		std::cout << t << " " << BSplinef::evalSecondDerivative( t ) << std::endl;
+	return 0;
+}
