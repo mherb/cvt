ab144c8a1463af2440bb366dc16aa1b1578c43af
Sebastian Klose <kloses@in.tum.de>
2011-10-26 13:49:12 +0200

    adapted interface of PoseTranslation
    	modified:   PoseTranslation.h

--- tests/mitracker/PoseTranslation.h
+++ tests/mitracker/PoseTranslation.h
@@ -13,28 +13,35 @@ namespace cvt
 	class PoseTranslation
 	{
 		public:
+			static const size_t NPARAMS	= 2;
+			typedef Eigen::Matrix<T, 3, 3> MatrixType;
+			typedef Eigen::Matrix<T, 3, NPARAMS> JacMatType;
+			typedef Eigen::Matrix<T, 3*NPARAMS, NPARAMS> HessMatType;
+			typedef Eigen::Matrix<T, 2, NPARAMS> ScreenJacType;
+			typedef Eigen::Matrix<T, NPARAMS, NPARAMS> ScreenHessType;
+			typedef Eigen::Matrix<T, NPARAMS, 1> ParameterVectorType;
+			typedef Eigen::Matrix<T, 3, 1> PointType;
+			
 			PoseTranslation();
 			~PoseTranslation();
 
-			const Matrix3<T> & pose() const { return _current; }
-				  Matrix3<T> & pose()		{ return _current; }
+			const MatrixType & transformation() const { return _current; }
+			MatrixType & transformation() { return _current; }
 			
+			void set( const Matrix3<T> & mat );
 
-			void screenJacobian( Eigen::Matrix<T, 2, 2>& jac, const Vector2<T>& point ) const;
+			void screenJacobian( ScreenJacType& jac, const Eigen::Matrix<T, 2, 1> & point ) const;
 
-			void screenHessian( Eigen::Matrix<T, 2, 2>& hx, 
-							    Eigen::Matrix<T, 2, 2>& hy,
-							    const Vector2<T>& point ) const;
+			void screenHessian( ScreenHessType& hx, 
+							    ScreenHessType& hy,
+							    const Eigen::Matrix<T, 2, 1> & point ) const;
 
 			/* construct the delta homography from the parameters and apply it to the current one */
-			void addDelta( const Eigen::Matrix<T, 2, 1>& delta );
-
-			void inverseCompositionalUpdate( const Eigen::Matrix<T, 2, 1>& delta );
-
-			void removeDelta( const Eigen::Matrix<T, 2, 1>& delta );
+			void apply( const ParameterVectorType& delta );
+			void applyInverse( const ParameterVectorType& delta );
 
 		private:
-			Matrix3<T>	_current;
+			Eigen::Matrix<T, 3, 3>	_current;
 	};
 
 	template <typename T>
@@ -49,40 +56,49 @@ namespace cvt
 	}
 
 	template <typename T>
+	inline void PoseTranslation<T>::set( const Matrix3<T> & mat )
+	{
+		_current( 0, 0 ) = mat[ 0 ][ 0 ];
+		_current( 0, 1 ) = mat[ 0 ][ 1 ];
+		_current( 0, 2 ) = mat[ 0 ][ 2 ];
+		_current( 1, 0 ) = mat[ 1 ][ 0 ];
+		_current( 1, 1 ) = mat[ 1 ][ 1 ];
+		_current( 1, 2 ) = mat[ 1 ][ 2 ];
+		_current( 2, 0 ) = mat[ 2 ][ 0 ];
+		_current( 2, 1 ) = mat[ 2 ][ 1 ];
+		_current( 2, 2 ) = mat[ 2 ][ 2 ];
+	}
+
+	template <typename T>
 	inline void PoseTranslation<T>::screenJacobian( Eigen::Matrix<T, 2, 2>& j, 
-												    const Vector2<T>& ) const
+												    const Eigen::Matrix<T, 2, 1>& ) const
 	{
 		j.setIdentity();
 	}
 			
 	template <typename T>
 	inline void PoseTranslation<T>::screenHessian( Eigen::Matrix<T, 2, 2>& hx, 
-												  Eigen::Matrix<T, 2, 2>& hy,
-												  const Vector2<T>& ) const
+												   Eigen::Matrix<T, 2, 2>& hy,
+												   const Eigen::Matrix<T, 2, 1>& ) const
 	{
 		hx.setZero();
 		hy.setZero();
 	}
 
 	template <typename T>
-	inline void PoseTranslation<T>::addDelta( const Eigen::Matrix<T, 2, 1>& delta )
+	inline void PoseTranslation<T>::apply( const Eigen::Matrix<T, 2, 1>& delta )
 	{
-		_current[ 0 ][ 2 ] += delta[ 0 ];
-		_current[ 1 ][ 2 ] += delta[ 1 ];
+		_current( 0 , 2 ) += delta[ 0 ];
+		_current( 1 , 2 ) += delta[ 1 ];
 	}
 
 	template <typename T>
-	inline void PoseTranslation<T>::removeDelta( const Eigen::Matrix<T, 2, 1>& delta )
+	inline void PoseTranslation<T>::applyInverse( const Eigen::Matrix<T, 2, 1>& delta )
 	{
-		_current[ 0 ][ 2 ] -= delta[ 0 ];
-		_current[ 1 ][ 2 ] -= delta[ 1 ];
+		_current( 0 , 2 ) += delta[ 0 ];
+		_current( 1 , 2 ) += delta[ 1 ];
 	}
 		
-	template <typename T>	
-	inline void PoseTranslation<T>::inverseCompositionalUpdate( const Eigen::Matrix<T, 2, 1>& delta )
-	{
-		removeDelta( delta );
-	}
 }
 
 #endif
