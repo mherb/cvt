0ca139f10108545d70fbb1500ebbe0db783a7a7a
Philipp Heise <heise@in.tum.de>
2011-10-26 12:51:15 +0200

    BSpline test ( newton convergence ... )
    	modified:   ../tests/bspline/main.cpp

--- tests/bspline/main.cpp
+++ tests/bspline/main.cpp
@@ -6,6 +6,11 @@ using namespace cvt;
 int main()
 {
 	for( float t = -2.5f; t <= 2.5f; t += 0.01f )
-		std::cout << t << " " << BSplinef::evalSecondDerivative( t ) << std::endl;
+		std::cout << t
+				  << " " << BSplinef::eval( t )
+				  << " " << BSplinef::evalDerivative( t )
+				  << " " << BSplinef::evalSecondDerivative( t )
+				  << " " << -BSplinef::evalDerivative( t ) / BSplinef::evalSecondDerivative( t )
+				  << std::endl;
 	return 0;
 }
