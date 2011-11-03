d9e48924b718473c9937b025a67abeca4d284f4a
Sebastian Klose <kloses@in.tum.de>
2011-11-03 13:09:13 +0100

    viewport hack
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/gfx/GFX.h
    	modified:   ../cvt/gui/GLView.h
    	modified:   ../tests/glview/glview.cpp

--- cvt/gui/GLView.h
+++ cvt/gui/GLView.h
@@ -8,20 +8,32 @@ namespace cvt
 	class GLView : public Widget
 	{
 		public:
-			GLView(){}
+			GLView() : 
+				_viewport( rect() ),
+				_allowViewportChange( false )
+			{
+			}
+
 			virtual ~GLView(){}
+
+			void setViewport( const Recti & rect );
+			void setViewport( int x, int y, size_t w, size_t h );
 
 		protected:
 			void paintEvent( PaintEvent* ev, GFX* g );
 			virtual void paintGLEvent( PaintEvent* ev ) = 0;
+
+			Recti	_viewport;
+			bool	_allowViewportChange;
 	};
 
-	inline void GLView::paintEvent( PaintEvent* ev, GFX* g )
+	inline void GLView::setViewport( int x, int y, size_t w, size_t h )
 	{
-		if( g->supportsGL() ){
-			g->beginGL();
-			paintGLEvent( ev );
-			g->endGL();
+		if( _allowViewportChange ) {
+			Recti r = rect();
+			Recti view( x + r.x, y + r.y, w, h );
+			r.intersect( view );
+			glViewport( r.x, _viewport.height - ( r.y + r.height ), r.width, r.height );
 		}
 	}
 } 
