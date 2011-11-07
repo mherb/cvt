abae90a884b8d6a2a22d7d0280e67f56581dece6
Philipp Heise <heise@in.tum.de>
2011-11-07 15:24:31 +0100

    OpenCL ROFFGP
    	modified:   ../../cvt/CMakeLists.txt
    	new file:   ../../cvt/cl/kernel/fgp/fgp_calce.cl
    	new file:   ../../cvt/cl/kernel/fgp/fgp_calcn.cl
    	new file:   ../../cvt/cl/kernel/fgp/fgp_calcu.cl
    	new file:   ../../cvt/cl/kernel/fgp/fgp_clear.cl
    	new file:   ../../cvt/gfx/ifilter/ROFFGPFilter.cpp
    	new file:   ../../cvt/gfx/ifilter/ROFFGPFilter.h
    	modified:   ../ROFFGP/main.cpp

---
+++ cvt/gfx/ifilter/ROFFGPFilter.h
@@ -0,0 +1,26 @@
+#ifndef CVT_ROFFGPFILTER_H
+#define CVT_ROFFGPFILTER_H
+
+#include <cvt/gfx/IFilter.h>
+#include <cvt/cl/CLKernel.h>
+
+namespace cvt {
+	class ROFFGPFilter : public IFilter {
+		public:
+			ROFFGPFilter();
+			void apply( Image& dst, const Image& src, float lambda = 0.05f, size_t iter = 10 ) const;
+			void apply( const ParamSet* set, IFilterType t = IFILTER_CPU ) const;
+
+		private:
+			mutable Image _imgn;
+			mutable Image _imge0;
+			mutable Image _imge1;
+
+			CLKernel _clfgpclear;
+			CLKernel _clfgpcalcu;
+			CLKernel _clfgpcalce;
+			CLKernel _clfgpcalcn;
+	};
+}
+
+#endif
