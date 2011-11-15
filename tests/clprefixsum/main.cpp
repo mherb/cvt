dafb381f98b5a00e7d08e012a8924a0e44532b0c
Philipp Heise <heise@in.tum.de>
2011-11-15 12:03:29 +0100

    Ooops, opencl prefixsum is already extremly fast ...
    	modified:   ../tests/clprefixsum/main.cpp

--- tests/clprefixsum/main.cpp
+++ tests/clprefixsum/main.cpp
@@ -17,95 +17,105 @@ using namespace cvt;
 
 int main( int argc, char** argv )
 {
-	if( argc != 2 ) {
+	/*	if( argc != 2 ) {
 		std::cout << "usage: " << argv[ 0 ] << " image" << std::endl;
 		return 0;
-	}
+		}*/
 
 	std::vector<CLPlatform> platforms;
 	CLPlatform::get( platforms );
 	CL::setDefaultDevice( platforms[ 0 ].defaultDevice() );
 
-	Image input( argv[ 1 ] );
-	Image climg( input.width(), input.height(), input.format(), IALLOCATOR_CL );
-	input.convert( climg );
-//	climg.fill( Color::WHITE );
-
-	Image output( input.width(), input.height(), IFormat::floatEquivalent( input.format() ), IALLOCATOR_CL );
+	//	Image input( argv[ 1 ] );
 
 	try {
-		Time t;
-		CLKernel kern( _prefixsum_pblock_source, "prefixsum_pblock" );
-		kern.setArg( 0, output );
-		kern.setArg( 1, climg );
-		kern.setArg( 2, CLLocalSpace( sizeof( cl_float4 ) * 32 * 32 ) );
-		kern.run( CLNDRange( Math::pad( input.width(), 32 ), Math::pad( input.height(), 32 ) ), CLNDRange( 32, 32 ) );
 
-		CLKernel kern2( _prefixsum_horiz_source, "prefixsum_horiz" );
-		kern2.setArg( 0, output );
-		kern2.setArg( 1, output );
-		kern2.setArg<int>( 2, 32 );
-		kern2.run( CLNDRange( Math::pad( input.height(), 32 ) ), CLNDRange( 32 ) );
+			CLKernel kern( _prefixsum_pblock_source, "prefixsum_pblock" );
 
-		CLKernel kern3( _prefixsum_vert_source, "prefixsum_vert" );
-		kern3.setArg( 0, output );
-		kern3.setArg( 1, output );
-		kern3.setArg<int>( 2, 32 );
-		kern3.run( CLNDRange( Math::pad( input.width(), 32 ) ), CLNDRange( 32 ) );
+			CLKernel kern2( _prefixsum_horiz_source, "prefixsum_horiz" );
 
-		CLKernel kern4( _prefixsum_block2_source, "prefixsum_block2" );
-		kern4.setArg( 0, output );
-		kern4.setArg( 1, output );
-		kern4.setArg( 2, CLLocalSpace( sizeof( cl_float4 ) * 32 ) );
-		kern4.setArg( 3, CLLocalSpace( sizeof( cl_float4 ) * 32 ) );
-		kern4.run( CLNDRange( Math::pad( input.width(), 32 ), Math::pad( input.height(), 32 ) ), CLNDRange( 32, 32 ) );
+			CLKernel kern3( _prefixsum_vert_source, "prefixsum_vert" );
 
-		std::cout << "Time: " << t.elapsedMilliSeconds() << std::endl;
+			CLKernel kern4( _prefixsum_block2_source, "prefixsum_block2" );
 
-		output.save( "prefixsum.png" );
+		for( int size = 64; size <= 4096; size++ ) {
+			Image climg( size, size, IFormat::GRAY_FLOAT, IALLOCATOR_CL );
+			//	input.convert( climg );
+			climg.fill( Color::WHITE );
+			Image output( size, size, IFormat::GRAY_FLOAT, IALLOCATOR_CL );
 
-		Image iicpu( input.width(), input.height(), IFormat::floatEquivalent( input.format() ) );
-		Image bla( input );
-		bla.integralImage( iicpu );
-		iicpu.mul( 1.0f / 255.0f );
-		std::cout << "SSD:"  << iicpu.ssd( output ) / ( float )( input.width() * input.height() ) << std::endl;
-		output.sub( iicpu );
-		output.save( "bla.png" );
-	//	climg.integralImage( iicpu );
+			Time t;
+			kern.setArg( 0, output );
+			kern.setArg( 1, climg );
+			kern.setArg( 2, CLLocalSpace( sizeof( cl_float4 ) * 32 * 32 ) );
+			kern.run( CLNDRange( Math::pad( size, 32 ), Math::pad( size, 32 ) ), CLNDRange( 32, 32 ) );
+
+			kern2.setArg( 0, output );
+			kern2.setArg( 1, output );
+			kern2.setArg<int>( 2, 32 );
+			kern2.run( CLNDRange( Math::pad( size, 32 ) ), CLNDRange( 32 ) );
+
+			kern3.setArg( 0, output );
+			kern3.setArg( 1, output );
+			kern3.setArg<int>( 2, 32 );
+			kern3.run( CLNDRange( Math::pad( size, 32 ) ), CLNDRange( 32 ) );
+
+			kern4.setArg( 0, output );
+			kern4.setArg( 1, output );
+			kern4.setArg( 2, CLLocalSpace( sizeof( cl_float4 ) * 32 ) );
+			kern4.setArg( 3, CLLocalSpace( sizeof( cl_float4 ) * 32 ) );
+			kern4.run( CLNDRange( Math::pad( size, 32 ), Math::pad( size, 32 ) ), CLNDRange( 32, 32 ) );
+
+			std::cout << size << " " << t.elapsedMilliSeconds() << std::endl;
+
 #if 0
-		{
-			size_t stride;
-			float* data = output.map<float>( &stride );
-			float* dptr = data;
-			for( int y = 0; y < 96; y++ ) {
-				for( int x = 0; x < 96; x++ ) {
-					std::cout << std::setw( 6 ) << std::setprecision( 5 ) << dptr[ x ] << " ";
-				}
-				std::cout << std::endl;
-				dptr += stride;
-			}
-			output.unmap( data );
-		}
+			output.save( "prefixsum.png" );
 
-
-		{
-			size_t stride;
-			float* data = iicpu.map<float>( &stride );
-			float* dptr = data;
-			for( int y = 0; y < 96; y++ ) {
-				for( int x = 0; x < 96; x++ ) {
-					std::cout << std::setw( 6 ) << std::setprecision( 5 ) << dptr[ x ] << " ";
-				}
-				std::cout << std::endl;
-				dptr += stride;
-			}
-			iicpu.unmap( data );
-		}
+			Image iicpu( input.width(), input.height(), IFormat::floatEquivalent( input.format() ) );
+			Image bla( input );
+			bla.integralImage( iicpu );
+			iicpu.mul( 1.0f / 255.0f );
+			std::cout << "SSD:"  << iicpu.ssd( output ) / ( float )( input.width() * input.height() ) << std::endl;
+			output.sub( iicpu );
+			output.save( "bla.png" );
+			//	climg.integralImage( iicpu );
 #endif
+
+#if 0
+			{
+				size_t stride;
+				float* data = output.map<float>( &stride );
+				float* dptr = data;
+				for( int y = 0; y < 96; y++ ) {
+					for( int x = 0; x < 96; x++ ) {
+						std::cout << std::setw( 6 ) << std::setprecision( 5 ) << dptr[ x ] << " ";
+					}
+					std::cout << std::endl;
+					dptr += stride;
+				}
+				output.unmap( data );
+			}
+
+
+			{
+				size_t stride;
+				float* data = iicpu.map<float>( &stride );
+				float* dptr = data;
+				for( int y = 0; y < 96; y++ ) {
+					for( int x = 0; x < 96; x++ ) {
+						std::cout << std::setw( 6 ) << std::setprecision( 5 ) << dptr[ x ] << " ";
+					}
+					std::cout << std::endl;
+					dptr += stride;
+				}
+				iicpu.unmap( data );
+			}
+#endif
+
+		}
 	} catch( CLException& e ) {
 		std::cout << e.what() << std::endl;
 	}
-
 
 	return 0;
 }
