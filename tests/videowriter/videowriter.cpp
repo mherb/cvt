f53346fe2f369fee87ea45c706051767f230dd6f
Sebastian Klose <kloses@in.tum.de>
2011-11-17 15:23:54 +0100

    videowriter writes camera data
    	modified:   ../tests/videowriter/videowriter.cpp

--- tests/videowriter/videowriter.cpp
+++ tests/videowriter/videowriter.cpp
@@ -1,30 +1,73 @@
 #include <cvt/io/RawVideoWriter.h>
+#include <cvt/io/Camera.h>
 #include <cvt/util/Time.h>
 
 using namespace cvt;
 
-int main( int argc, char* argv[] )
+Camera* selectCam()
 {
-	if( argc < 2 ){
-		std::cout << "Usage: " << argv[ 0 ] << " <img>" << std::endl;
-		return 1;
+    Camera::updateInfo();
+	size_t numCams = Camera::count();
+
+	std::cout << "Overall number of Cameras: " << numCams << std::endl;
+	if( numCams == 0 ){
+		std::cout << "Please connect a camera!" << std::endl;
+		return 0;
 	}
 
-	Image img( argv[ 1 ] );
+	for( size_t i = 0; i < numCams; i++ ){
+		const CameraInfo & info = Camera::info( i );
+		std::cout << "Camera " << i << ": " << info << std::endl;
+	}
+
+	size_t selection = numCams;
+	if( numCams == 1 )
+		selection = 0;
+	else {
+		std::cout << "Select camera: ";
+		std::cin >> selection;
+		while ( selection >= numCams ){
+			std::cout << "Index out of bounds -> select camera in Range:";
+			std::cin >> selection;
+		}
+	}
+
+    Camera * cam = 0;
+
+    try {
+		cam = Camera::get( selection, 640, 480, 60, IFormat::UYVY_UINT8 );
+	} catch( cvt::Exception e ) {
+		std::cout << e.what() << std::endl;
+        return 0;
+	}
+
+    return cam;
+}
+
+int main()
+{
+
+	Camera * cam = selectCam();
+	
+	cam->startCapture();
+	cam->nextFrame();
 
 	try {
 		RawVideoWriter writer( "test.rawvid" );
 
-		int i = 0;
 		Time t;
-		while( i++ < 500 ){
-			writer.write( img );
+		while( t.elapsedSeconds() < 20 ){
+			cam->nextFrame();
+			writer.write( cam->frame() );
 		}
+		std::cout << "Done" << std::endl;
 
-		std::cout << "Avg. Time / frame: " << t.elapsedMilliSeconds() / ( 1000 * float (i) ) << std::endl;
 	} catch ( const Exception & e ){
 		std::cout << e.what() << std::endl;
 	}
+
+	cam->stopCapture();
+	delete cam;
 
 	return 0;
 }
