90efc9b256080e3163368f98094a5c214df16eed
Philipp Heise <heise@in.tum.de>
2011-11-23 13:38:34 +0100

    More testing in ploview example
    	modified:   ../tests/plotview/main.cpp

--- tests/plotview/main.cpp
+++ tests/plotview/main.cpp
@@ -32,10 +32,19 @@ int main()
 	w.setMinimumSize( 320, 240 );
 
 	PlotDataSamples pds;
-	for( float x = -3.0f; x <= 3.0f; x += 0.25f )
+	for( float x = -3.0f; x <= 3.0f; x += 0.05f )
 		pds.addSample( Point2f( x, x * x ) );
 	pds.setPlotStyle( PLOT_STYLE_DOTS );
+
+	PlotDataSamples pds2;
+	for( float x = -3.0f; x <= 3.0f; x += 0.05f )
+		pds2.addSample( Point2f( x, 0.5f * x * x - 1.0f ) );
+	pds2.setPlotColor( Color::GREEN );
+	pds2.setPlotStyle( PLOT_STYLE_LINES );
+
+
 	pview.addPlotData( &pds );
+	pview.addPlotData( &pds2 );
 
 	Application::run();
 	return 0;
