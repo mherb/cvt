368ec79a96a3bd567dbcf27ece3400a36fe288ee
Philipp Heise <heise@in.tum.de>
2011-11-24 16:23:16 +0100

    Added Plot stuff
    	new file:   ../cvt/gui/PlotData.h
    	new file:   ../cvt/gui/PlotDataSamples.h
    	new file:   ../cvt/gui/PlotView.cpp
    	new file:   ../cvt/gui/PlotView.h

---
+++ cvt/gui/PlotDataSamples.h
@@ -0,0 +1,57 @@
+#ifndef CVT_PLOTDATASAMPLES_H
+#define CVT_PLOTDATASAMPLES_H
+
+#include <cvt/gui/PlotData.h>
+
+namespace cvt {
+	class PlotDataSamples : public PlotData 
+	{
+	 	public:
+			PlotDataSamples();
+			~PlotDataSamples();
+	
+			void addSample( const Point2f& pt );
+			void clear();
+
+			void dataInRectWithStyle( std::vector<Point2f>& data, const Rectf& rect, PlotStyle style ) const;
+
+		private:
+			std::vector<Point2f> _samples;
+	};
+
+	inline PlotDataSamples::PlotDataSamples()
+	{
+	}
+
+	
+	inline PlotDataSamples::~PlotDataSamples()
+	{
+	}
+
+	inline void PlotDataSamples::addSample( const Point2f& pt )
+	{
+		_samples.push_back( pt );
+		changed.notify( this );
+	}
+
+	inline void PlotDataSamples::clear()
+	{
+		_samples.clear();
+		changed.notify( this );
+	}
+
+	inline void PlotDataSamples::dataInRectWithStyle( std::vector<Point2f>& data, const Rectf& rect, PlotStyle style ) const
+	{
+		switch( style ) {
+			case PLOT_STYLE_LINES:
+				pointsInRectToLines( data, rect, &_samples[ 0 ], _samples.size() );
+				return;
+			case PLOT_STYLE_DOTS:
+				pointsInRect( data, rect, &_samples[ 0 ], _samples.size() );
+				return;
+		}
+	}
+
+};
+
+#endif
