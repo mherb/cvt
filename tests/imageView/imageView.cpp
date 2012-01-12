c8c67455df2680cd5ebf83f4a99fa56217e7b8db
Sebastian Klose <kloses@in.tum.de>
2012-01-12 17:13:55 +0100

    simple image viewer
    	modified:   CMakeLists.txt
    	new file:   imageView/CMakeLists.txt
    	new file:   imageView/imageView.cpp

---
+++ tests/imageView/imageView.cpp
@@ -0,0 +1,35 @@
+#include <cvt/gfx/Image.h>
+#include <cvt/gui/ImageView.h>
+#include <cvt/gui/Window.h>
+#include <cvt/gui/WidgetLayout.h>
+#include <cvt/gui/Application.h>
+
+using namespace cvt;
+
+int main( int argc, char* argv[] )
+{
+	if( argc < 2 ){
+		std::cout << "Usage: " << argv[ 0 ] << " <imagefile>" << std::endl;
+		return 0;
+	}
+
+	Image img( argv[ 1 ] );
+
+	Window w( "imageView" );
+	w.setSize( 320, 240 );
+	ImageView imview;
+	imview.setSize( 320, 240 );
+	imview.setImage( img );
+
+	WidgetLayout wl;
+	wl.setAnchoredTopBottom( 0, 0 );
+	wl.setAnchoredLeftRight( 0, 0 );
+	w.addWidget( &imview, wl );
+	w.setVisible( true );
+
+	w.update();
+
+	Application::run();
+
+	return 0;
+}
