83e3b9d4b10e127610fb877bd654b015370be60c
Sebastian Klose <kloses@in.tum.de>
2012-01-25 08:25:49 +0100

    feature stuff + findscripts
    	new file:   cmake/FindOpenNI.cmake
    	modified:   tests/feature_dataset/feature_dataset.cpp
    	deleted:    tests/features/FeatureTracker.cpp
    	deleted:    tests/features/FeatureTracker.h
    	modified:   tests/features/features.cpp
    	new file:   tests/openni/CMakeLists.txt
    	new file:   tests/openni/OpenNiWin.h
    	new file:   tests/openni/openni.cpp

---
+++ tests/openni/OpenNiWin.h
@@ -0,0 +1,82 @@
+#ifndef CVT_OPENNI_WIN_H
+#define CVT_OPENNI_WIN_H
+
+#include <cvt/gui/TimeoutHandler.h>
+#include <cvt/gui/Window.h>
+#include <cvt/gui/ImageView.h>
+#include <cvt/gui/Moveable.h>
+#include <cvt/io/OpenNICamera.h>
+#include <cvt/util/Time.h>
+
+namespace cvt
+{
+	class OpenNiWin : public TimeoutHandler
+	{
+		public:
+			OpenNiWin();
+			~OpenNiWin();
+
+			void onTimeout();
+
+		private:
+			uint32_t		_timerId;
+			Window			_window;
+			ImageView		_rgbView;
+			ImageView		_depthView;
+			Moveable		_rgbMov;
+			Moveable		_depthMov;
+			OpenNICamera	_cam;
+
+			Time			_time;
+			size_t			_iters;
+	};
+
+	inline OpenNiWin::OpenNiWin() :
+		_window( "OpenNI" ),
+		_rgbMov( &_rgbView ),
+		_depthMov( &_depthView )
+	{
+		_timerId = Application::registerTimer( 15, this );
+
+		_window.setSize( 800, 600 );
+		_depthMov.setSize( 320, 240 );
+		_rgbMov.setSize( 320, 240 );
+
+		_depthMov.setTitle( "Depth" );
+		_rgbMov.setTitle( "Image" );
+
+		_window.addWidget( &_depthMov );
+		_window.addWidget( &_rgbMov );
+
+		_window.setVisible( true );
+		_window.update();
+
+		_cam.startCapture();
+	}
+
+	inline OpenNiWin::~OpenNiWin()
+	{
+		Application::unregisterTimer( _timerId );
+		_cam.stopCapture();
+	}
+
+	inline void OpenNiWin::onTimeout()
+	{
+		_cam.nextFrame();
+
+		_iters++;
+		double t;
+		if( ( t = _time.elapsedSeconds() ) > 3.0 ){
+			String title;
+			title.sprintf( "OpenNI - FPS: %0.1f", _iters / t );
+			_window.setTitle( title );
+			_iters = 0;
+			_time.reset();
+		}
+
+		_rgbView.setImage( _cam.frame() );
+		_depthView.setImage( _cam.depth() );
+	}
+}
+
+#endif
---
+++ tests/openni/openni.cpp
@@ -0,0 +1,14 @@
+#include <cvt/io/OpenNICamera.h>
+
+#include <cvt/gui/Application.h>
+#include "OpenNiWin.h"
+
+using namespace cvt;
+
+int main()
+{
+	OpenNiWin win;
+	Application::run();
+
+	return 0;
+}
