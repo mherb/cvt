1ac3b8e077c06a68199b0c8490dfabc284c48c7e
Sebastian Klose <kloses@in.tum.de>
2012-02-06 16:18:45 +0100

    openni playing
    	modified:   ../cvt/CMakeLists.txt
    	modified:   ../cvt/gfx/Image.h
    	modified:   ../cvt/io/OpenNICamera.cpp
    	modified:   ../cvt/io/OpenNICamera.h
    	new file:   ../cvt/io/OpenNIManager.cpp
    	new file:   ../cvt/io/OpenNIManager.h
    	modified:   ../tests/openni/openni.cpp

--- tests/openni/openni.cpp
+++ tests/openni/openni.cpp
@@ -7,8 +7,22 @@ using namespace cvt;
 
 int main()
 {
+	size_t numCams = OpenNICamera::count();
+	std::cout << "Num Openni cams: " << numCams << std::endl;
+
+	/*
+	CameraInfo camInfo;
+	for( size_t i = 0; i < numCams; i++ ){
+		OpenNICamera::cameraInfo( i, camInfo );
+	}
+	*/
+
+	/*
+	if( numCams == 0 )
+		return 0;
+
 	OpenNiWin win;
 	Application::run();
-
+*/
 	return 0;
 }
