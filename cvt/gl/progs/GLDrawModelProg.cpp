42afd0d60c1b12677da6877efde9fcbb31252246
Philipp Heise <heise@in.tum.de>
2012-02-21 16:16:43 +0100

    Added HEADER to sources ...

--- cvt/gl/progs/GLDrawModelProg.cpp
+++ cvt/gl/progs/GLDrawModelProg.cpp
@@ -1,3 +1,13 @@
+/*
+			CVT - Computer Vision Tools Library
+
+ 	 Copyright (c) 2012, Philipp Heise, Sebastian Klose
+
+ 	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ 	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ 	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ 	PARTICULAR PURPOSE.
+ */
 #include <cvt/gl/progs/GLDrawModelProg.h>
 
 #include <cvt/gl/shader/120/basiclight_120_vert.h>
