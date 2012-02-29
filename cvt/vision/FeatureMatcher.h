67da82902bdfbb641b32d2fa2ed216326d2b1890
Sebastian Klose <kloses@in.tum.de>
2012-02-29 13:22:34 +0100

    stitching tests
    	modified:   cvt/vision/FeatureMatcher.h
    	modified:   tests/CMakeLists.txt
    	new file:   tests/homoStitch/CMakeLists.txt
    	new file:   tests/homoStitch/HomographyStitching.cpp
    	new file:   tests/homoStitch/HomographyStitching.h
    	new file:   tests/homoStitch/StitchApp.h
    	new file:   tests/homoStitch/homoStitch.cpp

--- cvt/vision/FeatureMatcher.h
+++ cvt/vision/FeatureMatcher.h
@@ -14,21 +14,29 @@
 #include <cvt/vision/FeatureMatch.h>
 #include <cvt/vision/ORB2.h>
 #include <cvt/vision/ORB.h>
+#include <set>
 
 namespace cvt
 {
 	class FeatureMatcher
 	{
 		public:
-			template <class FeatureType>
+			template <class FeatureContainerType0, class FeatureContainerType1>
 			static void matchBruteForce( std::vector<FeatureMatch> & matches, 
-										 const FeatureType & f0, 
-										 const FeatureType & f1,
+										 const FeatureContainerType0 & f0, 
+										 const FeatureContainerType1 & f1,
 										 float maxDistance );
+			
+			template <class FeatureContainerType0, class FeatureContainerType1>
+			static void matchWithWindow( std::vector<FeatureMatch> & matches, 
+									     const FeatureContainerType0 & f0, 
+									     const FeatureContainerType1 & f1,
+										 float maxDistance, float maxDescDistance );
+			
 	};
 
 	template<>
-	inline void FeatureMatcher::matchBruteForce<ORB2>( std::vector<FeatureMatch> & matches, 
+	inline void FeatureMatcher::matchBruteForce<ORB2, ORB2>( std::vector<FeatureMatch> & matches, 
 													   const ORB2 & orb0, 
 													   const ORB2 & orb1, 
 													   float maxDistance )
@@ -76,25 +84,93 @@ namespace cvt
 
 
 	template<>
-	inline void FeatureMatcher::matchBruteForce<ORB>( std::vector<FeatureMatch> & matches, 
+	inline void FeatureMatcher::matchBruteForce<ORB, ORB>( std::vector<FeatureMatch> & matches, 
 													  const ORB & orb0, 
 													  const ORB & orb1, 
 													  float maxDistance )
 	{
 		FeatureMatch m;
+		std::set<size_t> assigned;
 		for( size_t i = 0; i < orb0.size(); i++ ){
 			m.feature0 = &orb0[ i ];
 			m.distance = maxDistance;
 			for( size_t k = 0; k < orb1.size(); k++ ){
+				if( assigned.find( k ) != assigned.end() )
+					continue;
+
 				size_t dist = orb0[ i ].distance( orb1[ k ] );
 
 				if( dist < m.distance ){
 					m.distance = dist;
 					m.feature1 = &orb1[ k ];
+					assigned.insert( k );
 				}
 			}
 
 			if( m.distance < maxDistance ){
+				matches.push_back( m );
+			}
+		}
+	}
+
+
+	template<>
+	inline void FeatureMatcher::matchBruteForce<ORB, std::vector<ORBFeature> >( std::vector<FeatureMatch> & matches, 
+																			    const ORB & orb0, 
+																				const std::vector<ORBFeature> & orb1, 
+																				float maxDistance )
+	{
+		FeatureMatch m;
+		std::set<size_t> assigned;
+		for( size_t i = 0; i < orb0.size(); i++ ){
+			m.feature0 = &orb0[ i ];
+			m.distance = maxDistance;
+			for( size_t k = 0; k < orb1.size(); k++ ){
+				if( assigned.find( k ) != assigned.end() )
+					continue;
+
+				size_t dist = orb0[ i ].distance( orb1[ k ] );
+
+				if( dist < m.distance ){
+					m.distance = dist;
+					m.feature1 = &orb1[ k ];
+					assigned.insert( k );
+				}
+			}
+
+			if( m.distance < maxDistance ){
+				matches.push_back( m );
+			}
+		}
+	}
+
+	template<>
+	inline void FeatureMatcher::matchWithWindow<ORB, std::vector<ORBFeature> >( std::vector<FeatureMatch> & matches, 
+																			    const ORB & orb0, 
+																				const std::vector<ORBFeature> & orb1, 
+																				float maxDistance, float maxDescDistance )
+	{
+		float distThreshSqr = Math::sqr( maxDistance );
+		FeatureMatch m;
+		std::set<size_t> assigned;
+		for( size_t i = 0; i < orb0.size(); i++ ){
+			m.feature0 = &orb0[ i ];
+			m.distance = maxDescDistance;
+			for( size_t k = 0; k < orb1.size(); k++ ){
+				if( assigned.find( k ) != assigned.end() ||
+					( orb0[ i ].pt - orb1[ k ].pt ).lengthSqr() > distThreshSqr )
+					continue;
+
+				size_t dist = orb0[ i ].distance( orb1[ k ] );
+
+				if( dist < m.distance ){
+					m.distance = dist;
+					m.feature1 = &orb1[ k ];
+					assigned.insert( k );
+				}
+			}
+
+			if( m.distance < maxDescDistance ){
 				matches.push_back( m );
 			}
 		}
