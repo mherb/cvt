393a97d1be97effd5a37b2e652c34225192ae5d0
Philipp Heise <heise@in.tum.de>
2012-03-01 16:50:35 +0100

    ... etschi-betschi  ...
    	modified:   ../cvt/gfx/ifilter/IWarp.h

--- cvt/gfx/ifilter/IWarp.h
+++ cvt/gfx/ifilter/IWarp.h
@@ -12,6 +12,7 @@
 #define CVT_IWARP_H
 
 #include <cvt/gfx/IFilter.h>
+#include <cvt/gfx/IMapScoped.h>
 #include <cvt/util/Plugin.h>
 #include <cvt/util/PluginManager.h>
 #include <cvt/vision/CameraCalibration.h>
@@ -41,6 +42,13 @@ namespace cvt {
 			 */
 			static void warpUndistort( Image& idst, Matrix3f& Knew, const CameraCalibration& calib, size_t srcWidth, size_t srcHeight, float alpha = 0.0f, float beta = 0.0f );
 
+			/*
+			   @brief Generic warp function
+			   @param op a generic function object 'Vector2f function( const Vector2f& in )'
+			 */
+			template<typename TFUNC>
+			static void warpGeneric( Image& idst, TFUNC op );
+
 		private:
 			static void applyFC1( Image& dst, const Image& src, const Image& warp );
 			static void applyFC4( Image& dst, const Image& src, const Image& warp );
@@ -49,6 +57,30 @@ namespace cvt {
 
 			IWarp( const IWarp& t );
 	};
+
+	template<typename TFUNC>
+	inline void IWarp::warpGeneric( Image& idst, TFUNC op )
+	{
+		if( idst.format() != IFormat::GRAYALPHA_FLOAT )
+			throw CVTException( "Unsupported warp image type" );
+
+		float* dst;
+		size_t w, h;
+
+		IMapScoped<float> map( idst );
+		w = idst.width();
+		h = idst.height();
+
+		for( size_t y = 0; y < h; y++ ) {
+			dst = map.ptr();
+			for( size_t x = 0; x < w; x++ ) {
+				Vector2f p = op( Vector2f( x, y ) );
+				*dst++ = Math::clamp<float>( p.x, 0, w - 1 );
+				*dst++ = Math::clamp<float>( p.y, 0, h - 1 );
+			}
+			map++;
+		}
+	}
 }
 
 #endif
