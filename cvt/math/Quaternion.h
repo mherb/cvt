daa4d3c21f1b5a8cafd5fcce89ca3f98d7536f8b
Philipp Heise <heise@in.tum.de>
2012-03-01 12:43:12 +0100

    Fixed rotations & removed frameRotate
    	modified:   ../cvt/math/Matrix3.h
    	modified:   ../cvt/math/Matrix4.h
    	modified:   ../cvt/math/Quaternion.h

--- cvt/math/Quaternion.h
+++ cvt/math/Quaternion.h
@@ -347,15 +347,15 @@ namespace cvt {
 			wz = w * z2;
 
 			mat[ 0 ][ 0 ] = ( T ) 1 - ( yy + zz );
-			mat[ 0 ][ 1 ] = xy + wz;
-			mat[ 0 ][ 2 ] = xz - wy;
+			mat[ 0 ][ 1 ] = xy - wz;
+			mat[ 0 ][ 2 ] = xz + wy;
 
-			mat[ 1 ][ 0 ] = xy - wz;
+			mat[ 1 ][ 0 ] = xy + wz;
 			mat[ 1 ][ 1 ] = ( T ) 1 - ( xx + zz );
-			mat[ 1 ][ 2 ] = yz + wx;
+			mat[ 1 ][ 2 ] = yz - wx;
 
-			mat[ 2 ][ 0 ] = xz + wy;
-			mat[ 2 ][ 1 ] = yz - wx;
+			mat[ 2 ][ 0 ] = xz - wy;
+			mat[ 2 ][ 1 ] = yz + wx;
 			mat[ 2 ][ 2 ] = ( T ) 1 - ( xx + yy );
 
 			return mat;
@@ -387,19 +387,19 @@ namespace cvt {
 			wz = w * z2;
 
 			mat[ 0 ][ 0 ] = ( T ) 1 - ( yy + zz );
-			mat[ 0 ][ 1 ] = xy + wz;
-			mat[ 0 ][ 2 ] = xz - wy;
+			mat[ 0 ][ 1 ] = xy - wz;
+			mat[ 0 ][ 2 ] = xz + wy;
 			mat[ 0 ][ 3 ] = 0;
 
-			mat[ 1 ][ 0 ] = xy - wz;
+			mat[ 1 ][ 0 ] = xy + wz;
 			mat[ 1 ][ 1 ] = ( T ) 1 - ( xx + zz );
-			mat[ 1 ][ 2 ] = yz + wx;
+			mat[ 1 ][ 2 ] = yz - wx;
 			mat[ 1 ][ 3 ] = 0;
 
-			mat[ 2 ][ 0 ] = xz + wy;
-			mat[ 2 ][ 1 ] = yz - wx;
+			mat[ 2 ][ 0 ] = xz - wy;
+			mat[ 2 ][ 1 ] = yz + wx;
 			mat[ 2 ][ 2 ] = ( T ) 1 - ( xx + yy );
-			mat[ 2 ][ 3 ] = 0;
+			mat[ 3 ][ 3 ] = ( T ) 1;
 
 			mat[ 3 ][ 0 ] = 0;
 			mat[ 3 ][ 1 ] = 0;
