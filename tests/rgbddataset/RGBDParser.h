a447c08e4f7a3a83ad0b23bb1add8c3fe16cddd4
Sebastian Klose <kloses@in.tum.de>
2012-03-06 10:41:06 +0100

    class for loading the cremers rgbd datasets
    	modified:   CMakeLists.txt
    	new file:   rgbddataset/CMakeLists.txt
    	new file:   rgbddataset/RGBDParser.cpp
    	new file:   rgbddataset/RGBDParser.h
    	new file:   rgbddataset/rgbddataset.cpp

---
+++ tests/rgbddataset/RGBDParser.h
@@ -0,0 +1,63 @@
+#ifndef CVT_RGBDPARSER_H
+#define CVT_RGBDPARSER_H
+
+#include <cvt/util/String.h>
+#include <cvt/util/Data.h>
+#include <cvt/util/DataIterator.h>
+#include <cvt/io/FileSystem.h>
+#include <cvt/gfx/Image.h>
+#include <cvt/math/Matrix.h>
+
+namespace cvt
+{
+	class RGBDParser
+	{
+		public:
+			struct RGBDSample
+			{
+				Image		rgb;
+				Image		depth;
+				Matrix4d	pose;
+				double		stamp;
+			};
+
+			RGBDParser( const String& folder );
+
+			void loadNext();
+
+			size_t				size() const { return _stamps.size(); }
+			const RGBDSample&	data() const { return _sample; }
+
+		private:
+			const double			_maxStampDiff;
+			String					_folder;
+
+			std::vector<Matrix4d>	_groundTruthPoses;
+			std::vector<String>		_rgbFiles;
+			std::vector<String>		_depthFiles;
+			std::vector<double>		_stamps;
+
+			RGBDSample				_sample;
+			size_t					_idx;
+
+			void loadGroundTruth();
+			void loadRGBFilenames( std::vector<double> & stamps );
+			void loadDepthFilenames( std::vector<double> & stamps );
+
+			bool readNext( Matrix4d& pose, double& stamp, DataIterator& iter );
+			bool readNextFilename( String& filename, double& stamp, DataIterator& iter );
+
+			void sortOutData( const std::vector<double>& rgbStamps, 
+							  const std::vector<double>& depthStamps );
+	
+			size_t findClosestMatchInGTStamps( double val, size_t startIdx );
+
+			size_t findClosestMatchInDepthStamps( double stamp,
+												  size_t dIdx,
+												  const std::vector<double>& depth ); 
+	};
+			
+			
+}
+
+#endif
