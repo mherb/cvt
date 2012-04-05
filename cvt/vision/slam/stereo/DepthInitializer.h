bfa1c41fc4b7db1ea15ffa2cdc5fd96be1db1854
Sebastian Klose <kloses@in.tum.de>
2012-04-05 18:36:22 +0200

    minor stuff
    	modified:   ../cvt/vision/slam/stereo/DepthInitializer.h
    	modified:   ../cvt/vision/slam/stereo/FeatureTracking.h
    	modified:   ../cvt/vision/slam/stereo/ORBTracking.h

--- cvt/vision/slam/stereo/DepthInitializer.h
+++ cvt/vision/slam/stereo/DepthInitializer.h
@@ -3,28 +3,27 @@
 
 namespace cvt
 {
-	class DepthInitializer
-	{
-		public:
-			DepthInitializer( const CameraCalibration& c0, const CameraCalibration& c1 );
+    class DepthInitializer
+    {
+        public:
+            DepthInitializer( const CameraCalibration& c0, const CameraCalibration& c1 );
 
-			/**
-			 *	\brief triangulate features from two (undistorted) views
-			 */
-			virtual void triangulateFeatures( std::vector<Vector3d> & triangulated,
-											  const std::vector<Vector2f> & avoidPositionsImg0, 
-											  const Image& view0, const Image& view1 ) = 0;
+            /**
+             *	\brief triangulate features from two (undistorted) views
+             */
+            virtual void triangulateFeatures( std::vector<Vector3d> & triangulated,
+                                              const std::vector<Vector2f> & avoidPositionsImg0,
+                                              const Image& view0, const Image& view1 ) = 0;
+        protected:
+            const CameraCalibration & _calib0;
+            const CameraCalibration & _calib1;
+    };
 
-		protected:
-			const CameraCalibration & _calib0;
-			const CameraCalibration & _calib1;
-	};
-
-	inline DepthInitializer::DepthInitializer( const CameraCalibration& c0, const CameraCalibration& c1 ) : 
-		_calib0( c0 ),
-		_calib1( c1 )
-	{
-	}
+    inline DepthInitializer::DepthInitializer( const CameraCalibration& c0, const CameraCalibration& c1 ) :
+        _calib0( c0 ),
+        _calib1( c1 )
+    {
+    }
 }
 
 #endif
