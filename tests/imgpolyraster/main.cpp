6f0dfe83579c56210a7a33f46bd26583f30a1f5e
Philipp Heise <heise@in.tum.de>
2012-04-09 23:16:02 +0200

    Started image rasterizer
    	modified:   ../tests/CMakeLists.txt
    	new file:   ../tests/imgpolyraster/CMakeLists.txt
    	new file:   ../tests/imgpolyraster/IPolygonRaster.cpp
    	new file:   ../tests/imgpolyraster/IPolygonRaster.h
    	new file:   ../tests/imgpolyraster/main.cpp

---
+++ tests/imgpolyraster/main.cpp
@@ -0,0 +1,13 @@
+#include <cvt/gfx/Image.h>
+#include <cvt/geom/Polygon.h>
+#include <cvt/geom/Rect.h>
+
+using namespace cvt;
+
+
+int main()
+{
+	Image output( 800, 600, IFormat::GRAY_UINT8 );
+
+	return 0;
+}
