a3e91079fb0c7d1d59acf91c694895d172be239f
Sebastian Klose <kloses@in.tum.de>
2012-04-15 19:14:15 +0200

    RGBDParser into CVT & fix ... needs testing
    	modified:   cvt/CMakeLists.txt
    	new file:   cvt/io/RGBDParser.cpp
    	new file:   cvt/io/RGBDParser.h
    	modified:   tests/rgbddataset/RGBDDatasetApp.h
    	deleted:    tests/rgbddataset/RGBDParser.cpp
    	deleted:    tests/rgbddataset/RGBDParser.h
    	modified:   tests/rgbddataset/rgbddataset.cpp
    	deleted:    tests/tsdfVolume/RGBDParser.cpp
    	deleted:    tests/tsdfVolume/RGBDParser.h
    	modified:   tests/tsdfVolume/main.cpp
    	deleted:    tests/unproject/RGBDParser.cpp
    	deleted:    tests/unproject/RGBDParser.h
    	modified:   tests/unproject/main.cpp

--- tests/tsdfVolume/main.cpp
+++ tests/tsdfVolume/main.cpp
@@ -9,7 +9,7 @@
 
 #include <cvt/cl/kernel/TSDFVolume/TSDFVolume.h>
 
-#include "RGBDParser.h"
+#include <cvt/io/RGBDParser.h>
 
 #define VOL_WIDTH  256
 #define VOL_HEIGHT 256
@@ -19,110 +19,110 @@ using namespace cvt;
 
 int main( int argc, char** argv )
 {
-	if( argc < 2 ){
-		std::cout << "Usage: " << argv[ 0 ] << " <data_set_folder>" << std::endl;
-		return 0;
-	}
+    if( argc < 2 ){
+        std::cout << "Usage: " << argv[ 0 ] << " <data_set_folder>" << std::endl;
+        return 0;
+    }
 
 
 
-	std::vector<CLPlatform> platforms;
-	CLPlatform::get( platforms );
-	CL::setDefaultDevice( platforms[ 0 ].defaultDevice() );
+    std::vector<CLPlatform> platforms;
+    CLPlatform::get( platforms );
+    CL::setDefaultDevice( platforms[ 0 ].defaultDevice() );
 
-	try {
+    try {
 
-		String folder( argv[ 1 ] );
+        String folder( argv[ 1 ] );
 
-		RGBDParser rgbddata( folder );
+        RGBDParser rgbddata( folder );
 
-		Matrix3f intrinsics( 517.3f, 0.0f, 318.6f,
-							   0.0f,   516.5f, 255.3f,
-							    0.0,	  0.0f,  1.0f );
+        Matrix3f intrinsics( 517.3f, 0.0f, 318.6f,
+                               0.0f,   516.5f, 255.3f,
+                                0.0,	  0.0f,  1.0f );
 
-		Matrix4f gridToWorld( 2.0f / ( float )( VOL_WIDTH ), 0.0f, 0.0f, -0.2f,
- 				              0.0f, 2.0f / ( float )( VOL_HEIGHT ), 0.0f, 0.0f,
-							  0.0f, 0.0f, 2.0f / ( float ) ( VOL_DEPTH ), 0.4f,
-							  0.0f, 0.0f, 0.0f, 1.0f );
+        Matrix4f gridToWorld( 2.0f / ( float )( VOL_WIDTH ), 0.0f, 0.0f, -0.2f,
+                              0.0f, 2.0f / ( float )( VOL_HEIGHT ), 0.0f, 0.0f,
+                              0.0f, 0.0f, 2.0f / ( float ) ( VOL_DEPTH ), 0.4f,
+                              0.0f, 0.0f, 0.0f, 1.0f );
 
-		Matrix4f gridToCam;
-
-
-		CLKernel kernclr( _TSDFVolume_source, "TSDFVolume_clear" );
-		CLKernel kernadd( _TSDFVolume_source, "TSDFVolume_add" );
-		CLKernel kernslice( _TSDFVolume_source, "TSDFVolume_slice" );
-
-		CLBuffer cv( sizeof( cl_float2 ) * VOL_WIDTH * VOL_HEIGHT * VOL_DEPTH  );
+        Matrix4f gridToCam;
 
 
-		/* clear the volume */
-		kernclr.setArg( 0, cv );
-		kernclr.setArg( 1, VOL_WIDTH );
-		kernclr.setArg( 2, VOL_HEIGHT );
-		kernclr.setArg( 3, VOL_DEPTH );
-		kernclr.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ), VOL_DEPTH ), CLNDRange( 16, 16, 1 ) );
+        CLKernel kernclr( _TSDFVolume_source, "TSDFVolume_clear" );
+        CLKernel kernadd( _TSDFVolume_source, "TSDFVolume_add" );
+        CLKernel kernslice( _TSDFVolume_source, "TSDFVolume_slice" );
 
-		Image depthmap;
+        CLBuffer cv( sizeof( cl_float2 ) * VOL_WIDTH * VOL_HEIGHT * VOL_DEPTH  );
 
-		CLBuffer clintrin( ( void*)intrinsics.toMatrix4().ptr(), sizeof( float ) * 12 );
 
-		Time t;
-		/* add the depth maps */
-		for( int i = 0; i < 700; i++) {
-			rgbddata.loadNext();
-			rgbddata.data().depth.convert( depthmap, IFormat::GRAY_UINT16, IALLOCATOR_CL );
+        /* clear the volume */
+        kernclr.setArg( 0, cv );
+        kernclr.setArg( 1, VOL_WIDTH );
+        kernclr.setArg( 2, VOL_HEIGHT );
+        kernclr.setArg( 3, VOL_DEPTH );
+        kernclr.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ), VOL_DEPTH ), CLNDRange( 16, 16, 1 ) );
+
+        Image depthmap;
+
+        CLBuffer clintrin( ( void*)intrinsics.toMatrix4().ptr(), sizeof( float ) * 12 );
+
+        Time t;
+        /* add the depth maps */
+        for( int i = 0; i < 700; i++) {
+            rgbddata.loadNext();
+            rgbddata.data().depth.convert( depthmap, IFormat::GRAY_UINT16, IALLOCATOR_CL );
 //			camcalib.setExtrinsics( rgbddata.data().pose );
 //			rgbddata.data().rgb.convert( image, IFormat::RGBA_FLOAT );
 
-			gridToCam = rgbddata.data().pose.inverse() * gridToWorld;
-			CLBuffer clglobal( ( void*)gridToCam.ptr(), sizeof( float ) * 12 );
+            gridToCam = rgbddata.data().pose.inverse() * gridToWorld;
+            CLBuffer clglobal( ( void*)gridToCam.ptr(), sizeof( float ) * 12 );
 
-			kernadd.setArg( 0, cv );
-			kernadd.setArg( 1, VOL_WIDTH );
-			kernadd.setArg( 2, VOL_HEIGHT );
-			kernadd.setArg( 3, VOL_DEPTH );
-			kernadd.setArg( 4, depthmap );
-			kernadd.setArg( 5, ( float ) ( 0xffff ) / 5000.0f );
-			kernadd.setArg( 6, clintrin );
-			kernadd.setArg( 7, clglobal );
-			kernadd.setArg( 8, 5.0f );
-			kernadd.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ), VOL_DEPTH ), CLNDRange( 16, 16, 1 ) );
+            kernadd.setArg( 0, cv );
+            kernadd.setArg( 1, VOL_WIDTH );
+            kernadd.setArg( 2, VOL_HEIGHT );
+            kernadd.setArg( 3, VOL_DEPTH );
+            kernadd.setArg( 4, depthmap );
+            kernadd.setArg( 5, ( float ) ( 0xffff ) / 5000.0f );
+            kernadd.setArg( 6, clintrin );
+            kernadd.setArg( 7, clglobal );
+            kernadd.setArg( 8, 5.0f );
+            kernadd.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ), VOL_DEPTH ), CLNDRange( 16, 16, 1 ) );
 
-		}
-		std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
+        }
+        std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
 
-		{
-			FILE* f;
-			f = fopen("data.raw","wb");
-			float* ptr = ( float* ) cv.map();
-			float* origptr = ptr;
-			size_t n = VOL_WIDTH * VOL_HEIGHT * VOL_DEPTH;
-			while( n-- ) {
-				fwrite( ptr, sizeof( float ), 1, f );
-				ptr += 2;
-			}
-			fclose( f );
-			cv.unmap( origptr );
-		}
+        {
+            FILE* f;
+            f = fopen("data.raw","wb");
+            float* ptr = ( float* ) cv.map();
+            float* origptr = ptr;
+            size_t n = VOL_WIDTH * VOL_HEIGHT * VOL_DEPTH;
+            while( n-- ) {
+                fwrite( ptr, sizeof( float ), 1, f );
+                ptr += 2;
+            }
+            fclose( f );
+            cv.unmap( origptr );
+        }
 
-		/*depthmap.reallocate( VOL_WIDTH, VOL_HEIGHT, IFormat::GRAY_FLOAT, IALLOCATOR_CL );
+        /*depthmap.reallocate( VOL_WIDTH, VOL_HEIGHT, IFormat::GRAY_FLOAT, IALLOCATOR_CL );
 
-		for( int i = 0; i < 512; i++ ) {
-			kernslice.setArg( 0, depthmap );
-			kernslice.setArg( 1, i );
-			kernslice.setArg( 2, cv );
-			kernslice.setArg( 3, VOL_WIDTH );
-			kernslice.setArg( 4, VOL_HEIGHT );
-			kernslice.setArg( 5, VOL_DEPTH );
-			kernslice.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ) ), CLNDRange( 16, 16 ) );
-			String name;
-			name.sprintf("slice%03d.png", i );
-			depthmap.save( name );
-		}*/
+        for( int i = 0; i < 512; i++ ) {
+            kernslice.setArg( 0, depthmap );
+            kernslice.setArg( 1, i );
+            kernslice.setArg( 2, cv );
+            kernslice.setArg( 3, VOL_WIDTH );
+            kernslice.setArg( 4, VOL_HEIGHT );
+            kernslice.setArg( 5, VOL_DEPTH );
+            kernslice.run( CLNDRange( Math::pad16( VOL_WIDTH ), Math::pad16( VOL_HEIGHT ) ), CLNDRange( 16, 16 ) );
+            String name;
+            name.sprintf("slice%03d.png", i );
+            depthmap.save( name );
+        }*/
 
-	} catch( CLException& e ) {
-		std::cout << e.what() << std::endl;
-	}
+    } catch( CLException& e ) {
+        std::cout << e.what() << std::endl;
+    }
 
-	return 0;
+    return 0;
 }
