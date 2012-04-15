f39ed75f00945d2f50d6a17e58ab410f59fdf014
Sebastian Klose <kloses@in.tum.de>
2012-04-15 19:14:15 +0200

    RGBDParser into CVT & fix ... needs testing
    	modified:   cvt/CMakeLists.txt
    	new file:   cvt/io/RGBDParser.cpp
    	new file:   cvt/io/RGBDParser.h
    	modified:   tests/rgbddataset/RGBDDatasetApp.h
    	deleted:    tests/rgbddataset/RGBDParser.cpp
    	deleted:    tests/rgbddataset/RGBDParser.h
    	modified:   tests/rgbddataset/rgbddataset.cpp
    	deleted:    tests/tsdfVolume/RGBDParser.cpp
    	deleted:    tests/tsdfVolume/RGBDParser.h
    	modified:   tests/tsdfVolume/main.cpp
    	deleted:    tests/unproject/RGBDParser.cpp
    	deleted:    tests/unproject/RGBDParser.h
    	modified:   tests/unproject/main.cpp

--- tests/unproject/main.cpp
+++ tests/unproject/main.cpp
@@ -3,68 +3,68 @@
 #include <cvt/gui/Application.h>
 #include <cvt/vision/Vision.h>
 #include <cvt/vision/CameraCalibration.h>
+#include <cvt/io/RGBDParser.h>
 
 #include "GLSceneView.h"
-#include "RGBDParser.h"
 
 using namespace cvt;
 
 int main( int argc, char** argv )
 {
-	if( argc < 2 ){
-		std::cout << "Usage: " << argv[ 0 ] << " <data_set_folder>" << std::endl;
-		return 0;
-	}
+    if( argc < 2 ){
+        std::cout << "Usage: " << argv[ 0 ] << " <data_set_folder>" << std::endl;
+        return 0;
+    }
 
-	try {
-		String folder( argv[ 1 ] );
+    try {
+        String folder( argv[ 1 ] );
 
-		RGBDParser rgbddata( folder );
+        RGBDParser rgbddata( folder );
 
-		CameraCalibration camcalib;
-		camcalib.setIntrinsics( Matrix3f( 517.3f, 0.0f, 318.6f,
-										 0.0f,   516.5f, 255.3f,
-										 0.0,	  0.0f,  1.0f ) );
+        CameraCalibration camcalib;
+        camcalib.setIntrinsics( Matrix3f( 517.3f, 0.0f, 318.6f,
+                                         0.0f,   516.5f, 255.3f,
+                                         0.0,	  0.0f,  1.0f ) );
 
-		ScenePoints pts( "Points" );
-		for( int i = 0; i < 30; i++) {
-			rgbddata.loadNext();
-			camcalib.setExtrinsics( rgbddata.data().pose );
+        ScenePoints pts( "Points" );
+        for( int i = 0; i < 30; i++) {
+            rgbddata.loadNext();
+            camcalib.setExtrinsics( rgbddata.data().pose );
 //			std::cout << rgbddata.data().rgb << std::endl;
 //			std::cout << rgbddata.data().depth << std::endl;
-			Image depthmap, image;
-			rgbddata.data().rgb.convert( image, IFormat::RGBA_FLOAT );
-			rgbddata.data().depth.convert( depthmap, IFormat::GRAY_FLOAT );
+            Image depthmap, image;
+            rgbddata.data().rgb.convert( image, IFormat::RGBA_FLOAT );
+            rgbddata.data().depth.convert( depthmap, IFormat::GRAY_FLOAT );
 //			depthmap.save( "map.png" );
-			ScenePoints ptsnew( "BLA" );
-			Vision::unprojectToScenePoints( ptsnew, image, depthmap, camcalib, ( float ) ( 0xffff ) / 5000.0f );
-			pts.add( ptsnew );
-		}
+            ScenePoints ptsnew( "BLA" );
+            Vision::unprojectToScenePoints( ptsnew, image, depthmap, camcalib, ( float ) ( 0xffff ) / 5000.0f );
+            pts.add( ptsnew );
+        }
 //		pts.translate( -pts.centroid() );
 //		pts.scale( 1e6f );
 
-		std::cout << pts.boundingBox() << std::endl;
+        std::cout << pts.boundingBox() << std::endl;
 
 
-		Window w( "meshView" );
-		w.setSize( 640, 480 );
-		GLSceneView view( pts );
+        Window w( "meshView" );
+        w.setSize( 640, 480 );
+        GLSceneView view( pts );
 
-		WidgetLayout wl;
-		wl.setAnchoredTopBottom( 0, 0 );
-		wl.setAnchoredLeftRight( 0, 0 );
-		w.addWidget( &view, wl );
-		w.setVisible( true );
+        WidgetLayout wl;
+        wl.setAnchoredTopBottom( 0, 0 );
+        wl.setAnchoredLeftRight( 0, 0 );
+        w.addWidget( &view, wl );
+        w.setVisible( true );
 
-		w.update();
+        w.update();
 
-		Application::run();
+        Application::run();
 
-	} catch ( cvt::Exception& e ){
-		std::cout << e.what() << std::endl;
-	}
+    } catch ( cvt::Exception& e ){
+        std::cout << e.what() << std::endl;
+    }
 
 
-	return 0;
+    return 0;
 
 }
