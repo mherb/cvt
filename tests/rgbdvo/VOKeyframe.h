27a12e906f80e5266f361ffc756d054e62d3acda
Sebastian Klose <kloses@in.tum.de>
2012-04-16 13:31:33 +0200

    rgbdvo startup ...
    	modified:   ../cvt/io/RGBDParser.cpp
    	modified:   ../cvt/io/RGBDParser.h
    	new file:   rgbdvo/RGBDAlignment.cpp
    	modified:   rgbdvo/RGBDAlignment.h
    	new file:   rgbdvo/VOKeyframe.cpp
    	modified:   rgbdvo/VOKeyframe.h
    	modified:   rgbdvo/rgbdvo.cpp

--- tests/rgbdvo/VOKeyframe.h
+++ tests/rgbdvo/VOKeyframe.h
@@ -1,24 +1,54 @@
 #ifndef CVT_VO_KEYFRAME_H
 #define CVT_VO_KEYFRAME_H
 
+#include <cvt/gfx/Image.h>
+#include <cvt/math/Matrix.h>
+
+#include <vector>
+#include <Eigen/Core>
+
 namespace cvt
 {
-	class VOKeyframe
-	{
-		public:
-			/**
-			 * @param	rgb		RGB Image
-			 * @param	depth	Depth Image
-			 * @param	pose	pose for this keyframe
-			 */
-			VOKeyframe( const Image& rgb, const Image& depth, const Matrix4d& pose );
-			
-			~VOKeyframe();
+    class VOKeyframe
+    {
+        public:
+            typedef Eigen::Matrix<float, 6, 6> HessianType;
+            /**
+             * @param	rgb		RGB Image
+             * @param	depth	Depth Image
+             * @param	pose	pose for this keyframe
+             */
+            VOKeyframe( const Image& rgb, const Image& depth, const Matrix4f& pose );
+            ~VOKeyframe();
 
-		private:
-			// we need:
-			// jacobians per pixel 
-	};
+            const HessianType& inverseHessian() const { return _inverseHessian; }
+
+        private:
+            Matrix4f    _pose;
+
+            Image       _gray;
+            Image       _depth;
+
+            // jacobians per pixel mixed with the image gradients
+            std::vector<Image>  _stepestDescentImages;
+
+            // precomputed inverse of the hessian approx.
+            Eigen::Matrix<float, 6, 6>  _inverseHessian;
+    };
+
+    inline VOKeyframe::VOKeyframe( const Image& rgb, const Image& depth, const Matrix4f& pose ) :
+        _pose( pose ),
+        _depth( depth )
+    {
+        rgb.convert( _gray, IFormat::GRAY_UINT8 );
+
+        // todo: compute the gradients & build the jacobian images
+        //       for the jacobians, we also need the intrinsics of the rgb cam
+    }
+
+    inline VOKeyframe::~VOKeyframe()
+    {
+    }
 }
 
 #endif
