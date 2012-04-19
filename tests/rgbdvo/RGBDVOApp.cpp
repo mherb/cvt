953263cb736c59f313d5fa134f863eb5286a7efe
Sebastian Klose <kloses@in.tum.de>
2012-04-19 17:11:35 +0200

    rgbdvo working :-)
    	modified:   ../cvt/vision/Vision.cpp
    	new file:   rgbdvo/PoseView.cpp
    	new file:   rgbdvo/PoseView.h
    	modified:   rgbdvo/RGBDAlignment.cpp
    	modified:   rgbdvo/RGBDVOApp.cpp
    	modified:   rgbdvo/RGBDVOApp.h
    	modified:   rgbdvo/VOKeyframe.cpp

--- tests/rgbdvo/RGBDVOApp.cpp
+++ tests/rgbdvo/RGBDVOApp.cpp
@@ -3,22 +3,28 @@
 #include <cvt/gui/Application.h>
 #include <cvt/util/EigenBridge.h>
 #include <cvt/util/Delegate.h>
+#include <cvt/util/Time.h>
 
 namespace cvt
 {
 
     RGBDVOApp::RGBDVOApp( const String& folder, const Matrix3f& K ) :
         _parser( folder ),
-        _aligner( K, 20, 5000.0f),
+        _aligner( K, 15, 5000.0f),
         _mainWindow( "RGBD-VO" ),
         _kfMov( &_keyframeImage ),
         _imageMov( &_currentImage ),
         _gxMov( &_gxView ),
         _gyMov( &_gyView ),
+        _poseMov( &_poseView ),
         _nextButton( "next" ),
-        _nextPressed( false )
+        _nextPressed( false ),
+        _stepButton( "toggle stepping" ),
+        _step( true ),
+        _optimizeButton( "optimize" ),
+        _optimize( false )
     {
-        _timerId = Application::registerTimer( 30, this );
+        _timerId = Application::registerTimer( 50, this );
         setupGui();
 
         _parser.loadNext();
@@ -36,22 +42,28 @@ namespace cvt
 
     void RGBDVOApp::onTimeout()
     {
-        if( _nextPressed ){
+        if( _nextPressed || !_step ){
             _parser.loadNext();
+            _currentImage.setImage( _parser.data().rgb );
             _nextPressed = false;
         }
 
-        const RGBDParser::RGBDSample& d = _parser.data();
-        _currentImage.setImage( d.rgb );
+        if( _optimize ){
+            Time t;
+            const RGBDParser::RGBDSample& d = _parser.data();
+            // try to align:
+            _aligner.alignWithKeyFrame( _relativePose, *_activeKeyframe, d.rgb, d.depth );
 
-        // try to align:
-        _aligner.alignWithKeyFrame( _relativePose, *_activeKeyframe, d.rgb, d.depth );
+            std::cout << "Alignment took: " << t.elapsedMilliSeconds() << "ms" << std::endl;
 
-        // update the absolute pose
-        EigenBridge::toCVT( _absolutePose, _relativePose.transformation() );
-        _absolutePose *= _activeKeyframe->pose();
+            // update the absolute pose
+            Matrix4f tmp;
+            EigenBridge::toCVT( tmp, _relativePose.transformation() );
+            _absolutePose = _activeKeyframe->pose() * tmp.inverse();
 
-        std::cout << "Pose Error:\n" << _absolutePose - d.pose << std::endl;
+            _poseView.setCamPose( _absolutePose );
+            _poseView.setGTPose( d.pose );
+        }
     }
 
     void RGBDVOApp::setupGui()
@@ -79,24 +91,44 @@ namespace cvt
         _gyMov.setPosition( 300, 0 );
         _gyMov.setTitle( "Gradient Y" );
 
+        _mainWindow.addWidget( &_poseMov );
+        _poseMov.setSize( 300, 200 );
+        _poseMov.setPosition( 600, 200 );
+        _poseMov.setTitle( "Poses" );
+
         WidgetLayout wl;
+
+        /*
         wl.setRelativeLeftRight( 0.02f, 0.98f);
         wl.setRelativeTopBottom( 0.02f, 0.98f);
         _mainWindow.addWidget( &_sceneView, wl );
+        */
 
-        wl.setAnchoredBottom( 5, 20 );
-        wl.setAnchoredRight( 5, 50 );
+        wl.setAnchoredBottom( 5, 30 );
+        wl.setAnchoredRight( 5, 70 );
         _mainWindow.addWidget( &_nextButton, wl );
-
         Delegate<void ()> d( this, &RGBDVOApp::nextPressed );
         _nextButton.clicked.add( d );
+
+        wl.setAnchoredBottom( 40, 30 );
+        _mainWindow.addWidget( &_stepButton, wl );
+        Delegate<void ()> d1( this, &RGBDVOApp::toggleStepping );
+        _stepButton.clicked.add( d1 );
+
+        wl.setAnchoredBottom( 75, 30 );
+        _mainWindow.addWidget( &_optimizeButton, wl );
+        Delegate<void ()> d2( this, &RGBDVOApp::optimizePressed );
+        _optimizeButton.clicked.add( d2 );
 
         _mainWindow.setVisible( true );
     }
 
     void RGBDVOApp::addNewKeyframe( const RGBDParser::RGBDSample& sample, const Matrix4f& kfPose )
     {
+        Time t;
         VOKeyframe* kf = new VOKeyframe( sample.rgb, sample.depth, kfPose, _aligner.intrinsics(), 5000.0f );
+        std::cout << "Keyframe creation took: " << t.elapsedMilliSeconds() << "ms" << std::endl;
+
         _keyframes.push_back( kf );
         _activeKeyframe = _keyframes.back();
 
@@ -107,7 +139,12 @@ namespace cvt
         // reset the relative pose
         SE3<float>::MatrixType I = SE3<float>::MatrixType::Identity();
         _relativePose.set( I );
+        _absolutePose = kfPose;
 
+        _poseView.addKeyframe( kfPose );
+        _poseView.setCamPose( _absolutePose );
+
+        /*
         ScenePoints pts( "bla" );
         std::vector<Vector4f> colors;
         const float* c = kf->pixelData();
@@ -116,12 +153,23 @@ namespace cvt
         }
 
         pts.setVerticesWithColor( kf->pointsPtr(), &colors[ 0 ], kf->numPoints() );
-        _sceneView.setScenePoints( pts );
+        _sceneView.setScenePoints( pts );*/
     }
 
     void RGBDVOApp::nextPressed()
     {
+        std::cout << "click" << std::endl;
         _nextPressed = true;
+    }
+
+    void RGBDVOApp::optimizePressed()
+    {
+        _optimize = !_optimize;
+    }
+
+    void RGBDVOApp::toggleStepping()
+    {
+        _step = !_step;
     }
 
 }
