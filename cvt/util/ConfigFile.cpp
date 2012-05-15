a6a2fe06edbe1c5f02a69df094e42532a5e8d0e6
Sebastian Klose <kloses@in.tum.de>
2012-05-15 15:57:21 +0200

    moved Configfile to CVT
    	modified:   cvt/CMakeLists.txt
    	new file:   cvt/util/ConfigFile.cpp
    	new file:   cvt/util/ConfigFile.h

---
+++ cvt/util/ConfigFile.cpp
@@ -0,0 +1,110 @@
+/*
+			CVT - Computer Vision Tools Library
+
+ 	 Copyright (c) 2012, Philipp Heise, Sebastian Klose
+
+ 	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ 	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ 	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ 	PARTICULAR PURPOSE.
+*/
+
+#include <cvt/io/FileSystem.h>
+#include <cvt/util/ConfigFile.h>
+#include <cvt/util/Data.h>
+#include <cvt/util/DataIterator.h>
+#include <fstream>
+
+namespace cvt {
+
+    ConfigFile::ConfigFile( const String& filename )
+    {
+        if( !FileSystem::exists( filename ) ){
+            String msg;
+            msg.sprintf( "Could not find config file: %s", filename.c_str() );
+            throw CVTException(  msg.c_str() );
+        }
+
+        Data data;
+        FileSystem::load( data, filename );
+        DataIterator it( data );
+
+        while( it.hasNext() ){
+            std::vector<String> tokens;
+            it.tokenizeNextLine( tokens, "= " );
+            if( tokens.size() == 2 ){
+                _values[ tokens[ 0 ] ] = tokens[ 1 ];
+            }
+        }
+    }
+
+    void ConfigFile::save( const String& filename ) const
+    {
+        std::ofstream file;
+        file.open( filename.c_str() );
+
+        MapType::const_iterator it = _values.begin();
+        const MapType::const_iterator end = _values.end();
+
+        while( it != end ){
+            file << it->first << " = " << it->second << std::endl;
+            it++;
+        }
+
+        file.close();
+    }
+
+    template <>
+    String ConfigFile::valueForName<String>( const String& name, String defaultValue )
+    {
+        MapType::const_iterator it = _values.find( name );
+        if( it == _values.end() ){
+            _values[ name ] = defaultValue;
+            return defaultValue;
+        } else {
+            return it->second;
+        }
+    }
+
+    template <>
+    int ConfigFile::valueForName<int>( const String& name, int defaultValue )
+    {
+        MapType::const_iterator it = _values.find( name );
+        if( it == _values.end() ){
+            String sval;
+            sval.sprintf( "%d", defaultValue );
+            _values[ name ] = sval;
+            return defaultValue;
+        } else {
+            return it->second.toInteger();
+        }
+    }
+
+    template <>
+    float ConfigFile::valueForName<float>( const String& name, float defaultValue )
+    {
+        MapType::const_iterator it = _values.find( name );
+        if( it == _values.end() ){
+            String sval;
+            sval.sprintf( "%0.6f", defaultValue );
+            _values[ name ] = sval;
+            return defaultValue;
+        } else {
+            return it->second.toFloat();
+        }
+    }
+
+    template <>
+    double ConfigFile::valueForName<double>( const String& name, double defaultValue )
+    {
+        MapType::const_iterator it = _values.find( name );
+        if( it == _values.end() ){String sval;
+            sval.sprintf( "%0.10g", defaultValue );
+            _values[ name ] = sval;
+            return defaultValue;
+        } else {
+            return it->second.toDouble();
+        }
+    }
+
+}
