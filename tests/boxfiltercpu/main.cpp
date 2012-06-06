5f40de38bd02ce84fcd01e716422e925e3725dd2
Philipp Heise <heise@in.tum.de>
2012-06-06 19:52:14 +0200

    BoxFilter now support cpu version
    	modified:   ../cvt/gfx/ifilter/BoxFilter.cpp
    	modified:   ../cvt/gfx/ifilter/BoxFilter.h
    	modified:   ../tests/boxfilter/main.cpp
    	deleted:    ../tests/boxfiltercpu/BoxFilter.cpp
    	deleted:    ../tests/boxfiltercpu/BoxFilter.h
    	deleted:    ../tests/boxfiltercpu/Utils.cpp
    	deleted:    ../tests/boxfiltercpu/Utils.h
    	modified:   ../tests/boxfiltercpu/main.cpp
    	modified:   ../tests/stereogcv/main.cpp

--- tests/boxfiltercpu/main.cpp
+++ tests/boxfiltercpu/main.cpp
@@ -1,38 +1,30 @@
 #include <cvt/gfx/Image.h>
 #include <cvt/gfx/IMapScoped.h>
 #include <cvt/util/Time.h>
-
-#include "BoxFilter.h"
+#include <cvt/gfx/ifilter/BoxFilter.h>
+#include <cvt/cl/OpenCL.h>
+#include <cvt/cl/CLPlatform.h>
 
 using namespace cvt;
 
-static inline void boxfun( Image& dst, const Image& iimg, size_t r )
-{
-	dst.reallocate( iimg.width(), iimg.height(), IFormat::GRAY_UINT8 );
-	dst.fill( Color::WHITE );
-
-
-	cvt::IMapScoped<uint8_t> mapdst( dst );
-	cvt::IMapScoped<const float> mapsrc( iimg );
-
-	SIMD::instance()->boxFilterPrefixSum1_f_to_u8( mapdst.ptr(), mapdst.stride(), mapsrc.ptr(), mapsrc.stride(), dst.width(), dst.height(), 2*r+1, 2*r+1 );
-}
 
 int main( int argc, char** argv )
 {
 	Image tmp( argv[ 1 ] );
 	Image src, dst, isrc;
 
+	std::vector<CLPlatform> platforms;
+	CLPlatform::get( platforms );
+	CL::setDefaultDevice( platforms[ 0 ].defaultDevice() );
+
 	tmp.convert( src, IFormat::GRAY_UINT8 );
 	isrc.reallocate( src.width(), src.height(), IFormat::GRAY_FLOAT );
 	src.integralImage( isrc );
-	Time t;
-	BoxFilter( dst, isrc, Vector2i( 7, 7 ) );
-	std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
-	dst.save( "outbox1.png" );
 
-	t.reset();
-	boxfun( dst, isrc, 3 );
+	BoxFilter box;
+	dst.reallocate( src.width(), src.height(), IFormat::GRAY_UINT8 );
+	Time t;
+	box.apply( dst, isrc, 3 );
 	std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
 	dst.save( "outbox2.png" );
 
