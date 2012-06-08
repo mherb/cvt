f4412854df69d82572582e9d5822762ed8c11bca
Philipp Heise <heise@in.tum.de>
2012-06-08 17:17:35 +0200

    Added IComponents
    	modified:   ../cvt/CMakeLists.txt
    	new file:   ../cvt/gfx/IComponents.h
    	modified:   ../cvt/gfx/IMapScoped.h
    	modified:   ../tests/boxfiltercpu/main.cpp

--- tests/boxfiltercpu/main.cpp
+++ tests/boxfiltercpu/main.cpp
@@ -3,6 +3,7 @@
 #include <cvt/util/Time.h>
 #include <cvt/gfx/ifilter/BoxFilter.h>
 #include <cvt/gfx/ifilter/AdaptiveThreshold.h>
+#include <cvt/gfx/IComponents.h>
 #include <cvt/cl/OpenCL.h>
 #include <cvt/cl/CLPlatform.h>
 
@@ -33,6 +34,33 @@ int main( int argc, char** argv )
 	dst2.reallocate( src.width(), src.height(), IFormat::GRAY_UINT8 );
 	AdaptiveThreshold ath;
 
-	ath.apply( dst2, src, dst, 0.05f );
+	ath.apply( dst2, src, dst, 0.01f );
 	dst2.save( "outath.png" );
+
+	t.reset();
+	IComponents<float> comp( dst2 );
+
+	std::cout << t.elapsedMilliSeconds() << " ms" << std::endl;
+
+	{
+		for( size_t i = 0; i < comp.size(); i++ ) {
+			Image icomps( src.width(), src.height(), IFormat::GRAY_UINT8 );
+			icomps.fill( Color::BLACK );
+			{
+				PointSet<2,float>& ptset = comp[ i ];
+				if( ptset.size() < 20 )
+					continue;
+
+				IMapScoped<uint8_t> map( icomps );
+				for( size_t k = 0; k < ptset.size(); k++ ) {
+					Vector2f pt = ptset[ k ];
+					*( map.base() + map.stride() * ( int ) pt.y + ( int ) pt.x ) = 0xff;
+				}
+			}
+			icomps.save( "outcomponents.png" );
+			getchar();
+		}
+	}
+
+
 }
