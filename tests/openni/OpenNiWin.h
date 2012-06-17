5b41685b1b7ca3ae8f5681bae36728d4089336a8
Sebastian Klose <kloses@in.tum.de>
2012-06-17 14:19:59 +0200

    openni test updates
    	modified:   tests/openni/OpenNiWin.h
    	modified:   tests/openni/openni.cpp

--- tests/openni/OpenNiWin.h
+++ tests/openni/OpenNiWin.h
@@ -10,83 +10,86 @@
 
 namespace cvt
 {
-	class OpenNiWin : public TimeoutHandler
-	{
-		public:
-			OpenNiWin();
-			~OpenNiWin();
+    class OpenNiWin : public TimeoutHandler
+    {
+        public:
+            OpenNiWin();
+            ~OpenNiWin();
 
-			void onTimeout();
+            void onTimeout();
 
-		private:
-			uint32_t		_timerId;
-			Window			_window;
-			ImageView		_rgbView;
-			ImageView		_depthView;
-			Moveable		_rgbMov;
-			Moveable		_depthMov;
-			OpenNICamera	_cam;
+        private:
+            uint32_t		_timerId;
+            Window			_window;
+            ImageView		_rgbView;
+            ImageView		_depthView;
+            Moveable		_rgbMov;
+            Moveable		_depthMov;
+            OpenNICamera	_cam;
 
-			Time			_time;
-			size_t			_iters;
-	};
+            Time			_time;
+            size_t			_iters;
+    };
 
-	inline OpenNiWin::OpenNiWin() :
-		_window( "OpenNI" ),
-		_rgbMov( &_rgbView ),
-		_depthMov( &_depthView ),
-        //_cam( 0, CameraMode( 640, 480, 30, IFormat::UYVY_UINT8 ) )
-          _cam( 0, CameraMode( 320, 240, 60, IFormat::UYVY_UINT8 ) )
+    inline OpenNiWin::OpenNiWin() :
+        _window( "OpenNI" ),
+        _rgbMov( &_rgbView ),
+        _depthMov( &_depthView ),
+        _cam( 0, CameraMode( 640, 480, 30, IFormat::UYVY_UINT8 ) )
+//          _cam( 0, CameraMode( 320, 240, 60, IFormat::UYVY_UINT8 ) )
+    //      _cam( 0, CameraMode( 320, 240, 60, IFormat::BAYER_GRBG_UINT8 ) )
     //  _cam( 0, CameraMode( 1280, 1024, 30, IFormat::UYVY_UINT8 ) )
-	{
-		_timerId = Application::registerTimer( 10, this );
-                _cam.startCapture();
+    {
+        _timerId = Application::registerTimer( 10, this );
 
-		_window.setSize( 800, 600 );
-		_depthMov.setSize( 320, 240 );
-		_rgbMov.setSize( 320, 240 );
+        _cam.startCapture();
 
-		_depthMov.setTitle( "Depth" );
-		_rgbMov.setTitle( "Image" );
+        _window.setSize( 800, 600 );
+        _depthMov.setSize( 320, 240 );
+        _rgbMov.setSize( 320, 240 );
 
-		_window.addWidget( &_depthMov );
-		_window.addWidget( &_rgbMov );
 
-		_window.setVisible( true );
-		_window.update();
-		
-	}
+        _depthMov.setTitle( "Depth" );
+        _rgbMov.setTitle( "Image" );
 
-	inline OpenNiWin::~OpenNiWin()
-	{
-		Application::unregisterTimer( _timerId );
-		_cam.stopCapture();
-	}
+        _window.addWidget( &_depthMov );
+        _window.addWidget( &_rgbMov );
 
-	inline void OpenNiWin::onTimeout()
-	{
+        _window.setVisible( true );
+        _window.update();
+
+    }
+
+    inline OpenNiWin::~OpenNiWin()
+    {
+        Application::unregisterTimer( _timerId );
+        _cam.stopCapture();
+    }
+
+    inline void OpenNiWin::onTimeout()
+    {
                 _cam.nextFrame();
-        
+
                 _iters++;
-		double t;
-		if( ( t = _time.elapsedSeconds() ) > 3.0 ){
-			String title;
-			title.sprintf( "OpenNI - FPS: %0.1f", _iters / t );
-			_window.setTitle( title );
-			_iters = 0;
-			_time.reset();
-		}
-        
+        double t;
+        if( ( t = _time.elapsedSeconds() ) > 3.0 ){
+            String title;
+            title.sprintf( "OpenNI - FPS: %0.1f", _iters / t );
+            _window.setTitle( title );
+            _iters = 0;
+            _time.reset();
+        }
+
                 _cam.depthFocalLength();
 
                 Image img;
                 _cam.frame().convert( img, IFormat::RGBA_UINT8 );
-                
+
                 Image dCopy( _cam.depth() );
                 dCopy.mul( 3.0f );
-		_rgbView.setImage( img );
-		_depthView.setImage( dCopy );
-	}
+        _rgbView.setImage( img );
+        _depthView.setImage( dCopy );
+    }
 }
 
 #endif
