649f25231a298a635aa534e7e1cf5f58bd9234f7
Sebastian Klose <kloses@in.tum.de>
2012-06-25 09:40:27 +0200

    filter type in constructor
    	modified:   vision/ImagePyramid.h

--- cvt/vision/ImagePyramid.h
+++ cvt/vision/ImagePyramid.h
@@ -1,101 +1,102 @@
 /*
-			CVT - Computer Vision Tools Library
+            CVT - Computer Vision Tools Library
 
- 	 Copyright (c) 2012, Philipp Heise, Sebastian Klose
+     Copyright (c) 2012, Philipp Heise, Sebastian Klose
 
- 	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
- 	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- 	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- 	PARTICULAR PURPOSE.
+    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+    PARTICULAR PURPOSE.
  */
 #ifndef CVT_IMAGE_PYRAMID_H
 #define CVT_IMAGE_PYRAMID_H
 
 #include <cvt/gfx/Image.h>
+#include <cvt/gfx/IScaleFilter.h>
 
 namespace cvt
 {
-	class ImagePyramid
-	{
-		public:
-            ImagePyramid( size_t octaves, float scaleFactor );
+    class ImagePyramid
+    {
+        public:
+            ImagePyramid( size_t octaves, float scaleFactor, const IScaleFilter& filter = IScaleFilterBilinear() );
 
-			/**
-			 * \brief operators to access the scale space images
-			 */
-			Image&		 operator[]( size_t octave )	   { return _image[ octave ]; }
-			const Image& operator[]( size_t octave ) const { return _image[ octave ]; }
+            /**
+             * \brief operators to access the scale space images
+             */
+            Image&		 operator[]( size_t octave )	   { return _image[ octave ]; }
+            const Image& operator[]( size_t octave ) const { return _image[ octave ]; }
 
-			/**
-			 * \brief update the pyramid: pyr[ 0 ] = image; other scales will be computed from that
-			 * \param img the zeroth scale image
-			 */
-			void update( const Image& img );
-		
-			/**
-			 * \brief	returns number of octaves in the pyramid
-			 * \desc	we start counting at 0 for the highest (biggest) image
-			 */	
-			size_t octaves() const { return _image.size(); }
+            /**
+             * \brief update the pyramid: pyr[ 0 ] = image; other scales will be computed from that
+             * \param img the zeroth scale image
+             */
+            void update( const Image& img );
 
-			/**
-			 * \brief	returns the scalefactor between the octaves
-			 */
-			float scaleFactor() const { return _scaleFactor; }
-			
-			/**
-			 *	\brief	set the scalefactor 
-			 */
-			void setScaleFactor( float scale ) { _scaleFactor = scale; }
+            /**
+             * \brief	returns number of octaves in the pyramid
+             * \desc	we start counting at 0 for the highest (biggest) image
+             */
+            size_t octaves() const { return _image.size(); }
 
-			/**
-			 *	\brief	reset the number of octaves of the pyramid
-			 *	\param	o	the new number of octaves
-			 */
-			void setNumOctaves( size_t o );
+            /**
+             * \brief	returns the scalefactor between the octaves
+             */
+            float scaleFactor() const { return _scaleFactor; }
 
-		private:
-			std::vector<Image>	 _image;
-			float				 _scaleFactor;
-            //IScaleFilterBilinear _filter;
-            IScaleFilterGauss    _filter;
-	
-			/* recompute the scale space from the first octave */	
-			void recompute();
-	};
+            /**
+             *	\brief	set the scalefactor
+             */
+            void setScaleFactor( float scale ) { _scaleFactor = scale; }
 
-	inline ImagePyramid::ImagePyramid( size_t octaves, float scaleFactor ) :
-		_scaleFactor( scaleFactor )
-	{
-		setNumOctaves( octaves );
-	}
+            /**
+             *	\brief	reset the number of octaves of the pyramid
+             *	\param	o	the new number of octaves
+             */
+            void setNumOctaves( size_t o );
 
-	inline void ImagePyramid::update( const Image& img )
-	{
-		_image[ 0 ].reallocate( img );
-		_image[ 0 ] = img;
-		recompute();
-	}
+        private:
+            std::vector<Image>	 _image;
+            float				 _scaleFactor;
+            const IScaleFilter&  _filter;
 
-	inline void ImagePyramid::recompute()
-	{
-		float w = _image[ 0 ].width();
-		float h = _image[ 0 ].height();
+            /* recompute the scale space from the first octave */
+            void recompute();
+    };
 
-		for( size_t i = 1; i < _image.size(); i++ ){
-			w *= _scaleFactor;
-			h *= _scaleFactor;
-			_image[ i - 1].scale( _image[ i ], ( size_t )w, ( size_t )h, _filter );
-		}
-	}
+    inline ImagePyramid::ImagePyramid( size_t octaves, float scaleFactor, const IScaleFilter& filter ) :
+        _scaleFactor( scaleFactor ),
+        _filter( filter )
+    {
+        setNumOctaves( octaves );
+    }
 
-	inline void ImagePyramid::setNumOctaves( size_t o )
-	{
-		if( _image.size() == o )
-			return;
+    inline void ImagePyramid::update( const Image& img )
+    {
+        _image[ 0 ].reallocate( img );
+        _image[ 0 ] = img;
+        recompute();
+    }
 
-		_image.resize( o );
-	}
+    inline void ImagePyramid::recompute()
+    {
+        float w = _image[ 0 ].width();
+        float h = _image[ 0 ].height();
+
+        for( size_t i = 1; i < _image.size(); i++ ){
+            w *= _scaleFactor;
+            h *= _scaleFactor;
+            _image[ i - 1].scale( _image[ i ], ( size_t )w, ( size_t )h, _filter );
+        }
+    }
+
+    inline void ImagePyramid::setNumOctaves( size_t o )
+    {
+        if( _image.size() == o )
+            return;
+
+        _image.resize( o );
+    }
 
 }
 
