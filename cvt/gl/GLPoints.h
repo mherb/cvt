90ecb05844936fcc8fead784d3080f634741f41f
Sebastian Klose <kloses@in.tum.de>
2012-07-02 13:50:27 +0200

    using point size 	modified:   cvt/gl/GLPoints.h

--- cvt/gl/GLPoints.h
+++ cvt/gl/GLPoints.h
@@ -1,12 +1,12 @@
 /*
-			CVT - Computer Vision Tools Library
+            CVT - Computer Vision Tools Library
 
- 	 Copyright (c) 2012, Philipp Heise, Sebastian Klose
+     Copyright (c) 2012, Philipp Heise, Sebastian Klose
 
- 	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
- 	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- 	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- 	PARTICULAR PURPOSE.
+    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+    PARTICULAR PURPOSE.
  */
 #ifndef CVT_GLPOINTS_H
 #define CVT_GLPOINTS_H
@@ -15,41 +15,42 @@
 #include <cvt/geom/scene/ScenePoints.h>
 
 namespace cvt {
-	class GLPoints {
-		public:
-			GLPoints();
-			GLPoints( const ScenePoints& pts );
-			/*GLPoints( const Vector3f* vertices, const Vector3f* normals, const Vector2f* texcoords, size_t vsize,
-				    const unsigned int* indices, size_t isize, MeshType type );*/
-			~GLPoints();
+    class GLPoints {
+        public:
+            GLPoints();
+            GLPoints( const ScenePoints& pts );
+            /*GLPoints( const Vector3f* vertices, const Vector3f* normals, const Vector2f* texcoords, size_t vsize,
+                    const unsigned int* indices, size_t isize, MeshType type );*/
+            ~GLPoints();
 
                         void setScenePoints( const ScenePoints& pts );
-			void draw();
+            void draw();
 
-		private:
-			GLVertexArray _vao;
-			GLBuffer _vertices;
-			GLBuffer _colors;
-			size_t	 _numPts;
-	};
+        private:
+            GLVertexArray _vao;
+            GLBuffer _vertices;
+            GLBuffer _colors;
+            size_t	 _numPts;
+            float    _pointSize;
+    };
 
-	inline GLPoints::GLPoints() :
-		_vertices( GL_ARRAY_BUFFER ),
-		_colors( GL_ARRAY_BUFFER ),
-		_numPts( 0 )
-	{
-	}
+    inline GLPoints::GLPoints() :
+        _vertices( GL_ARRAY_BUFFER ),
+        _colors( GL_ARRAY_BUFFER ),
+        _numPts( 0 )
+    {
+    }
 
-	inline GLPoints::GLPoints( const ScenePoints& points ) :
-		_vertices( GL_ARRAY_BUFFER ),
+    inline GLPoints::GLPoints( const ScenePoints& points ) :
+        _vertices( GL_ARRAY_BUFFER ),
                 _colors( GL_ARRAY_BUFFER )
-	{
+    {
                 setScenePoints( points );
-	}
+    }
 
-	inline GLPoints::~GLPoints()
-	{
-	}
+    inline GLPoints::~GLPoints()
+    {
+    }
 
         inline void GLPoints::setScenePoints( const ScenePoints& pts )
         {
@@ -59,12 +60,15 @@ namespace cvt {
 
             _vao.setVertexData( _vertices, 3, GL_FLOAT );
             _vao.setColorData( _colors, 4, GL_FLOAT );
+            _pointSize = pts.pointSize();
         }
 
-	inline void GLPoints::draw()
-	{
-		_vao.draw( GL_POINTS, 0, _numPts );
-	}
+    inline void GLPoints::draw()
+    {
+        glPointSize( _pointSize );
+        _vao.draw( GL_POINTS, 0, _numPts );
+        glPointSize( 1.0f );
+    }
 }
 
 #endif
