33cecce2adc0bf70d37e7e809da4ecb576e3753a
Philipp Heise <heise@in.tum.de>
2012-09-01 12:47:57 +0200

    Fix GCC warning
    	modified:   ../../cvt/math/Math.h
    	modified:   IPolygonRaster.cpp

--- cvt/math/Math.h
+++ cvt/math/Math.h
@@ -83,7 +83,7 @@ namespace cvt {
 			   Standford Graphics */
 			v = v - ( ( v >> 1 ) & 0x55555555 );
 			v = ( v & 0x33333333 ) + ( ( v >> 2 ) & 0x33333333 );
-			return ( ( v + ( v >> 4 ) & 0xF0F0F0F ) * 0x1010101 ) >> 24;
+			return ( ( v + ( ( v >> 4 ) & 0xF0F0F0F ) ) * 0x1010101 ) >> 24;
 		}
 
         template<typename T> static inline T sqr( T v ) { return v * v; }
