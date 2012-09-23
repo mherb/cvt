69f6bae20447ec0e9655e0add940d44dfe2c37d4
Sebastian Klose <kloses@in.tum.de>
2012-09-23 17:49:23 +0200

    added Systembuilder for rgbdvo
    	modified:   cvt/CMakeLists.txt
    	modified:   cvt/vision/rgbdvo/Optimizer.h
    	new file:   cvt/vision/rgbdvo/SystemBuilder.h
    	modified:   tests/rgbdvo/RGBDVOApp.h

--- cvt/vision/rgbdvo/Optimizer.h
+++ cvt/vision/rgbdvo/Optimizer.h
@@ -13,6 +13,7 @@
 #define OPTIMIZER_H
 
 #include <cvt/vision/rgbdvo/RGBDKeyframe.h>
+#include <cvt/vision/rgbdvo/SystemBuilder.h>
 
 namespace cvt {
 
@@ -24,16 +25,17 @@ namespace cvt {
             typedef typename RGBDKeyframe<WarpFunc>::Result   ResultType;
 
             Optimizer();
+            virtual ~Optimizer(){}
 
             void setMaxIterations( size_t iter ) { _maxIter = iter; }
             void setMinUpdate( float v )         { _minUpdate = v; }
             void setRobustThreshold( float v )   { _robustThreshold = v; }
 
-            void optimize( ResultType& result,
-                           const Matrix4f& posePrediction,
-                           RGBDKeyframe<WarpFunc>& reference,
-                           const ImagePyramid& grayPyramid,
-                           const Image& depthImage ) const;
+            virtual void optimize( ResultType& result,
+                                   const Matrix4f& posePrediction,
+                                   RGBDKeyframe<WarpFunc>& reference,
+                                   const ImagePyramid& grayPyramid,
+                                   const Image& depthImage ) const;
 
 
         private:
@@ -45,17 +47,6 @@ namespace cvt {
             float   _minUpdate;
             float   _robustThreshold;
 
-            void optimizeNonRobust( ResultType& result,
-                                    const Matrix4f& posePrediction,
-                                    RGBDKeyframe<WarpFunc>& reference,
-                                    const ImagePyramid& grayPyramid,
-                                    const Image& depthImage ) const;
-
-            void optimizeRobust( ResultType& result,
-                                 const Matrix4f& posePrediction,
-                                 RGBDKeyframe<WarpFunc>& reference,
-                                 const ImagePyramid& grayPyramid,
-                                 const Image& depthImage ) const;
     };
 
     template <class WarpFunc, class LossFunc>
@@ -71,117 +62,7 @@ namespace cvt {
                                                          const Matrix4f& posePrediction,
                                                          RGBDKeyframe<WarpFunc> &reference,
                                                          const ImagePyramid& grayPyramid,
-                                                         const Image& depthImage ) const
-    {
-        if( IsRobustWeighting<LossFunc>::Value )
-            optimizeRobust( result, posePrediction, reference, grayPyramid, depthImage );
-        else
-            optimizeNonRobust( result, posePrediction, reference, grayPyramid, depthImage );
-    }
-
-    template <class WarpFunc, class LossFunc>
-    inline void Optimizer<WarpFunc, LossFunc>::optimizeNonRobust( ResultType& result,
-                                                                  const Matrix4f& posePrediction,
-                                                                  RGBDKeyframe<WarpFunc>& reference,
-                                                                  const ImagePyramid& grayPyramid,
-                                                                  const Image& /*depthImage*/ ) const
-    {
-        SIMD* simd = SIMD::instance();
-        Matrix4f tmp4;
-        tmp4 = posePrediction.inverse() * reference.pose();
-
-        result.warp.setPose( tmp4 );
-        result.costs = 0.0f;
-        result.iterations = 0;
-        result.numPixels = 0;
-        result.pixelPercentage = 0.0f;
-
-        Matrix4f projMat;
-        std::vector<Vector2f> warpedPts;
-        std::vector<float> interpolatedPixels;
-        // sum of jacobians * delta
-        JacobianType deltaSum, jtmp;
-
-        for( int o = grayPyramid.octaves() - 1; o >= 0; o-- ){
-            ResultType scaleResult;
-            scaleResult = result;
-
-            const size_t width = grayPyramid[ o ].width();
-            const size_t height = grayPyramid[ o ].height();
-
-            const AlignDataType& data = reference.dataForScale( o );
-
-            const size_t num = data.points3d.size();
-            Matrix4f K4( data.intrinsics );
-            const Vector3f* p3dPtr = &data.points3d[ 0 ];
-            const float* referencePixVals = &data.pixelValues[ 0 ];
-            const JacobianType* referenceJ = &data.jacobians[ 0 ];
-
-            warpedPts.resize( num );
-            interpolatedPixels.resize( num );
-
-            IMapScoped<const float> grayMap( grayPyramid[ o ] );
-
-            scaleResult.iterations = 0;
-            scaleResult.numPixels = 0;
-            scaleResult.pixelPercentage = 0.0f;
-
-            while( scaleResult.iterations < _maxIter ){
-                // build the updated projection Matrix
-                projMat = K4 * scaleResult.warp.poseMatrix();
-
-                // project the points:
-                simd->projectPoints( &warpedPts[ 0 ], projMat, p3dPtr, num );
-
-                // interpolate the pixel values
-                simd->warpBilinear1f( &interpolatedPixels[ 0 ], &warpedPts[ 0 ].x, grayMap.ptr(), grayMap.stride(), width, height, -1.0f, num );
-
-                deltaSum.setZero();
-                scaleResult.numPixels = 0;
-                scaleResult.costs = 0.0f;
-                for( size_t i = 0; i < num; i++ ){
-                    // compute the delta
-                    if( interpolatedPixels[ i ] >= 0.0f ){
-                        float delta = result.warp.computeResidual( referencePixVals[ i ], interpolatedPixels[ i ] );
-                        scaleResult.costs += Math::sqr( delta );
-                        scaleResult.numPixels++;
-
-                        jtmp = delta * referenceJ[ i ];
-                        deltaSum += jtmp;
-                    }
-                }
-
-                if( !scaleResult.numPixels ){
-                    break;
-                }
-
-                // evaluate the delta parameters
-                DeltaType deltaP = -data.inverseHessian * deltaSum.transpose();
-                scaleResult.warp.updateParameters( deltaP );
-
-                scaleResult.iterations++;
-                if( deltaP.norm() < _minUpdate )
-                    break;
-            }
-
-            if( scaleResult.numPixels )
-                scaleResult.pixelPercentage = ( float )scaleResult.numPixels / ( float )num;
-
-            // TODO: ensure the result on this scale is good enough (pixel percentage & error )
-            result = scaleResult;
-        }
-
-        tmp4 = result.warp.poseMatrix();
-        tmp4 = reference.pose() * tmp4.inverse();
-        result.warp.setPose( tmp4 );
-    }
-
-    template <class WarpFunc, class LossFunc>
-    inline void Optimizer<WarpFunc, LossFunc>::optimizeRobust( ResultType& result,
-                                                               const Matrix4f& posePrediction,
-                                                               RGBDKeyframe<WarpFunc>& reference,
-                                                               const ImagePyramid& grayPyramid,
-                                                               const Image& /*depthImage*/ ) const
+                                                         const Image& /*depthImage*/ ) const
     {
         SIMD* simd = SIMD::instance();
         Matrix4f tmp4;
@@ -195,12 +76,14 @@ namespace cvt {
 
         /* TODO: robust statistics should use median of residuals for threshold */
         LossFunc weighter( _robustThreshold );
+        SystemBuilder<LossFunc> builder( weighter );
 
         Matrix4f projMat;
         std::vector<Vector2f> warpedPts;
         std::vector<float> interpolatedPixels;
+        std::vector<float> residuals;
         // sum of jacobians * delta
-        JacobianType deltaSum, jtmp;
+        JacobianType deltaSum;
         HessianType  hessian;
 
         for( int o = grayPyramid.octaves() - 1; o >= 0; o-- ){
@@ -220,14 +103,13 @@ namespace cvt {
 
             warpedPts.resize( num );
             interpolatedPixels.resize( num );
+            residuals.resize( num );
 
             IMapScoped<const float> grayMap( grayPyramid[ o ] );
 
             scaleResult.iterations = 0;
             scaleResult.numPixels = 0;
             scaleResult.pixelPercentage = 0.0f;
-
-            hessian.setZero();
 
             while( scaleResult.iterations < _maxIter ){
                 // build the updated projection Matrix
@@ -238,25 +120,16 @@ namespace cvt {
 
                 // interpolate the pixel values
                 simd->warpBilinear1f( &interpolatedPixels[ 0 ], &warpedPts[ 0 ].x, grayMap.ptr(), grayMap.stride(), width, height, -1.0f, num );
+                scaleResult.warp.computeResiduals( &residuals[ 0 ], referencePixVals, &interpolatedPixels[ 0 ], num );
 
-                deltaSum.setZero();
-                scaleResult.numPixels = 0;
-                scaleResult.costs = 0.0f;
-                for( size_t i = 0; i < num; i++ ){
-                    // compute the delta
-                    if( interpolatedPixels[ i ] >= 0.0f ){
-                        float delta = result.warp.computeResidual( referencePixVals[ i ], interpolatedPixels[ i ] );
-                        scaleResult.costs += Math::sqr( delta );
-                        scaleResult.numPixels++;
-
-                        float weight = weighter.weight( delta );
-                        jtmp = weight * referenceJ[ i ];
-
-                        hessian.noalias() += jtmp.transpose() * referenceJ[ i ];
-                        deltaSum.noalias() += jtmp * delta;
-                    }
-                }
-
+                /* a hack: the builder does not touch the hessian if its a non robust lossfunc!*/
+                hessian = data.hessian;
+                scaleResult.numPixels = builder.build( hessian, deltaSum,
+                                                       referenceJ,
+                                                       &residuals[ 0 ],
+                                                       &interpolatedPixels[ 0 ],
+                                                       scaleResult.costs,
+                                                       num );
                 if( !scaleResult.numPixels ){
                     break;
                 }
