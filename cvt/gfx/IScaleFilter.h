dd2966aacc07bb69a5ba5a072a96f60826db4c0e
Sebastian Klose <kloses@in.tum.de>
2012-11-20 09:35:27 +0100

    removed unnecessary semicolons 	modified:   cvt/gfx/IScaleFilter.h

--- cvt/gfx/IScaleFilter.h
+++ cvt/gfx/IScaleFilter.h
@@ -33,8 +33,8 @@ namespace cvt {
 
 	class IScaleFilter {
 		public:
-			IScaleFilter( float support = 1.0f, float sharpsmooth = 0.0f ) : _support( support ), _sharpsmooth( sharpsmooth ) {};
-			virtual ~IScaleFilter() {};
+			IScaleFilter( float support = 1.0f, float sharpsmooth = 0.0f ) : _support( support ), _sharpsmooth( sharpsmooth ) {}
+			virtual ~IScaleFilter() {}
 			virtual float eval( float x ) const = 0;
 			virtual const std::string name() const = 0;
 			size_t getAdaptiveConvolutionWeights( size_t dst, size_t src, IConvolveAdaptivef& conva, bool nonegincr = true ) const;
@@ -48,65 +48,65 @@ namespace cvt {
 	class IScaleFilterBilinear : public IScaleFilter
 	{
 		public:
-			IScaleFilterBilinear( float sharpsmooth = 0.0f ) : IScaleFilter( 1.0f, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Bilinear"; };
+			IScaleFilterBilinear( float sharpsmooth = 0.0f ) : IScaleFilter( 1.0f, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "Bilinear"; }
 	};
 
 	class IScaleFilterCubic : public IScaleFilter
 	{
 		public:
-			IScaleFilterCubic( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Cubic"; };
+			IScaleFilterCubic( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "Cubic"; }
 	};
 
 	class IScaleFilterCatmullRom : public IScaleFilter
 	{
 		public:
-			IScaleFilterCatmullRom( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "CatmullRom"; };
+			IScaleFilterCatmullRom( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "CatmullRom"; }
 	};
 
 	class IScaleFilterMitchell : public IScaleFilter
 	{
 		public:
-			IScaleFilterMitchell( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Mitchell"; };
+			IScaleFilterMitchell( float sharpsmooth = 0.0f ) : IScaleFilter( 2.0f, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "Mitchell"; }
 	};
 
 	class IScaleFilterLanczos : public IScaleFilter
 	{
 		public:
-			IScaleFilterLanczos( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Lanczos"; };
+			IScaleFilterLanczos( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "Lanczos"; }
 	};
 
 	class IScaleFilterBlackman : public IScaleFilter
 	{
 		public:
-			IScaleFilterBlackman( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Blackman"; };
+			IScaleFilterBlackman( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "Blackman"; }
 	};
 
 	class IScaleFilterBlackmanHarris : public IScaleFilter
 	{
 		public:
-			IScaleFilterBlackmanHarris( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {};
-			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "BlackmanHarris"; };
+			IScaleFilterBlackmanHarris( float support = 3.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {}
+			virtual float eval( float x ) const;
+			virtual const std::string name() const { return "BlackmanHarris"; }
 	};
 
 	class IScaleFilterGauss : public IScaleFilter
 	{
 		public:
-			IScaleFilterGauss( float support = 2.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {};
+			IScaleFilterGauss( float support = 2.0f, float sharpsmooth = 0.0f ) : IScaleFilter( support, sharpsmooth ) {}
 			virtual float eval( float x ) const ;
-			virtual const std::string name() const { return "Gauss"; };
+			virtual const std::string name() const { return "Gauss"; }
 	};
 
 };
