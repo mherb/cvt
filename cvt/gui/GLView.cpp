daa62afab4dbffb71f9305800c7b86ad92c0252c
Philipp Heise <heise@in.tum.de>
2013-05-05 17:30:27 +0200

    Fix nasty GL bug ...
    	modified:   ../cvt/gui/GLView.cpp

--- cvt/gui/GLView.cpp
+++ cvt/gui/GLView.cpp
@@ -17,7 +17,7 @@ namespace cvt {
 	void GLView::paintEvent( PaintEvent& ev, GFX& g )
 	{
 		if( g.supportsGL() ){
-			_viewport = ( ( GFXEngineGL* ) &g._engine )->viewport();
+			_viewport = ( ( GFXEngineGL* ) g._engine )->viewport();
 			g.beginGL();
 			_allowViewportChange = true;
 			paintGLEvent( ev );
