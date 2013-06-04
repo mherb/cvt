5f583f22abcbd75465002cad51a994819f39dfad
Sebastian Klose <kloses@in.tum.de>
2013-06-04 12:44:30 +0200

    EPnPSac template for float and double
    	modified:   cvt/math/sac/EPnPSAC.h

--- cvt/math/sac/EPnPSAC.h
+++ cvt/math/sac/EPnPSAC.h
@@ -24,30 +24,37 @@
 
 namespace cvt
 {
-    class EPnPSAC;
+	template <typename T> class EPnPSAC;
 
     template<>
-    struct SACModelTraits<EPnPSAC>
+	struct SACModelTraits<EPnPSAC<float> >
     {
-        typedef Matrix4d   ResultType;
-        typedef double      DistanceType;
+		typedef Matrix4f	ResultType;
+		typedef float		DistanceType;
     };
+	template<>
+	struct SACModelTraits<EPnPSAC<double> >
+	{
+		typedef Matrix4d	ResultType;
+		typedef double      DistanceType;
+	};
 
-    class EPnPSAC : public SampleConsensusModel<EPnPSAC>
-{
+	template <typename T>
+	class EPnPSAC : public SampleConsensusModel<EPnPSAC<T> >
+	{
       public:
-        typedef SACModelTraits<EPnPSAC>::ResultType       ResultType;
-        typedef SACModelTraits<EPnPSAC>::DistanceType     DistanceType;
+		typedef typename SACModelTraits<EPnPSAC<T> >::ResultType	ResultType;
+		typedef typename SACModelTraits<EPnPSAC>::DistanceType		DistanceType;
 
 
-        EPnPSAC( const PointSet3d & p3d, const PointSet2d & p2d, const Matrix3d & K ); 
+		EPnPSAC( const PointSet<3, T> & p3d, const PointSet<2, T> & p2d, const Matrix3<T> & K );
 
         size_t size() const { return _points3d.size(); }
 
         size_t minSampleSize() const
         {
             // what is a good number for EPnP?
-            // In principle it should work with 4
+			// In theory it should work with 4
             return 5;
         }
 
@@ -57,55 +64,60 @@ namespace cvt
         void inliers( std::vector<size_t> & inlierIndices, const ResultType & estimate, const DistanceType maxDistance ) const;
 
       private:
-        const PointSet3d &    _points3d;
-        const PointSet2d &    _points2d;
-        const Matrix3d	 &    _intrinsics;
+		const PointSet<3, T> &    _points3d;
+		const PointSet<2, T> &    _points2d;
+		const Matrix3<T>	 &    _intrinsics;
     };
 
         
-	inline EPnPSAC::EPnPSAC( const PointSet3d & p3d, const PointSet2d & p2d, const Matrix3d & K ) :
+	template <typename T>
+	inline EPnPSAC<T>::EPnPSAC( const PointSet<3, T> & p3d, const PointSet<2, T> & p2d, const Matrix3<T> & K ) :
 		_points3d( p3d ),
 		_points2d( p2d ),
 		_intrinsics( K )
     {
     }
 
-    inline EPnPSAC::ResultType EPnPSAC::estimate( const std::vector<size_t> & sampleIndices ) const
+	template <typename T>
+	inline typename EPnPSAC<T>::ResultType EPnPSAC<T>::estimate( const std::vector<size_t> & sampleIndices ) const
     {
-        PointSet3d p3d;
-        PointSet2d p2d;
+		PointSet<3, T> p3d;
+		PointSet<2, T> p2d;
 
         for( size_t i = 0; i < sampleIndices.size(); i++ ){
             p3d.add( _points3d[ sampleIndices[ i ] ] );
             p2d.add( _points2d[ sampleIndices[ i ] ] );
         }
 		
-		EPnPd epnp( p3d );
+		EPnP<T> epnp( p3d );
 
-		Matrix4d trans;
+		Matrix4<T> trans;
 		epnp.solve( trans, p2d, _intrinsics );
 
         return trans; 
     }
 
-    inline EPnPSAC::ResultType EPnPSAC::refine( const ResultType&, const std::vector<size_t> & inlierIndices ) const
+	template <typename T>
+	inline typename EPnPSAC<T>::ResultType EPnPSAC<T>::refine( const ResultType&, const std::vector<size_t> & inlierIndices ) const
     {
         // TODO: would be nicer, to use estimate, to get a linear estimate,
         //       and then refine it iteratively using GN or LM e.g.
         return estimate( inlierIndices );
     }
 
-    inline void EPnPSAC::inliers( std::vector<size_t> & inlierIndices,
-                                 const ResultType & estimate,
-                                 const DistanceType maxDistance ) const
+	template <typename T>
+	inline void EPnPSAC<T>::inliers( std::vector<size_t> & inlierIndices,
+									 const ResultType & estimate,
+									 const DistanceType maxDistance ) const
     {
         // reproject all matches and compute the inliers
-        Vector2d p2;
-        Vector3d p3;
+		Vector2<T> p2;
+		Vector3<T> p3;
 
 		/* invert the pose */
-		Matrix3d R = _intrinsics * estimate.toMatrix3();
-        Vector3d t( estimate[ 0 ][ 3 ], estimate[ 1 ][ 3 ], estimate[ 2 ][ 3 ] );
+		Matrix3<T> R = _intrinsics * estimate.toMatrix3();
+		Vector3<T> t( estimate[ 0 ][ 3 ], estimate[ 1 ][ 3 ], estimate[ 2 ][ 3 ] );
+
 		// apply intrinsics
 		t = _intrinsics * t;
 
@@ -113,7 +125,7 @@ namespace cvt
             // calc p' = estimate * p
             p3 = R * _points3d[ i ] + t;
 
-			if( Math::abs( p3.z ) < 1e-6 )
+			if( Math::abs( p3.z ) < ( T )1e-6 )
 				continue;
 
 			p2.x = p3.x / p3.z;
@@ -123,6 +135,7 @@ namespace cvt
                 inlierIndices.push_back( i );
         }
     }
+
 }
 
 #endif
