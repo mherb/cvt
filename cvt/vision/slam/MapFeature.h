799a08ec7a82c30514d4ecdc9e83641f93543bc3
Sebastian Klose <kloses@in.tum.de>
2013-06-04 12:51:18 +0200

    indent 	modified:   MapFeature.h

--- cvt/vision/slam/MapFeature.h
+++ cvt/vision/slam/MapFeature.h
@@ -31,13 +31,13 @@ namespace cvt
          MapFeature( const Eigen::Vector4d & p, const Eigen::Matrix4d & covariance );
          MapFeature();
 
-         Eigen::Vector4d&       estimate()         { return _point; }
-         const Eigen::Vector4d&	estimate()   const { return _point; }
-         Eigen::Matrix4d&		covariance()          { return _covariance; }
-         const Eigen::Matrix4d&	covariance() const { return _covariance; }
+		 Eigen::Vector4d&       estimate()			{ return _point; }
+		 const Eigen::Vector4d&	estimate()   const	{ return _point; }
+		 Eigen::Matrix4d&		covariance()		{ return _covariance; }
+		 const Eigen::Matrix4d&	covariance() const	{ return _covariance; }
 
-         ConstPointTrackIterator pointTrackBegin() const	{ return _pointTrack.begin(); };
-         ConstPointTrackIterator pointTrackEnd() const	{ return _pointTrack.end();   };
+		 ConstPointTrackIterator pointTrackBegin()	const	{ return _pointTrack.begin(); }
+		 ConstPointTrackIterator pointTrackEnd()	const	{ return _pointTrack.end();   }
 
          void addPointTrack( size_t camId ) { _pointTrack.insert( camId ); }
 
