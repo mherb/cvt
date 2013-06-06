22e84401f731ee30f6e3154989909468ba785dda
Sebastian Klose <kloses@cs.tum.edu>
2013-06-06 18:22:19 +0200

    added StereoInput base class
    	modified:   CMakeLists.txt
    	new file:   io/StereoInput.h

---
+++ cvt/io/StereoInput.h
@@ -0,0 +1,43 @@
+/*
+            CVT - Computer Vision Tools Library
+
+     Copyright (c) 2012, Philipp Heise, Sebastian Klose
+
+    THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+    PARTICULAR PURPOSE.
+ */
+
+#ifndef CVT_STEREO_INPUT_H
+#define CVT_STEREO_INPUT_H
+
+#include <cvt/gfx/Image.h>
+#include <cvt/vision/StereoCameraCalibration.h>
+
+namespace cvt {
+    class StereoInput {
+        public:
+            virtual ~StereoInput(){}
+
+            virtual bool            nextFrame() = 0;
+
+            /**
+              left rectified image
+             */
+            virtual const Image&    left() const = 0;
+
+            /**
+              left rectified image
+             */
+            virtual const Image&    right() const = 0;
+
+            /**
+              calibration data (rectified rig)
+             */
+            virtual const StereoCameraCalibration& stereoCalib() const = 0;
+
+    };
+}
+
+#endif
