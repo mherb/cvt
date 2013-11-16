db69c8febf7fc08f7173d0707a1ece24e433bf11
Markus Herb <mail@markusherb.de>
2013-11-16 13:26:42 +0100

    Add AVX version of SAD and SSD

--- cvt/util/SIMDAVX.cpp
+++ cvt/util/SIMDAVX.cpp
@@ -23,10 +23,114 @@
 */
 
 #include <cvt/util/SIMDAVX.h>
-
-#include <xmmintrin.h>
-#include <smmintrin.h>
+#include <immintrin.h>
 
 namespace cvt
 {
+	float SIMDAVX::SAD( float const* src1, float const* src2, const size_t n ) const
+	{
+		size_t i = n >> 3;
+
+		__m256 a, b, diff, negdiff, max, sum;
+		sum = _mm256_setzero_ps( );
+		if( ( ( size_t ) src1 | ( size_t ) src2 ) & 0x1f ) {
+			while( i-- ) {
+				a = _mm256_loadu_ps( src1 );
+				b = _mm256_loadu_ps( src2 );
+				diff = _mm256_sub_ps( a, b );
+				negdiff = _mm256_sub_ps( _mm256_setzero_ps( ), diff );
+				max = _mm256_max_ps( diff, negdiff );
+				sum = _mm256_add_ps( sum, max );
+				src1 += 8; src2 += 8;
+			}
+		} else {
+			while( i-- ) {
+				a = _mm256_load_ps( src1 );
+				b = _mm256_load_ps( src2 );
+				diff = _mm256_sub_ps( a, b );
+				negdiff = _mm256_sub_ps( _mm256_setzero_ps( ), diff );
+				max = _mm256_max_ps( diff, negdiff );
+				sum = _mm256_add_ps( sum, max );
+				src1 += 8; src2 += 8;
+			}
+		}
+
+		float sad = 0.0f;
+
+		// Reduce sum to single float
+		float tmp[ 8 ];
+		if( ( ( size_t ) tmp ) & 0x1f ) {
+			_mm256_storeu_ps( tmp, sum );
+		} else {
+			_mm256_stream_ps( tmp, sum );
+		}
+
+		i = 8;
+		while( i-- ) {
+			sad += tmp[ i ];
+		}
+
+		i = n & 0x7;
+		while( i-- ) {
+			sad += Math::abs( *src1++ - *src2++ );
+		}
+
+		// Zero upper half of AVX registers to avoid AVX-SSE transition penalties
+		_mm256_zeroupper( );
+
+		return sad;
+	}
+
+	float SIMDAVX::SSD( float const* src1, float const* src2, const size_t n ) const
+	{
+		size_t i = n >> 3;
+
+		__m256 a, b, diff, sqr, sum;
+		sum = _mm256_setzero_ps( );
+		if( ( ( size_t ) src1 | ( size_t ) src2 ) & 0x1f ) {
+			while( i-- ) {
+				a = _mm256_loadu_ps( src1 );
+				b = _mm256_loadu_ps( src2 );
+				diff = _mm256_sub_ps( a, b );
+				sqr = _mm256_mul_ps( diff, diff );
+				sum = _mm256_add_ps( sum, sqr );
+				src1 += 8; src2 += 8;
+			}
+		} else {
+			while( i-- ) {
+				a = _mm256_load_ps( src1 );
+				b = _mm256_load_ps( src2 );
+				diff = _mm256_sub_ps( a, b );
+				sqr = _mm256_mul_ps( diff, diff );
+				sum = _mm256_add_ps( sum, sqr );
+				src1 += 8; src2 += 8;
+			}
+		}
+
+		float ssd = 0.0f;
+
+		// Reduce sum to single float
+		float tmp[ 8 ];
+		if( ( ( size_t ) tmp ) & 0x1f ) {
+			_mm256_storeu_ps( tmp, sum );
+		} else {
+			_mm256_stream_ps( tmp, sum );
+		}
+
+		i = 8;
+		while( i-- ) {
+			ssd += tmp[ i ];
+		}
+
+		i = n & 0x7;
+		while( i-- ) {
+			ssd += Math::sqr( *src1++ - *src2++ );
+		}
+
+		// Zero upper half of AVX registers to avoid AVX-SSE transition penalties
+		_mm256_zeroupper( );
+
+		return ssd;
+	}
+
 }
