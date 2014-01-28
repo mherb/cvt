9f3aa61186b0feafc9545bee8c20e7e3d9cf650d
Sebastian Klose <kloses@in.tum.de>
2014-01-28 10:34:21 +0100

    indent fix
    	modified:   cvt/util/ParamSet.h

--- cvt/util/ParamSet.h
+++ cvt/util/ParamSet.h
@@ -31,121 +31,121 @@
 
 namespace cvt
 {
-	class ParamSet;
-	std::ostream & operator<<( std::ostream & out, const ParamSet& pSet );
+    class ParamSet;
+    std::ostream & operator<<( std::ostream & out, const ParamSet& pSet );
 
-	class ParamSet
-	{
-		public:
-                        /**
-                         * \brief Constructor for a ParamSet
-                         * \param pInfos	array of ParamInfo
-                         * \param n		size of array
-                         * \param genOffsets	if true, the offsets will be generated and set during construction
-                         *                      if e.g. you want to store your params in a struct, you should specify the offsets yourself!
-			 */
-			ParamSet( ParamInfo** pInfos, size_t n, bool genOffsets = true );
+    class ParamSet
+    {
+        public:
+            /**
+             * \brief Constructor for a ParamSet
+             * \param pInfos	array of ParamInfo
+             * \param n		size of array
+             * \param genOffsets	if true, the offsets will be generated and set during construction
+             *                      if e.g. you want to store your params in a struct,
+             *                      you should specify the offsets yourself!
+             */
+            ParamSet( ParamInfo** pInfos, size_t n, bool genOffsets = true );
 
-			~ParamSet();
+            ~ParamSet();
 
-                        /**
-                         *  \brief get handle to a parameter with the given name
-                         *  \param name name of the parameter
-                         *  \return the handle (id) which can be used to access the parameter
-                         */
-			size_t paramHandle( const std::string & name );
+            /**
+             *  \brief get handle to a parameter with the given name
+             *  \param name name of the parameter
+             *  \return the handle (id) which can be used to access the parameter
+             */
+            size_t paramHandle( const std::string & name );
 
-			/* set parameter */
-			template <class T>
-			void setArg( size_t handle, T value, size_t localIndex = 0 );
+            /* set parameter */
+            template <class T>
+            void setArg( size_t handle, T value, size_t localIndex = 0 );
 
-			template <class T>
-			T arg( size_t handle, size_t localIndex = 0 ) const;
+            template <class T>
+            T arg( size_t handle, size_t localIndex = 0 ) const;
 
-			template <typename T>
-			T * ptr();
-		
-			template <class T>
-			const std::string & selectionString( size_t handle );
-        
+            template <typename T>
+            T * ptr();
+
+            template <class T>
+            const std::string & selectionString( size_t handle );
             size_t  size() const { return _numParameters; }
-       
-		   	const ParamInfo* paramInfo( size_t i ) const;
 
-		private:
-			uint8_t *	_parameterMem;
-			ParamInfo**	_pInfos;
-			size_t		_numParameters;
+            const ParamInfo* paramInfo( size_t i ) const;
 
-			friend std::ostream& operator<< ( std::ostream &, const ParamSet& );
-	};
+        private:
+            uint8_t *	_parameterMem;
+            ParamInfo**	_pInfos;
+            size_t		_numParameters;
 
-	template<class T>
-	inline void ParamSet::setArg( size_t handle, T value, size_t localIndex )
-	{
-		ParamInfo * pInfo = _pInfos[ handle ];
-		
-		if( localIndex > pInfo->count )
-			throw CVTException( "Parameter \"" + pInfo->name + "\" local index out of bounds!" );
+            friend std::ostream& operator<< ( std::ostream &, const ParamSet& );
+    };
 
-		if( !PTypeCheck<T>::check( pInfo->type ) ){
-			throw CVTException( "Parameter \"" + pInfo->name + "\" types do not match!" );
-		}
-		
-		// Type T is ensured here!
-		ParamInfoTyped<T>* pInfoT =  ( ParamInfoTyped<T>* )pInfo;
-		if( pInfo->hasRange ){
-			if( value < pInfoT->minValue() ){
-				value = pInfoT->minValue();
-			} else if( value > pInfoT->maxValue() ) {
-				value = pInfoT->maxValue();
-			}
-		}
+    template<class T>
+    inline void ParamSet::setArg( size_t handle, T value, size_t localIndex )
+    {
+        ParamInfo * pInfo = _pInfos[ handle ];
 
-		*( T* )( _parameterMem + pInfo->offset + localIndex * sizeof( T ) ) = value;
-	}
+        if( localIndex > pInfo->count )
+            throw CVTException( "Parameter \"" + pInfo->name + "\" local index out of bounds!" );
 
-	template <class T>
-	inline T ParamSet::arg( size_t handle, size_t localIndex ) const
-	{
-		ParamInfo * pInfo = _pInfos[ handle ];
+        if( !PTypeCheck<T>::check( pInfo->type ) ){
+            throw CVTException( "Parameter \"" + pInfo->name + "\" types do not match!" );
+        }
 
-		if( localIndex > pInfo->count )
-			throw CVTException( "Parameter \"" + pInfo->name + "\" local index out of bounds!" );
+        // Type T is ensured here!
+        ParamInfoTyped<T>* pInfoT =  ( ParamInfoTyped<T>* )pInfo;
+        if( pInfo->hasRange ){
+            if( value < pInfoT->minValue() ){
+                value = pInfoT->minValue();
+            } else if( value > pInfoT->maxValue() ) {
+                value = pInfoT->maxValue();
+            }
+        }
 
-		if( !PTypeCheck<T>::check( pInfo->type ) ){
-			throw CVTException( "Parameter \"" + pInfo->name + "\" types do not match!" );
-		}
+        *( T* )( _parameterMem + pInfo->offset + localIndex * sizeof( T ) ) = value;
+    }
 
-		return *( T* )( _parameterMem + pInfo->offset + localIndex * sizeof( T ) );
-	}	
+    template <class T>
+    inline T ParamSet::arg( size_t handle, size_t localIndex ) const
+    {
+        ParamInfo * pInfo = _pInfos[ handle ];
 
-	template <class T>
-	inline T * ParamSet::ptr()
-	{
-		return ( T* )_parameterMem;
-	}
-	
-	template<>
-	inline const std::string & ParamSet::selectionString<Selection>( size_t handle )
-	{
-		ParamInfo * p = _pInfos[ handle ];
-		if( p->type != PTYPE_SELECTION ){
-			throw CVTException( "Not a selection type" );
-		}
+        if( localIndex > pInfo->count )
+            throw CVTException( "Parameter \"" + pInfo->name + "\" local index out of bounds!" );
 
-		Selection current = this->arg<Selection>( handle );
-		
-		return ( ( ParamInfoTyped<Selection>* )p )->description( current );
-	}
+        if( !PTypeCheck<T>::check( pInfo->type ) ){
+            throw CVTException( "Parameter \"" + pInfo->name + "\" types do not match!" );
+        }
+
+        return *( T* )( _parameterMem + pInfo->offset + localIndex * sizeof( T ) );
+    }
+
+    template <class T>
+    inline T * ParamSet::ptr()
+    {
+        return ( T* )_parameterMem;
+    }
+
+    template<>
+    inline const std::string & ParamSet::selectionString<Selection>( size_t handle )
+    {
+        ParamInfo * p = _pInfos[ handle ];
+        if( p->type != PTYPE_SELECTION ){
+            throw CVTException( "Not a selection type" );
+        }
+
+        Selection current = this->arg<Selection>( handle );
+
+        return ( ( ParamInfoTyped<Selection>* )p )->description( current );
+    }
 
 
-	inline const ParamInfo* ParamSet::paramInfo( size_t i ) const
-	{
-		if( i >= _numParameters )
-			throw CVTException( "Out of parameter bounds!" );
-		return _pInfos[ i ];
-	}
+    inline const ParamInfo* ParamSet::paramInfo( size_t i ) const
+    {
+        if( i >= _numParameters )
+            throw CVTException( "Out of parameter bounds!" );
+        return _pInfos[ i ];
+    }
 }
 
 #endif
