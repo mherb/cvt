c8ff58c55fb14e1fa6195d486cda1d7dd1e22d9a
Brian Jensen <Jensen.J.Brian@gmail.com>
2014-07-03 16:06:17 +0200

    fixed error in non POD scoped buffer size decrement past zero

--- cvt/util/ScopedBuffer.h
+++ cvt/util/ScopedBuffer.h
@@ -20,7 +20,7 @@
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
-*/
+ */
 
 #ifndef CVT_SCOPEDBUFFER_H
 #define CVT_SCOPEDBUFFER_H
@@ -29,123 +29,128 @@
 #include <cvt/util/Util.h>
 #include <stdint.h>
 
-namespace cvt {
-	template<typename T, bool is_pod = false > class ScopedBuffer;
+namespace cvt
+{
+    template<typename T, bool is_pod = false >
+    class ScopedBuffer;
 
-	template<typename T>
-	class ScopedBuffer<T,false> {
-		public:
-			ScopedBuffer( size_t size, size_t alignment = 16 );
-			~ScopedBuffer();
+    template<typename T>
+    class ScopedBuffer<T, false>
+    {
+        public:
+            ScopedBuffer( size_t size, size_t alignment = 16 );
+            ~ScopedBuffer( );
 
-			size_t size() const;
+            size_t size( ) const;
 
-			const T* ptr() const;
-			T* ptr();
+            const T* ptr( ) const;
+            T* ptr( );
 
-		private:
-			ScopedBuffer( const ScopedBuffer<T>& buffer );
+        private:
+            ScopedBuffer( const ScopedBuffer<T>& buffer );
 
-			size_t	   _size;
-			uint8_t*   _memptr;
-			T*		   _buffer;
-	};
+            size_t _size;
+            uint8_t*   _memptr;
+            T*         _buffer;
+    };
 
-	template<typename T>
-	class ScopedBuffer<T, true> {
-		public:
-			ScopedBuffer( size_t size, size_t alignment = 16 );
-			~ScopedBuffer();
+    template<typename T>
+    class ScopedBuffer<T, true>
+    {
+        public:
+            ScopedBuffer( size_t size, size_t alignment = 16 );
+            ~ScopedBuffer( );
 
-			size_t size() const;
+            size_t size( ) const;
 
-			const T* ptr() const;
-			T* ptr();
+            const T* ptr( ) const;
+            T* ptr( );
 
-		private:
-			ScopedBuffer( const ScopedBuffer<T>& buffer );
+        private:
+            ScopedBuffer( const ScopedBuffer<T>& buffer );
 
-			size_t	   _size;
-			uint8_t*   _memptr;
-			T*		   _buffer;
-	};
+            size_t _size;
+            uint8_t*   _memptr;
+            T*         _buffer;
+    };
 
 
-	template<typename T>
-	inline ScopedBuffer<T, false>::ScopedBuffer( size_t size, size_t alignment ) : _size( size )
-	{
-        size_t memsize = Math::pad( sizeof( T ) * _size, alignment ) + alignment;// needed for 32-bit architectures!
-		_memptr = new uint8_t[ memsize ];
-		_buffer = Util::alignPtr<T>( ( T* ) _memptr, alignment );
-		T* obj = _buffer;
-		while( _size-- ) {
-			obj = new( ( void* ) obj ) T();
-			obj++;
-		}
-
-	}
-
-	template<typename T>
-	inline ScopedBuffer<T, false>::~ScopedBuffer()
-	{
-		T* obj = _buffer;
-		while( _size-- ) {
-			obj->~T();
-			obj++;
-		}
-		delete[] _memptr;
-	}
-
-	template<typename T>
-	inline ScopedBuffer<T, true>::ScopedBuffer( size_t size, size_t alignment ) : _size( size )
-	{
-        size_t memsize = Math::pad( sizeof( T ) * _size, alignment ) + alignment;// needed for 32-bit architectures!
-		_memptr = new uint8_t[ memsize ];
+    template<typename T>
+    inline ScopedBuffer<T, false>::ScopedBuffer( size_t size, size_t alignment ) : _size( size )
+    {
+        size_t memsize = Math::pad( sizeof( T ) * _size, alignment ) + alignment; // needed for 32-bit architectures!
+        _memptr = new uint8_t[ memsize ];
         _buffer = Util::alignPtr<T>( ( T* ) _memptr, alignment );
-	}
+        T* obj = _buffer;
 
-	template<typename T>
-	inline ScopedBuffer<T, true>::~ScopedBuffer()
-	{
-		delete[] _memptr;
-	}
+        size_t n = size;
+        while( n-- ) {
+            obj = new( ( void* ) obj )T( );
+            obj++;
+        }
 
-	template<typename T>
-	inline size_t ScopedBuffer<T,true>::size() const
-	{
-		return _size;
-	}
+    }
 
-	template<typename T>
-	inline size_t ScopedBuffer<T,false>::size() const
-	{
-		return _size;
-	}
+    template<typename T>
+    inline ScopedBuffer<T, false>::~ScopedBuffer( )
+    {
+        T* obj = _buffer;
+        while( _size-- ) {
+            obj->~T( );
+            obj++;
+        }
+        delete[] _memptr;
+    }
 
-	template<typename T>
-	inline const T* ScopedBuffer<T,true>::ptr() const
-	{
-		return _buffer;
-	}
+    template<typename T>
+    inline ScopedBuffer<T, true>::ScopedBuffer( size_t size, size_t alignment ) : _size( size )
+    {
+        size_t memsize = Math::pad( sizeof( T ) * _size, alignment ) + alignment; // needed for 32-bit architectures!
+        _memptr = new uint8_t[ memsize ];
+        _buffer = Util::alignPtr<T>( ( T* ) _memptr, alignment );
+    }
 
-	template<typename T>
-	inline const T* ScopedBuffer<T,false>::ptr() const
-	{
-		return _buffer;
-	}
+    template<typename T>
+    inline ScopedBuffer<T, true>::~ScopedBuffer( )
+    {
+        delete[] _memptr;
+    }
 
+    template<typename T>
+    inline size_t ScopedBuffer<T, true>::size( ) const
+    {
+        return _size;
+    }
 
-	template<typename T>
-	inline T* ScopedBuffer<T,true>::ptr()
-	{
-		return _buffer;
-	}
+    template<typename T>
+    inline size_t ScopedBuffer<T, false>::size( ) const
+    {
+        return _size;
+    }
 
-	template<typename T>
-	inline T* ScopedBuffer<T,false>::ptr()
-	{
-		return _buffer;
-	}
+    template<typename T>
+    inline const T* ScopedBuffer<T, true>::ptr( ) const
+    {
+        return _buffer;
+    }
+
+    template<typename T>
+    inline const T* ScopedBuffer<T, false>::ptr( ) const
+    {
+        return _buffer;
+    }
+
+    template<typename T>
+    inline T* ScopedBuffer<T, true>::ptr( )
+    {
+        return _buffer;
+    }
+
+    template<typename T>
+    inline T* ScopedBuffer<T, false>::ptr( )
+    {
+        return _buffer;
+    }
 
 }
 
